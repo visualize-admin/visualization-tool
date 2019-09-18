0f27ab1d42ac0a0ebe7410d6d2b3f7f7448c56ee
Luc <luc.guillemot@gmail.com>
2019-09-18 15:33:55 +0200

    feat: update Controls component to fetch all observations

--- app/components/dataset-controls.tsx
+++ app/components/dataset-controls.tsx
@@ -2,28 +2,65 @@ import { Label, Radio } from "@rebass/forms";
 import { Dimension, Measure, DataCube } from "@zazuko/query-rdf-data-cube";
 import React, { useState } from "react";
 import { useDataSetMetadata, useObservations } from "../domain/data-cube";
-import { useLocale } from "../lib/use-locale";
-import { DSVisualization } from "./dataset-visualization";
+
+import { DSBars } from "./chart-bars";
 import { DSDimensionSelect } from "./dataset-dimension-select";
+import { DSFilter } from "./dataset-dimension-filter";
+
+const Controls = ({
+  dataset,
+  dimensions,
+  measures
+}: {
+  dataset: DataCube;
+  dimensions: Dimension[];
+  measures: Measure[];
+}) => {
+  const [selectedDimension, updateDimension] = useState(
+    dimensions[0].iri.value
+  );
+  const [dimensionFilter, addDimensionFilter] = useState({}); // {dimension: dimensionValue[]}
+
+  const observations = useObservations({
+    dataset,
+    dimensions,
+    selectedDimension,
+    measures
+  });
+  console.log({ observations });
+  const dimensionsWithLabel = dimensions.map(dim => {
+    const key = dim.labels[0].value;
+    return [key, dim];
+  });
+  console.log({ dimensionsWithLabel });
+
+  return observations.state === "loaded" ? (
+    <>
+      <DSDimensionSelect
+        dimensions={dimensions}
+        selectedDimension={selectedDimension}
+        updateDimension={updateDimension}
+      />
+      <DSFilter observations={observations.data} dimensions={dimensions} />
+      {/* <DSBars
+        dataset={dataset}
+        dimensions={dimensions}
+        xAxis={selectedDimension}
+        aggregationFunction={"sum"}
+        measures={measures}
+      /> */}
+    </>
+  ) : null;
+};
 
 export const DSControls = ({ dataset }: { dataset: DataCube }) => {
-  const [selectedDimension, updateDimension] = useState("");
-  const locale = useLocale();
   const meta = useDataSetMetadata(dataset);
 
   return meta.state === "loaded" ? (
     <>
-      <h3>Dimensions</h3>
-      <DSDimensionSelect
-        dimensions={meta.data.dimensions}
-        selectedDimension={selectedDimension}
-        updateDimension={updateDimension}
-      />
-
-      <DSVisualization
+      <Controls
         dataset={dataset}
         dimensions={meta.data.dimensions}
-        selectedDimension={selectedDimension}
         measures={meta.data.measures}
       />
     </>
