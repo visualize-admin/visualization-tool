1372b9f9ed56628270efda908ef9c830c7762d07
Luc <luc.guillemot@gmail.com>
2019-09-19 09:13:32 +0200

    feat: add generic line chart

---
+++ app/components/charts-generic/lines.tsx
@@ -0,0 +1,175 @@
+import * as React from "react";
+import * as vega from "vega";
+import { Spec } from "vega";
+
+interface Props {
+  data: any;
+  width: number;
+  xField: string;
+  yField: string;
+  groupBy: string;
+  groupByLabel: string;
+  aggregateFunction: "sum";
+}
+
+export const Lines = ({
+  data,
+  width,
+  xField,
+  yField,
+  groupBy,
+  groupByLabel,
+  aggregateFunction
+}: Props) => {
+  const spec: Spec = {
+    $schema: "https://vega.github.io/schema/vega/v5.json",
+    width,
+    height: width * 0.5,
+    padding: { left: 16, right: 16, top: 64, bottom: 16 },
+    autosize: { type: "fit-x", contains: "padding" },
+
+    // title: { text: title, orient: "none", align: "left", fontSize: 16 },
+
+    data: [
+      {
+        name: "table",
+        values: data,
+        transform: [
+          {
+            type: "aggregate",
+            groupby: [xField, groupBy],
+            fields: [yField, yField],
+            ops: [aggregateFunction, aggregateFunction],
+            as: ["byTime", "byGroup"]
+          }
+        ]
+      }
+    ],
+
+    scales: [
+      {
+        name: "x",
+        type: "time",
+        range: "width",
+        domain: {
+          data: "table",
+          field: xField
+        }
+      },
+      {
+        name: "y",
+        type: "linear",
+        range: "height",
+        nice: true,
+        zero: true,
+        domain: {
+          data: "table",
+          field: "byGroup"
+        }
+      },
+      {
+        name: "color",
+        type: "ordinal",
+        range: "category",
+        domain: {
+          data: "table",
+          field: groupBy
+        }
+      }
+    ],
+
+    axes: [
+      {
+        orient: "bottom",
+        scale: "x",
+        format: "%Y"
+      },
+      {
+        orient: "left",
+        scale: "y"
+      }
+    ],
+
+    marks: [
+      {
+        type: "group",
+        from: {
+          facet: {
+            name: "series",
+            data: "table",
+            groupby: groupBy
+          }
+        },
+        // from: { data: "table" },
+        marks: [
+          {
+            type: "line",
+            from: {
+              data: "series"
+            },
+            encode: {
+              enter: {
+                x: {
+                  scale: "x",
+                  field: xField
+                },
+                y: {
+                  scale: "y",
+                  field: "byGroup"
+                },
+                stroke: {
+                  scale: "color",
+                  field: groupBy
+                },
+
+                strokeWidth: {
+                  value: 1
+                }
+              },
+
+              hover: {
+                fillOpacity: {
+                  value: 0.5
+                }
+              }
+            }
+          }
+        ]
+      }
+    ],
+    legends: [
+      {
+        fill: "color",
+        title: groupByLabel,
+        orient: "bottom",
+        direction: "vertical",
+        columns: 3
+      }
+    ]
+  };
+
+  return <LinesChart spec={spec} />;
+};
+
+const LinesChart = ({ spec }: { spec: any }) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    const createView = async () => {
+      try {
+        const view = new vega.View(vega.parse(spec), {
+          logLevel: vega.Warn,
+          renderer: "svg",
+          container: ref.current,
+          hover: true
+        });
+        await view.runAsync();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    createView();
+  }, [spec]);
+
+  return <div ref={ref} />;
+};
