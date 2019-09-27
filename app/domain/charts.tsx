5b66b05152ca5f532f83dde30f41dc6e334eefee
Luc <luc.guillemot@gmail.com>
2019-09-27 08:50:34 +0200

    fix(domain): prepare data for Vega

--- app/domain/charts.tsx
+++ app/domain/charts.tsx
@@ -1,4 +1,5 @@
 import { Dimension } from "@zazuko/query-rdf-data-cube";
+import { getDimensionLabelFromIri } from "./data-cube";
 
 export const formatData = ({ observations }: { observations: any }) => {
   return observations.map((d: any) => {
@@ -33,24 +34,28 @@ export const formatDataForBarChart = ({
 //TODO: replace generic names by actual labels
 export const formatDataForLineChart = ({
   observations,
+  dimensions,
   xField,
   groupByField,
   heightField
 }: {
   observations: any;
+  dimensions: Dimension[];
   xField: string;
   groupByField: string;
   heightField: string;
 }) => {
-  console.log({ observations });
   return observations.map((d: any) => {
-    return Object.keys(d).reduce((obj: any, key) => {
-      if (key === groupByField) {
-        obj[key] = (d as any)[key].value.value;
-      } else if (key === "measure" || key === xField) {
-        obj[key] = +(d as any)[key].value.value;
-      }
-      return obj;
-    }, {});
+    return {
+      // FIXME: this is not robust, time value ("Jahr") and measure
+      // are nested in dim.value.value,
+      // whereas categorical value is under dim.label.value.
+      [getDimensionLabelFromIri({ dimensionIri: xField, dimensions })]: d.xField
+        .value.value,
+      [getDimensionLabelFromIri({ dimensionIri: groupByField, dimensions })]: d
+        .groupByField.label.value,
+      [getDimensionLabelFromIri({ dimensionIri: heightField, dimensions })]: +d
+        .measure.value.value
+    };
   });
 };
