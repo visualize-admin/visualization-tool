7a2990f9b3b21f65fa18a75b520c3e523be57732
Luc <luc.guillemot@gmail.com>
2019-10-01 08:06:10 +0200

    fix: move font definition to _app

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,52 +1,11 @@
-import { Box, Flex, Heading } from "rebass";
-import { ReactNode } from "react";
-import { ThemeProvider } from "emotion-theming";
-import { Global, css } from "@emotion/core";
 import preset from "@rebass/preset";
-
+import { ThemeProvider } from "emotion-theming";
+import { ReactNode } from "react";
+import { Box, Flex, Heading } from "rebass";
 import { LanguageMenu } from "./language-menu";
-
-const globalCss = css`
-  @font-face {
-    font-family: "FrutigerNeueBold";
-    font-style: normal;
-    font-weight: 700;
-    src: url("../../static/fonts/FrutigerNeueW02-Bd.woff2") format("woff2"),
-      url("../../static/fonts/FrutigerNeueW02-Bd.woff") format("woff");
-  }
-  @font-face {
-    font-family: "FrutigerNeueRegular";
-    font-style: normal;
-    font-weight: 400;
-    src: url("../../static/fonts/FrutigerNeueW02-Regular.woff2") format("woff2"),
-      url("../../static/fonts/FrutigerNeueW02-Regular.woff") format("woff");
-  }
-  @font-face {
-    font-family: "FrutigerNeueLight";
-    font-style: normal;
-    font-weight: 300;
-    src: url("../../static/fonts/FrutigerNeueW02-Light.woff2") format("woff2"),
-      url("../../static/fonts/FrutigerNeueW02-Light.woff") format("woff");
-  }
-  @font-face {
-    font-family: "FrutigerNeueItalic";
-    font-style: italic;
-    font-weight: 400;
-    src: url("../../static/fonts/FrutigerNeueW02-It.woff2") format("woff2"),
-      url("../../static/fonts/FrutigerNeueW02-It.woff") format("woff");
-  }
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: FrutigerNeueRegular, -apple-system, BlinkMacSystemFont,
-      Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji,
-      Segoe UI Symbol;
-  }
-`;
 
 export const AppLayout = ({ children }: { children?: ReactNode }) => (
   <ThemeProvider theme={preset}>
-    <Global styles={globalCss} />
     <Box p={2} bg="muted" color="secondary">
       <Flex justifyContent="space-between" alignItems="center">
         <Heading fontSize={3}>Visualize</Heading>
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -3,6 +3,45 @@ import App, { AppContext } from "next/app";
 import ErrorPage from "next/error";
 import { locales, defaultLocale, catalogs } from "../locales/locales";
 import { I18nProvider } from "@lingui/react";
+import { css, Global } from "@emotion/core";
+
+const globalCss = css`
+  @font-face {
+    font-family: "FrutigerNeueBold";
+    font-style: normal;
+    font-weight: 700;
+    src: url("../../static/fonts/FrutigerNeueW02-Bd.woff2") format("woff2"),
+      url("../../static/fonts/FrutigerNeueW02-Bd.woff") format("woff");
+  }
+  @font-face {
+    font-family: "FrutigerNeueRegular";
+    font-style: normal;
+    font-weight: 400;
+    src: url("../../static/fonts/FrutigerNeueW02-Regular.woff2") format("woff2"),
+      url("../../static/fonts/FrutigerNeueW02-Regular.woff") format("woff");
+  }
+  @font-face {
+    font-family: "FrutigerNeueLight";
+    font-style: normal;
+    font-weight: 300;
+    src: url("../../static/fonts/FrutigerNeueW02-Light.woff2") format("woff2"),
+      url("../../static/fonts/FrutigerNeueW02-Light.woff") format("woff");
+  }
+  @font-face {
+    font-family: "FrutigerNeueItalic";
+    font-style: italic;
+    font-weight: 400;
+    src: url("../../static/fonts/FrutigerNeueW02-It.woff2") format("woff2"),
+      url("../../static/fonts/FrutigerNeueW02-It.woff") format("woff");
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: FrutigerNeueRegular, -apple-system, BlinkMacSystemFont,
+      Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji,
+      Segoe UI Symbol;
+  }
+`;
 
 class MyApp extends App<{ locale: string; statusCode: void | number }> {
   static async getInitialProps(appContext: AppContext) {
@@ -36,6 +75,7 @@ class MyApp extends App<{ locale: string; statusCode: void | number }> {
       <ErrorPage statusCode={statusCode} />
     ) : (
       <I18nProvider language={locale} catalogs={catalogs}>
+        <Global styles={globalCss} />
         <Component {...pageProps} />
       </I18nProvider>
     );
