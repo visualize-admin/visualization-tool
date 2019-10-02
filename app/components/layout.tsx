9dd6e08b649221f4a4e9fe1ff6e259420278866e
Jeremy Stucki <jeremy@interactivethings.com>
2019-10-02 10:43:19 +0200

    feat: Visual tweaks to index page

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -3,13 +3,21 @@ import { Box, Flex, Heading } from "rebass";
 import { LanguageMenu } from "./language-menu";
 
 export const AppLayout = ({ children }: { children?: ReactNode }) => (
-  <>
+  <Flex flexDirection="column" sx={{ minHeight: "100vh" }}>
     <Box p={2} bg="muted" color="secondary">
       <Flex justifyContent="space-between" alignItems="center">
         <Heading fontSize={3}>Visualize</Heading>
         <LanguageMenu />
       </Flex>
     </Box>
-    <Box p={2}>{children}</Box>
-  </>
+    <Flex p={2} flex={1} flexDirection="column">
+      {children}
+    </Flex>
+  </Flex>
+);
+
+export const Center = ({ children }: { children?: ReactNode }) => (
+  <Flex flex={1} justifyContent="center" alignItems="center">
+    {children}
+  </Flex>
 );
