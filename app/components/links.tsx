4e4b023b77df4c841440549a1fd7ffc17fafe1a5
Jeremy Stucki <jeremy@interactivethings.com>
2019-10-02 12:00:26 +0200

    fix: Properly replace dynamic route params in current page link

--- app/components/links.tsx
+++ app/components/links.tsx
@@ -4,6 +4,44 @@ import { useLocale } from "../lib/use-locale";
 import * as qs from "querystring";
 import { useRouter } from "next/router";
 
+const createDynamicRouteProps = ({
+  pathname,
+  query
+}: {
+  pathname: string;
+  query: qs.ParsedUrlQuery;
+}) => {
+  let asPath = pathname;
+  let regularQueryParams: qs.ParsedUrlQuery = {};
+
+  // Get dynamic route params from pathname
+  const dynamicParamRe = /\[(\w+)\]/g;
+  let dynamicParams: Set<string> = new Set();
+  let match: RegExpExecArray | null;
+  while ((match = dynamicParamRe.exec(pathname))) {
+    dynamicParams.add(match[1]);
+  }
+
+  // Replace dynamic route params in `asPath`
+  for (const [k, v] of Object.entries(query)) {
+    if (dynamicParams.has(k)) {
+      asPath = asPath.replace(`[${k}]`, v.toString());
+    } else {
+      regularQueryParams[k] = v;
+    }
+  }
+
+  // Add query params that aren't part of the route as search to `asPath`
+  if (Object.keys(regularQueryParams).length > 0) {
+    asPath = `${asPath}?${qs.stringify(regularQueryParams)}`;
+  }
+
+  return {
+    href: { pathname, query },
+    as: asPath
+  };
+};
+
 export const LocalizedLink = ({
   href,
   as,
@@ -30,10 +68,14 @@ export const CurrentPageLink = ({
   children: React.ReactNode;
 }) => {
   const { pathname, query } = useRouter();
-  const { locale: _, ...queryWithoutLocale } = query;
-  const search = qs.stringify(queryWithoutLocale);
-  const localizedAs =
-    pathname.replace(/\[locale\]/, locale) + (search ? `?${search}` : "");
 
-  return <Link href={{ pathname, query }} as={localizedAs} {...rest} />;
+  return (
+    <Link
+      {...rest}
+      {...createDynamicRouteProps({
+        pathname,
+        query: { ...query, locale }
+      })}
+    />
+  );
 };
