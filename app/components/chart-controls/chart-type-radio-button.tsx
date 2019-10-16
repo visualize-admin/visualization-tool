00195bb6e23afd65cc64cf32689d795dff0d50ae
Luc Guillemot <luc@interactivethings.com>
2019-10-16 16:21:48 +0200

    - add Icon component with an icon name prop - replace custom icons previously used

--- app/components/chart-controls/chart-type-radio-button.tsx
+++ app/components/chart-controls/chart-type-radio-button.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { FieldProps } from "../../domain/config-form";
 import { Flex } from "rebass";
+import { Icon, IconName } from "../../icons";
 
 export const ChartTypeRadio = ({
   label,
@@ -36,26 +37,8 @@ export const ChartTypeRadio = ({
           }
         }}
       >
-        <Icon /> {label}
+        <Icon name={label as IconName} /> {label}
       </Flex>
     </label>
   );
 };
-
-export const Icon = ({ size = 50, color = "currentColor" }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke={color}
-    strokeWidth="1"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <circle cx="12" cy="12" r="10"></circle>
-    <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
-    <line x1="12" y1="17" x2="12" y2="17"></line>
-  </svg>
-);
---
+++ app/icons/index.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { IconCheck } from "./ic-check";
+import { IconBarChart } from "./ic-bar-chart";
+import { IconColumnChart } from "./ic-column-chart";
+import { IconLineChart } from "./ic-line-chart";
+import { IconAreaChart } from "./ic-area-chart";
+import { IconScatterplot } from "./ic-scatterplot";
+
+export const Icons = {
+  check: IconCheck,
+  bar: IconBarChart,
+  column: IconColumnChart,
+  line: IconLineChart,
+  area: IconAreaChart,
+  scatterplot: IconScatterplot
+};
+
+export type IconName = keyof (typeof Icons);
+
+export const Icon = ({
+  size,
+  color,
+  name,
+  ...props
+}: {
+  size?: number;
+  color?: string;
+  name: IconName;
+}) => {
+  const IconComponent = Icons[name];
+  return <IconComponent size={size} color={color} {...props} />;
+};
