3bb13d93b677780e4f0e7df784a42c36abf6af56
Jan Wächter <jan@interactivethings.com>
2019-10-16 16:39:32 +0200

    fix: remove hard-coded color values

--- app/icons/ic-line-chart.tsx
+++ app/icons/ic-line-chart.tsx
@@ -14,9 +14,7 @@ export const IconLineChart = ({ size = 48, color = "currentColor" }) => (
     strokeLinejoin="round"
     >
     <g
-        id="chart"
         transform="translate(3.000000, 6.000000)"
-        fill="#006699"
     >
         <g opacity="0.5" transform="translate(0.534413, 19.639098)">
         <polygon points="14.7727268 12.7524415 2.93117397 1.0658141e-14 -2.48689958e-14 2.7218044 14.1584472 17.9693629 26.9289199 9.45571441 39.1614726 11.3376456 39.7697014 7.3841588 26.002254 5.26608998"></polygon>
