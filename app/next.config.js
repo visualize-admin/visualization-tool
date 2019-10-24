87f79270a0af792c1949a8941d1448a0ff8dc47a
Tomas Carnecky <tomc@caurea.org>
2019-10-24 09:26:38 +0200

    Enable source maps in production

--- app/next.config.js
+++ app/next.config.js
@@ -20,6 +20,28 @@ module.exports = withBundleAnalyzer(
       //     /node_modules/
       //   ]
       // });
+      
+      /* Enable source maps in production */
+      if (!dev) {
+        config.devtool = "source-map";
+
+        for (const plugin of config.plugins) {
+          if (plugin.constructor.name === "UglifyJsPlugin") {
+            plugin.options.sourceMap = true;
+            break;
+          }
+        }
+
+        if (config.optimization && config.optimization.minimizer) {
+          for (const plugin of config.optimization.minimizer) {
+            if (plugin.constructor.name === "TerserPlugin") {
+              plugin.options.sourceMap = true;
+              break;
+            }
+          }
+        }
+      }
+
 
       return config;
     }
