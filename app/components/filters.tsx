085107f2542c4bb7750958d042f70629098fbd1f
Luc Guillemot <luc@interactivethings.com>
2019-10-30 11:01:01 +0100

    Filter all

--- app/components/filters.tsx
+++ app/components/filters.tsx
@@ -9,6 +9,10 @@ import {
 import { Field } from "./field";
 import { Loading } from "./hint";
 import { ControlSection, ControlList } from "./chart-controls";
+import { useConfiguratorState } from "../domain/configurator-state";
+import { Literal, NamedNode } from "rdf-js";
+import { Text } from "rebass";
+import { Trans } from "@lingui/macro";
 
 export const Filters = ({
   chartId,
@@ -52,9 +56,49 @@ const DimensionValues = ({
 }) => {
   const dimensionValues = useDimensionValues({ dataSet, dimension });
   const dimensionIri = getDimensionIri(dimension);
+  const [, dispatch] = useConfiguratorState();
+
+  const [all, toggleAll] = React.useState(false);
+
+  const toggle = (
+    dimIri: string,
+    dimVal: {
+      label: Literal;
+      value: NamedNode;
+    }[]
+  ) => {
+    toggleAll(!all);
+    dispatch({
+      type: "CHART_CONFIG_CHANGED",
+      value: {
+        path: `filters["${dimIri}"]`,
+        value: all
+          ? { [dimVal[0].value.value]: true }
+          : Object.values(dimVal).reduce(
+              (obj, cur, i) => ({ ...obj, [cur.value.value]: true }),
+              {}
+            )
+      }
+    });
+  };
+
   if (dimensionValues.state === "loaded") {
     return (
       <>
+        {!isTimeDimension(dimension) && (
+          <Text
+            variant="paragraph2"
+            mb={4}
+            onClick={() => toggle(dimensionIri, dimensionValues.data)}
+            sx={{ textDecoration: "underline", cursor: "pointer" }}
+          >
+            {all ? (
+              <Trans>Nur erste auswählen</Trans>
+            ) : (
+              <Trans>Alle auswählen</Trans>
+            )}
+          </Text>
+        )}
         {dimensionValues.data.map(dv => {
           return (
             <Field
