59f2896287fe1439de8bbf0bc20f8e56c8107755
Jeremy Stucki <jeremy@interactivethings.com>
2019-10-31 10:18:17 +0100

    chore: Warn on explicit `any` types
    Might change to error when we get rid of these

--- app/ambient.d.ts
+++ app/ambient.d.ts
@@ -1,15 +1,27 @@
+// -----------------------------------------------------------------------------
+// TypeScript helpers
+
+/** Fix this type, preferably before accepting the PR */
+type $FixMe = any; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+/** This `any` is intentional => never has to be fixed */
+type $IntentionalAny = any; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+/** TS cannot express the proper type atm */
+type $Unexpressable = any; // eslint-disable-line @typescript-eslint/no-explicit-any
+
 declare module "@rebass/preset" {
-  const theme: any;
+  const theme: $IntentionalAny;
   export default theme;
 }
 
 declare module "@rebass/forms" {
-  export const Label: (props: any) => JSX.Element;
-  export const Input: (props: any) => JSX.Element;
-  export const Select: (props: any) => JSX.Element;
-  export const Radio: (props: any) => JSX.Element;
-  export const Checkbox: (props: any) => JSX.Element;
-  export const Textarea: (props: any) => JSX.Element;
+  export const Label: (props: $IntentionalAny) => JSX.Element;
+  export const Input: (props: $IntentionalAny) => JSX.Element;
+  export const Select: (props: $IntentionalAny) => JSX.Element;
+  export const Radio: (props: $IntentionalAny) => JSX.Element;
+  export const Checkbox: (props: $IntentionalAny) => JSX.Element;
+  export const Textarea: (props: $IntentionalAny) => JSX.Element;
 }
 
 // -----------------------------------------------------------------------------
