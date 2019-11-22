a89f6a9ec666152d02f156e4330b949bacd0977d
Luc <luc.guillemot@gmail.com>
2019-11-22 16:35:13 +0100

    feat: add components to change title and description

---
+++ app/components/chart-annotations-selector.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ConfiguratorStateDescribingChart } from "../domain";
+import { locales } from "../locales/locales";
+import { CollapsibleSection } from "./chart-controls";
+import { MetaInputField } from "./field";
+
+export const ChartAnnotationsSelector = ({
+  state
+}: {
+  state: ConfiguratorStateDescribingChart;
+}) => {
+  return (
+    <CollapsibleSection title={state.activeField}>
+      {locales.map(locale => (
+        <MetaInputField
+          key={`${locale}-${state.activeField!}`}
+          metaKey={state.activeField!} // FIXME !
+          locale={locale}
+          label={locale}
+        />
+      ))}
+    </CollapsibleSection>
+  );
+};
