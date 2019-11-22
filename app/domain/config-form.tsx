6438233841e6fda55d5329b95c4a66eae7ff709b
Luc <luc.guillemot@gmail.com>
2019-11-22 15:34:04 +0100

    feat: add new hook for FilterTab

--- app/domain/config-form.tsx
+++ app/domain/config-form.tsx
@@ -1,6 +1,6 @@
 import get from "lodash/get";
 import { ChangeEvent, InputHTMLAttributes, useCallback } from "react";
-import { ChartType, MetaKey } from "./config-types";
+import { ChartType, MetaKey, FilterValueSingle } from "./config-types";
 import { useConfiguratorState } from "./configurator-state";
 import { DataSetMetadata } from "./data-cube";
 import { Locales } from "../locales/locales";
@@ -65,6 +65,34 @@ export type FieldProps = Pick<
 // };
 
 export const useControlTab = ({
+  value
+}: {
+  value: string;
+}): FieldProps & {
+  onClick: (x: string) => void;
+} => {
+  const [state, dispatch] = useConfiguratorState();
+
+  const onClick = useCallback<() => void>(() => {
+    dispatch({
+      type: "CONTROL_TAB_CHANGED",
+      value
+    });
+  }, [dispatch, value]);
+
+  const stateValue =
+    state.state === "CONFIGURING_CHART" ? state.activeField : "";
+
+  const checked = stateValue === value;
+
+  return {
+    value,
+    checked,
+    onClick
+  };
+};
+
+export const useFilterTab = ({
   value
 }: {
   value: string;
