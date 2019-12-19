5bb10a7ca095cd497155ea31ebe11c22790f1a44
Jeremy Stucki <jeremy@interactivethings.com>
2019-12-19 17:05:24 +0100

    chore: Also build a versioned image on release

---
+++ scripts/get-package-version.sh
@@ -0,0 +1,8 @@
+# Version key/value should be on his own line
+
+echo $(cat package.json \
+| grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
