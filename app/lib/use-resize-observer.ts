50fd89edced7af7a790e068fc83762527c5012e4
Jeremy Stucki <jeremy@interactivethings.com>
2020-01-15 14:46:48 +0100

    fix: use Ref for ResizeObserver
    I assume this will properly be garbage collected.

--- app/lib/use-resize-observer.ts
+++ app/lib/use-resize-observer.ts
@@ -2,38 +2,39 @@ import { useEffect, useState, useRef } from "react";
 import { ResizeObserver } from "@juggle/resize-observer";
 
 export const useResizeObserver = <T extends Element>() => {
-  const ref = useRef<T>(null);
+  const roRef = useRef<ResizeObserver>()
+  const elRef = useRef<T>(null);
   const [width, changeWidth] = useState(1);
   const [height, changeHeight] = useState(1);
 
   useEffect(() => {
-    if (!ref.current) {
+    if (!elRef.current) {
       return;
     }
 
-    const el = ref.current;
+    if (!roRef.current) {
+      roRef.current = new ResizeObserver(entries => {
+        // Since we only observe the one element, we don't need to loop over the
+        // array
+        if (!entries.length) {
+          return;
+        }
+  
+        const entry = entries[0];
+  
+        const { inlineSize: width, blockSize: height } = entry.contentBoxSize[0];
+  
+        changeWidth(width);
+        changeHeight(height);
+      });
+    }
 
-    const resizeObserver = new ResizeObserver(entries => {
-      // Since we only observe the one element, we don't need to loop over the
-      // array
-      if (!entries.length) {
-        return;
-      }
-
-      const entry = entries[0];
-
-      const { inlineSize: width, blockSize: height } = entry.contentBoxSize[0];
-
-      changeWidth(width);
-      changeHeight(height);
-    });
-
-    resizeObserver.observe(el);
+    roRef.current.observe(elRef.current);
 
     return () => {
-      resizeObserver.disconnect();
+      roRef.current?.disconnect();
     };
   }, []);
 
-  return [ref, width, height] as const;
+  return [elRef, width, height] as const;
 };
