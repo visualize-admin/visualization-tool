66767abc816f1d3780d5017d53a18aac7cd527ee
Jeremy Stucki <jeremy@interactivethings.com>
2020-01-30 11:01:27 +0100

    feat: Add test API route for fetching SPARQL server-side

---
+++ app/pages/api/sparql-test.ts
@@ -0,0 +1,77 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { DataCubeEntryPoint } from "@zazuko/query-rdf-data-cube";
+import { SPARQL_ENDPOINT } from "../../domain/env";
+import { locales } from "../../locales/locales";
+
+const entry = new DataCubeEntryPoint(SPARQL_ENDPOINT, {
+  languages: [...locales],
+  extraMetadata: [
+    {
+      variable: "contact",
+      iri: "https://pcaxis.described.at/contact",
+      multilang: true
+    },
+    {
+      variable: "source",
+      iri: "https://pcaxis.described.at/source",
+      multilang: true
+    },
+    {
+      variable: "survey",
+      iri: "https://pcaxis.described.at/survey",
+      multilang: true
+    },
+    {
+      variable: "database",
+      iri: "https://pcaxis.described.at/database",
+      multilang: true
+    },
+    {
+      variable: "unit",
+      iri: "https://pcaxis.described.at/unit",
+      multilang: true
+    },
+    {
+      variable: "note",
+      iri: "https://pcaxis.described.at/note",
+      multilang: true
+    },
+    {
+      variable: "dateCreated",
+      iri: "http://schema.org/dateCreated",
+      multilang: false
+    },
+    { variable: "dateModified", iri: "http://schema.org/dateModified" },
+    {
+      variable: "description",
+      iri: "http://www.w3.org/2000/01/rdf-schema#comment",
+      multilang: true
+    }
+  ]
+});
+
+
+/**
+ * Endpoint to write configuration to.
+ */
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const result = await entry.dataCubes();
+
+        // TODO: Make this 201 and set final URI as Location header
+        res.status(200).json(result.map(cb => cb.iri));
+      } catch (e) {
+        console.error(e);
+        res.status(500).json({ message: "Something went wrong!" });
+      }
+
+      break;
+    default:
+      res.setHeader("Allow", ["GET"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+};
