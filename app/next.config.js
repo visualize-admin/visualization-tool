fb251f1f9487413832d7ba023c6d51b89292ee8f
Jeremy Stucki <jeremy@interactivethings.com>
2020-02-26 17:09:25 +0100

    feat: Set up monorepo using preconstruct

--- app/next.config.js
+++ app/next.config.js
@@ -3,18 +3,18 @@ const withMDX = require("@next/mdx")();
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 });
-
+const withPreconstruct = require("@preconstruct/next");
 
 const VERSION = `v${pkg.version}`;
 
 const publicRuntimeConfig = {
   // SPARQL_ENDPOINT: "https://ld.stadt-zuerich.ch/query"
   SPARQL_ENDPOINT:
-  process.env.SPARQL_ENDPOINT ||
-  "https://trifid-lindas.test.cluster.ldbar.ch/query",
+    process.env.SPARQL_ENDPOINT ||
+    "https://trifid-lindas.test.cluster.ldbar.ch/query",
   PUBLIC_URL: process.env.PUBLIC_URL
-  ? process.env.PUBLIC_URL.replace(/\/$/, "")
-  : "",
+    ? process.env.PUBLIC_URL.replace(/\/$/, "")
+    : "",
   GA_TRACKING_ID: process.env.GA_TRACKING_ID
 };
 
@@ -22,77 +22,79 @@ console.log("Starting with publicRuntimeConfig\n", publicRuntimeConfig);
 console.log("Version", VERSION);
 console.log("Extra Certs", process.env.NODE_EXTRA_CA_CERTS);
 
-module.exports = withBundleAnalyzer(
-  withMDX({
-    publicRuntimeConfig,
+module.exports = withPreconstruct(
+  withBundleAnalyzer(
+    withMDX({
+      publicRuntimeConfig,
 
-    // Build-time env variables
-    env: {
-      VERSION
-    },
+      // Build-time env variables
+      env: {
+        VERSION
+      },
 
-    pageExtensions: ["js", "ts", "tsx", "mdx"],
+      pageExtensions: ["js", "ts", "tsx", "mdx"],
 
-    webpack(config, { dev, isServer, defaultLoaders }) {
-      // Transpile ES6 modules from node_modules
-      // ATTENTION: this does actually NOT WORK. Probably needs a different loader than the next-babel-loader (like e.g. vanilla babel-loader). Figure out later. Cf. https://github.com/facebook/create-react-app/blob/f36d61a5dbabd0266c65bcdb3061d8bf9334f752/packages/react-scripts/config/webpack.config.js#L444-L482
-      // config.module.rules.push({
-      //   test: /\.(js|mjs)$/,
-      //   loader: defaultLoaders.babel,
-      //   include: [
-      //     /node_modules/
-      //   ]
-      // });
+      webpack(config, { dev, isServer, defaultLoaders }) {
+        // Transpile ES6 modules from node_modules
+        // ATTENTION: this does actually NOT WORK. Probably needs a different loader than the next-babel-loader (like e.g. vanilla babel-loader). Figure out later. Cf. https://github.com/facebook/create-react-app/blob/f36d61a5dbabd0266c65bcdb3061d8bf9334f752/packages/react-scripts/config/webpack.config.js#L444-L482
+        // config.module.rules.push({
+        //   test: /\.(js|mjs)$/,
+        //   loader: defaultLoaders.babel,
+        //   include: [
+        //     /node_modules/
+        //   ]
+        // });
 
-      // config.module.rules.push({
-      //   test: /\.(js|mjs)$/,
-      //   include: [/node_modules/],
-      //   exclude: [/babel\/standalone/, /core-js/, /next/],
-      //   use: {
-      //     loader: "babel-loader"
-      //   }
-      // });
-      config.module.rules.push({
-        test: /\.(js|mjs)$/,
-        include: [/node_modules/],
-        exclude: [/@babel\/standalone/, /@babel\/runtime/, /core-js/, /next/],
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-            // plugins: ["@babel/plugin-syntax-dynamic-import"]
+        // config.module.rules.push({
+        //   test: /\.(js|mjs)$/,
+        //   include: [/node_modules/],
+        //   exclude: [/babel\/standalone/, /core-js/, /next/],
+        //   use: {
+        //     loader: "babel-loader"
+        //   }
+        // });
+        config.module.rules.push({
+          test: /\.(js|mjs)$/,
+          include: [/node_modules/],
+          exclude: [/@babel\/standalone/, /@babel\/runtime/, /core-js/, /next/],
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+              // plugins: ["@babel/plugin-syntax-dynamic-import"]
+            }
           }
-        }
-      });
+        });
 
-      config.module.rules.push({
-        test: /\.(graphql|gql)$/,
-        exclude: /node_modules/,
-        loader: "graphql-tag/loader"
-      });
+        config.module.rules.push({
+          test: /\.(graphql|gql)$/,
+          exclude: /node_modules/,
+          loader: "graphql-tag/loader"
+        });
 
-      /* Enable source maps in production */
-      if (!dev) {
-        config.devtool = "source-map";
+        /* Enable source maps in production */
+        if (!dev) {
+          config.devtool = "source-map";
 
-        for (const plugin of config.plugins) {
-          if (plugin.constructor.name === "UglifyJsPlugin") {
-            plugin.options.sourceMap = true;
-            break;
-          }
-        }
-
-        if (config.optimization && config.optimization.minimizer) {
-          for (const plugin of config.optimization.minimizer) {
-            if (plugin.constructor.name === "TerserPlugin") {
+          for (const plugin of config.plugins) {
+            if (plugin.constructor.name === "UglifyJsPlugin") {
               plugin.options.sourceMap = true;
               break;
             }
           }
-        }
-      }
 
-      return config;
-    }
-  })
+          if (config.optimization && config.optimization.minimizer) {
+            for (const plugin of config.optimization.minimizer) {
+              if (plugin.constructor.name === "TerserPlugin") {
+                plugin.options.sourceMap = true;
+                break;
+              }
+            }
+          }
+        }
+
+        return config;
+      }
+    })
+  )
 );
