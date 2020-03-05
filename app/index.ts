bcf687da4d5125aaa6f446b1c53481f532e2a53e
Jeremy Stucki <jeremy@interactivethings.com>
2020-03-05 09:24:51 +0100

    feat: Export i18n features in npm package

--- app/index.ts
+++ app/index.ts
@@ -8,3 +8,14 @@ export {
 } from "./domain/configurator-state";
 
 export { ChartEditor } from "./components/editor/chart-editor";
+
+export { useLocale, LocaleProvider } from "./lib/use-locale";
+
+export { I18nProvider } from "@lingui/react";
+
+export {
+  catalogs,
+  defaultLocale,
+  locales,
+  parseLocaleString
+} from "./locales/locales";
