32f3aeac63ccffaea0b79d7fe9efc7450f543603
Luc <luc.guillemot@gmail.com>
2020-04-09 09:21:28 +0200

    fix: remove a11y table sorting

--- app/components/a11y-table.tsx
+++ app/components/a11y-table.tsx
@@ -1,6 +1,5 @@
-import { Trans } from "@lingui/macro";
-import { ascending, descending } from "d3-array";
-import React, { memo, useMemo, useState } from "react";
+import { Box } from "@theme-ui/components";
+import React, { memo, useMemo } from "react";
 import { ChartFields, Observation } from "../domain";
 import { ComponentFieldsFragment } from "../graphql/query-hooks";
 
@@ -10,7 +9,7 @@ export const A11yTable = memo(
     dimensions,
     measures,
     fields,
-    observations
+    observations,
   }: {
     title: string;
     dimensions: ComponentFieldsFragment[];
@@ -18,29 +17,29 @@ export const A11yTable = memo(
     fields: ChartFields;
     observations: Observation[];
   }) => {
-    const [sortingField, setSortingField] = useState();
-    const [direction, setDirection] = useState();
+    // const [sortingField, setSortingField] = useState();
+    // const [direction, setDirection] = useState();
 
-    const sortBy = (fieldKey: string) => {
-      let newDirection: "ascending" | "descending";
-      let isAscending = direction === "ascending";
-      if (fieldKey === sortingField) {
-        newDirection = isAscending ? "descending" : "ascending";
-      } else {
-        newDirection = "ascending";
-      }
-      observations.sort((a, b) =>
-        newDirection === "ascending"
-          ? ascending(a[fieldKey], b[fieldKey])
-          : descending(a[fieldKey], b[fieldKey])
-      );
-      setSortingField(fieldKey);
-      setDirection(newDirection);
-    };
+    // const sortBy = (fieldKey: string) => {
+    //   let newDirection: "ascending" | "descending";
+    //   let isAscending = direction === "ascending";
+    //   if (fieldKey === sortingField) {
+    //     newDirection = isAscending ? "descending" : "ascending";
+    //   } else {
+    //     newDirection = "ascending";
+    //   }
+    //   observations.sort((a, b) =>
+    //     newDirection === "ascending"
+    //       ? ascending(a[fieldKey], b[fieldKey])
+    //       : descending(a[fieldKey], b[fieldKey])
+    //   );
+    //   setSortingField(fieldKey);
+    //   setDirection(newDirection);
+    // };
 
     const headers = useMemo(() => {
       const obsKeys = new Set(Object.keys(observations[0]));
-      return [...dimensions, ...measures].filter(d => obsKeys.has(d.iri));
+      return [...dimensions, ...measures].filter((d) => obsKeys.has(d.iri));
     }, [dimensions, measures, observations]);
 
     return (
@@ -49,17 +48,17 @@ export const A11yTable = memo(
         <tbody>
           <tr>
             {headers.map(({ iri, label }) => {
-              const isSortingField = sortingField === iri;
+              // const isSortingField = sortingField === iri;
               return (
                 <th
                   role="columnheader"
                   scope="col"
                   key={iri}
-                  aria-sort={isSortingField ? direction : "none"}
+                  // aria-sort={isSortingField ? direction : "none"}
                 >
-                  <button onClick={() => sortBy(iri)}>
-                    {label}
-                    {isSortingField && (
+                  {/* <button onClick={() => sortBy(iri)}> */}
+                  <Box>{label}</Box>
+                  {/* {isSortingField && (
                       <>
                         {", "}
                         <Trans id="accessibility.table.sorting">
@@ -76,8 +75,8 @@ export const A11yTable = memo(
                           order
                         </Trans>
                       </>
-                    )}
-                  </button>
+                    )} */}
+                  {/* </button> */}
                 </th>
               );
             })}
