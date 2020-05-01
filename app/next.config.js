eb80fca48d6951a752ed815466b296de56f22f9d
Jeremy Stucki <jeremy@interactivethings.com>
2020-05-01 16:56:56 +0200

    chore: Clean up

--- app/next.config.js
+++ app/next.config.js
@@ -35,36 +35,6 @@ module.exports = withPreconstruct(
       pageExtensions: ["js", "ts", "tsx", "mdx"],
 
       webpack(config, { dev, isServer, defaultLoaders }) {
-        // Transpile ES6 modules from node_modules
-        // ATTENTION: this does actually NOT WORK. Probably needs a different loader than the next-babel-loader (like e.g. vanilla babel-loader). Figure out later. Cf. https://github.com/facebook/create-react-app/blob/f36d61a5dbabd0266c65bcdb3061d8bf9334f752/packages/react-scripts/config/webpack.config.js#L444-L482
-        // config.module.rules.push({
-        //   test: /\.(js|mjs)$/,
-        //   loader: defaultLoaders.babel,
-        //   include: [/node_modules/],
-        //   exclude: [/@babel/, /core-js/, /next/],
-        // });
-
-        // config.module.rules.push({
-        //   test: /\.(js|mjs)$/,
-        //   include: [/node_modules/],
-        //   exclude: [/babel\/standalone/, /core-js/, /next/],
-        //   use: {
-        //     loader: "babel-loader"
-        //   }
-        // });
-        // config.module.rules.push({
-        //   test: /\.(js|mjs)$/,
-        //   include: [/node_modules/],
-        //   exclude: [/@babel\/standalone/, /@babel\/runtime/, /core-js/, /next/],
-        //   use: {
-        //     loader: "babel-loader",
-        //     options: {
-        //       presets: ["@babel/preset-env"]
-        //       // plugins: ["@babel/plugin-syntax-dynamic-import"]
-        //     }
-        //   }
-        // });
-
         config.module.rules.push({
           test: /\.(graphql|gql)$/,
           exclude: /node_modules/,
