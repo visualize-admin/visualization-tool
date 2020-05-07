4dce7db2c0d5930a48a25fe0b311633d1dd66ced
Jan Wächter <jan@interactivethings.com>
2020-05-07 15:34:43 +0200

    feat: Homepage grid background

--- app/components/homepage/intro.tsx
+++ app/components/homepage/intro.tsx
@@ -1,13 +1,26 @@
 import React from "react";
-import { Box, Button, Text, Flex } from "@theme-ui/components";
+import { Box, Button, Text, Flex, Grid } from "@theme-ui/components";
 import { HintRed } from "../hint";
 import { LocalizedLink } from "../links";
+import { buildExecutionContext } from "graphql/execution/execute";
+import { IconArrowDown } from "../../icons/ic-arrow-down";
+import { IconX } from "../../icons/ic-x";
+import { IconY } from "../../icons/ic-y";
+import { IconScatterplot } from "../../icons/ic-scatterplot";
+import { IconBarChart } from "../../icons/ic-bar-chart";
+import { IconTable } from "../../icons/ic-table";
+import { IconAreaChart } from "../../icons/ic-area-chart";
+import { IconSegment } from "../../icons/ic-segment";
+import { IconFilter } from "../../icons/ic-filter";
+import { IconLineChart } from "../../icons/ic-line-chart";
+import { IconColumnChart } from "../../icons/ic-column-chart";
+import { IconText } from "../../icons/ic-text";
 
 export const Intro = ({
   hint,
   title,
   teaser,
-  buttonLabel,
+  buttonLabel
 }: {
   hint: string;
   title: string;
@@ -21,32 +34,87 @@ export const Intro = ({
       </Box>
       <Box
         sx={{
-          backgroundImage: [
-            'url("chart_icons_s.jpg")',
-            'url("chart_icons.jpg")',
-            'url("chart_icons.jpg")',
-          ],
+          // backgroundImage: [
+          //   'url("chart_icons_s.jpg")',
+          //   'url("chart_icons.jpg")',
+          //   'url("chart_icons.jpg")'
+          // ],
           backgroundSize: "contain",
           backgroundRepeat: "no-repeat",
           mx: 4,
           pt: ["62px", "88px"],
-          pb: [7, 8],
+          pb: [7, 8]
         }}
       >
-        <Box sx={{ maxWidth: 768, m: "0 auto" }}>
-          <Title>{title}</Title>
-          <Teaser>{teaser}</Teaser>
-          <Flex sx={{ justifyContent: "center" }}>
-            <LocalizedLink
-              pathname="/[locale]/create/[chartId]"
-              query={{ chartId: "new" }}
-              passHref
-            >
-              <Button as="a" variant="primary">
-                {buttonLabel}
-              </Button>
-            </LocalizedLink>
-          </Flex>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(12, minmax(24px, 1fr))",
+            gridTemplateRows: "repeat(5, minmax(60px, 1fr))",
+            justifyItems: "center",
+            alignItems: "center"
+          }}
+        >
+          <Box sx={{ gridColumnStart: 1, gridRowStart: 1 }}>
+            <IconX color="#375172" size={24} />
+          </Box>
+          <Box sx={{ gridColumnStart: 5, gridRowStart: 1 }}>
+            <IconY color="#8D5A54" size={24} />
+          </Box>
+          <Box sx={{ gridColumnStart: 7, gridRowStart: 2 }}>
+            <IconColumnChart color="#008F85" size={24} />
+          </Box>
+          <Box sx={{ gridColumnStart: 3, gridRowStart: 2 }}>
+            <IconScatterplot color="#F38B3C" size={24} />
+          </Box>
+          <Box sx={{ gridColumnStart: 9, gridRowStart: 1 }}>
+            <IconTable color="#928D88" size={24} />
+          </Box>
+          <Box sx={{ gridColumnStart: 12, gridRowStart: 2 }}>
+            <IconScatterplot color="#32B8DF" size={24} />
+          </Box>
+          <Box sx={{ gridColumnStart: 2, gridRowStart: 3 }}>
+            <IconAreaChart color="#008F85" size={24} />
+          </Box>
+          <Box sx={{ gridColumnStart: 11, gridRowStart: 3 }}>
+            <IconSegment color="#8D5A54" size={24} />
+          </Box>
+          <Box sx={{ gridColumnStart: 1, gridRowStart: 4 }}>
+            <IconFilter color="#375172" size={24} />
+          </Box>
+          <Box sx={{ gridColumnStart: 12, gridRowStart: 4 }}>
+            <IconText color="#008F85" size={24} />
+          </Box>
+          <Box sx={{ gridColumnStart: 3, gridRowStart: 5 }}>
+            <IconLineChart color="#C97146" size={24} />
+          </Box>
+          <Box sx={{ gridColumnStart: 10, gridRowStart: 5 }}>
+            <IconBarChart color="#F9C16E" size={24} />
+          </Box>
+          <Box
+            sx={{
+              maxWidth: 768,
+              m: "0 auto",
+              gridColumnStart: 1,
+              gridColumnEnd: 13,
+              gridRowStart: 2,
+              gridRowEnd: 5
+            }}
+          >
+            <Title>{title}</Title>
+            <Teaser>{teaser}</Teaser>
+            <Flex sx={{ justifyContent: "center" }}>
+              <LocalizedLink
+                pathname="/[locale]/create/[chartId]"
+                query={{ chartId: "new" }}
+                passHref
+              >
+                <Button as="a" variant="primary">
+                  {buttonLabel}
+                </Button>
+              </LocalizedLink>
+            </Flex>
+          </Box>
         </Box>
       </Box>
     </Box>
@@ -64,7 +132,7 @@ export const Title = ({ children }: { children: React.ReactNode }) => (
       fontWeight: "bold",
       fontSize: [8, "3.5rem", "3.5rem"],
       hyphens: "auto",
-      mb: [4],
+      mb: [4]
     }}
   >
     {children}
@@ -77,7 +145,7 @@ export const Teaser = ({ children }: { children: React.ReactNode }) => (
       fontSize: [4, 4, 4],
       textAlign: "center",
       color: "monochrome700",
-      mb: [6, 5],
+      mb: [6, 5]
     }}
   >
     {children}
