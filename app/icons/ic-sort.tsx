b211f7844fde35cf3308046da9334785599130fe
Jan Wächter <jan@interactivethings.com>
2020-05-14 09:45:42 +0200

    fix: Add sort icon

---
+++ app/icons/ic-sort.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export const IconSort = ({ size = 24, color = "currentColor" }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill={color}
+    fillRule="evenodd"
+    stroke="none"
+  >
+    <path d="M14,17 L14,19 L10,19 L10,17 L14,17 Z M16,13 L16,15 L8,15 L8,13 L16,13 Z M18,9 L18,11 L6,11 L6,9 L18,9 Z M20,5 L20,7 L4,7 L4,5 L20,5 Z"></path>
+  </svg>
+);
