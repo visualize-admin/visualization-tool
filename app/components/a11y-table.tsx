1520cfaee73c8a760465d95ceb3241a27e4940f5
Luc Guillemot <luc.guillemot@gmail.com>
2020-05-25 16:25:13 +0200

    A11y table fix (#15)
    * fix: remove unused code
    
    * fix: visually hide data table (without display:none)

--- app/components/a11y-table.tsx
+++ app/components/a11y-table.tsx
@@ -2,6 +2,7 @@ import { Box } from "@theme-ui/components";
 import React, { memo, useMemo } from "react";
 import { ChartFields, Observation } from "../domain";
 import { ComponentFieldsFragment } from "../graphql/query-hooks";
+import VisuallyHidden from "@reach/visually-hidden";
 
 export const A11yTable = memo(
   ({
@@ -17,81 +18,37 @@ export const A11yTable = memo(
     fields: ChartFields;
     observations: Observation[];
   }) => {
-    // const [sortingField, setSortingField] = useState();
-    // const [direction, setDirection] = useState();
-
-    // const sortBy = (fieldKey: string) => {
-    //   let newDirection: "ascending" | "descending";
-    //   let isAscending = direction === "ascending";
-    //   if (fieldKey === sortingField) {
-    //     newDirection = isAscending ? "descending" : "ascending";
-    //   } else {
-    //     newDirection = "ascending";
-    //   }
-    //   observations.sort((a, b) =>
-    //     newDirection === "ascending"
-    //       ? ascending(a[fieldKey], b[fieldKey])
-    //       : descending(a[fieldKey], b[fieldKey])
-    //   );
-    //   setSortingField(fieldKey);
-    //   setDirection(newDirection);
-    // };
-
     const headers = useMemo(() => {
       const obsKeys = new Set(Object.keys(observations[0]));
       return [...dimensions, ...measures].filter((d) => obsKeys.has(d.iri));
     }, [dimensions, measures, observations]);
 
     return (
-      <table style={{ display: "none" }}>
-        <caption>{title}</caption>
-        <tbody>
-          <tr>
-            {headers.map(({ iri, label }) => {
-              // const isSortingField = sortingField === iri;
+      <VisuallyHidden>
+        <table>
+          <caption>{title}</caption>
+          <tbody>
+            <tr>
+              {headers.map(({ iri, label }) => {
+                return (
+                  <th role="columnheader" scope="col" key={iri}>
+                    <Box>{label}</Box>
+                  </th>
+                );
+              })}
+            </tr>
+            {observations.map((obs, i) => {
               return (
-                <th
-                  role="columnheader"
-                  scope="col"
-                  key={iri}
-                  // aria-sort={isSortingField ? direction : "none"}
-                >
-                  {/* <button onClick={() => sortBy(iri)}> */}
-                  <Box>{label}</Box>
-                  {/* {isSortingField && (
-                      <>
-                        {", "}
-                        <Trans id="accessibility.table.sorting">
-                          sorted by {label} in
-                          {direction === "ascending" ? (
-                            <Trans id="accessibility.table.sorting.ascending">
-                              ascending
-                            </Trans>
-                          ) : (
-                            <Trans id="accessibility.table.sorting.descending">
-                              descending
-                            </Trans>
-                          )}{" "}
-                          order
-                        </Trans>
-                      </>
-                    )} */}
-                  {/* </button> */}
-                </th>
+                <tr key={i}>
+                  {headers.map(({ iri }) => (
+                    <td key={iri}>{obs[iri]}</td>
+                  ))}
+                </tr>
               );
             })}
-          </tr>
-          {observations.map((obs, i) => {
-            return (
-              <tr key={i}>
-                {headers.map(({ iri }) => (
-                  <td key={iri}>{obs[iri]}</td>
-                ))}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </VisuallyHidden>
     );
   }
 );
