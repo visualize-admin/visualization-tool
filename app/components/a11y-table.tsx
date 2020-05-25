ac28cad0b6280e6093d9460571d480e03cf989ac
Luc <luc.guillemot@gmail.com>
2020-05-25 14:54:20 +0200

    fix: remove unused code

--- app/components/a11y-table.tsx
+++ app/components/a11y-table.tsx
@@ -17,66 +17,20 @@ export const A11yTable = memo(
     fields: ChartFields;
     observations: Observation[];
   }) => {
-    // const [sortingField, setSortingField] = useState();
-    // const [direction, setDirection] = useState();
-
-    // const sortBy = (fieldKey: string) => {
-    //   let newDirection: "ascending" | "descending";
-    //   let isAscending = direction === "ascending";
-    //   if (fieldKey === sortingField) {
-    //     newDirection = isAscending ? "descending" : "ascending";
-    //   } else {
-    //     newDirection = "ascending";
-    //   }
-    //   observations.sort((a, b) =>
-    //     newDirection === "ascending"
-    //       ? ascending(a[fieldKey], b[fieldKey])
-    //       : descending(a[fieldKey], b[fieldKey])
-    //   );
-    //   setSortingField(fieldKey);
-    //   setDirection(newDirection);
-    // };
-
     const headers = useMemo(() => {
       const obsKeys = new Set(Object.keys(observations[0]));
       return [...dimensions, ...measures].filter((d) => obsKeys.has(d.iri));
     }, [dimensions, measures, observations]);
 
     return (
-      <table style={{ display: "none" }}>
+      <table>
         <caption>{title}</caption>
         <tbody>
           <tr>
             {headers.map(({ iri, label }) => {
-              // const isSortingField = sortingField === iri;
               return (
-                <th
-                  role="columnheader"
-                  scope="col"
-                  key={iri}
-                  // aria-sort={isSortingField ? direction : "none"}
-                >
-                  {/* <button onClick={() => sortBy(iri)}> */}
+                <th role="columnheader" scope="col" key={iri}>
                   <Box>{label}</Box>
-                  {/* {isSortingField && (
-                      <>
-                        {", "}
-                        <Trans id="accessibility.table.sorting">
-                          sorted by {label} in
-                          {direction === "ascending" ? (
-                            <Trans id="accessibility.table.sorting.ascending">
-                              ascending
-                            </Trans>
-                          ) : (
-                            <Trans id="accessibility.table.sorting.descending">
-                              descending
-                            </Trans>
-                          )}{" "}
-                          order
-                        </Trans>
-                      </>
-                    )} */}
-                  {/* </button> */}
                 </th>
               );
             })}
