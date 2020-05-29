91c35e8f2657213f2e77b3533a6f2810bf64e1d1
Jeremy Stucki <jeremy@interactivethings.com>
2020-05-29 17:59:21 +0200

    fix: Update tests

--- app/test/snapshot.test.ts
+++ app/test/snapshot.test.ts
@@ -11,18 +11,19 @@ declare global {
 const RENDER_DELAY = 1000;
 const SNAPSHOT_OPTS = {
   failureThreshold: 10,
-  failureThresholdType: "pixel",
-  customSnapshotIdentifier: ({ currentTestName, counter }: $IntentionalAny) =>
-    `${currentTestName}`
+  failureThresholdType: "pixel"
 };
 
 const configIds = loadFixtureConfigIdsSync();
 
-test.each(configIds)("chart-%s", async id => {
+test.each(configIds)("chart-%s", async (id) => {
   await page.goto("http://localhost:3000/en/__test/" + id, {
-    waitUntil: "networkidle2"
+    waitUntil: "networkidle2",
   });
   await page.waitFor(RENDER_DELAY);
   const image = await page.screenshot();
-  expect(image).toMatchImageSnapshot(SNAPSHOT_OPTS);
+  expect(image).toMatchImageSnapshot({
+    ...SNAPSHOT_OPTS,
+    customSnapshotIdentifier: `chart-${id}`,
+  });
 });
