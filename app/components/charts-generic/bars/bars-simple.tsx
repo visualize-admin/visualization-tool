6ad85201e0a3948eafe6766883320a75a8826adf
Luc <luc.guillemot@gmail.com>
2020-09-22 09:54:08 +0200

    feat: add BarLabels component

--- app/components/charts-generic/bars/bars-simple.tsx
+++ app/components/charts-generic/bars/bars-simple.tsx
@@ -17,7 +17,6 @@ export const Bars = () => {
   const theme = useTheme();
   const {
     labelColor,
-    gridColor,
     labelFontSize,
     fontFamily,
     domainColor,
@@ -57,6 +56,44 @@ export const Bars = () => {
                 getX(d) <= 0 ? theme.colors.secondary : theme.colors.primary
               }
             />
+          </g>
+        );
+      })}
+    </g>
+  );
+};
+
+export const BarLabels = () => {
+  const {
+    sortedData,
+    bounds,
+    getX,
+    xScale,
+    getY,
+    yScale,
+  } = useChartState() as BarsState;
+  const { labelColor, labelFontSize, fontFamily } = useChartTheme();
+  const { margins } = bounds;
+
+  return (
+    <g transform={`translate(${margins.left}, ${margins.top})`}>
+      {sortedData.map((d, i) => {
+        return (
+          <g transform={`translate(0, ${yScale(getY(d))} )`} key={i}>
+            <text
+              key={i}
+              style={{
+                fontFamily,
+                fill: labelColor,
+                fontSize: labelFontSize,
+              }}
+              x={0}
+              y={BAR_SPACE_ON_TOP - BAR_AXIS_OFFSET}
+              dx={6}
+              dy={labelFontSize * 1.5}
+            >
+              <tspan fontWeight="bold">{getX(d)}</tspan>
+            </text>
           </g>
         );
       })}
