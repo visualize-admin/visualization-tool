1c1e34508b7fbeb6683ac2027f03eeef78e006e6
Luc <luc.guillemot@gmail.com>
2020-10-13 14:44:46 +0200

    feat: add Cell components

--- app/components/data-table/cell.tsx
+++ app/components/data-table/cell.tsx
@@ -1,25 +1,89 @@
-import { Box, Button } from "@theme-ui/components";
+import { Box } from "@theme-ui/components";
 import * as React from "react";
+import { CellPropGetter, TableCellProps } from "react-table";
+import { SystemStyleObject } from "@styled-system/css";
 
-export const CellUI = ({
-  cell,
-  children,
+export const TextCell = ({
+  value,
+  color,
+  bg,
+  textAlign,
+  fontWeight,
+  cellProps,
 }: {
-  cell: $FixMe;
-  children: React.ReactNode;
+  value: string | number;
+  color: SystemStyleObject;
+  bg: SystemStyleObject;
+  textAlign: SystemStyleObject;
+  fontWeight: SystemStyleObject;
+  cellProps?: (propGetter?: CellPropGetter<$FixMe>) => TableCellProps;
 }) => (
-  <td
-    style={{
-      background: cell.isGrouped
-        ? "#0aff0082"
-        : cell.isAggregated
-        ? "#ffa50078"
-        : cell.isPlaceholder
-        ? "#ff000042"
-        : "white",
-    }}
-    {...cell.getCellProps()}
-  >
-    {children}
-  </td>
+  <Box as="td" sx={{ color, bg, textAlign, fontWeight }} {...cellProps}>
+    {value}
+  </Box>
+);
+export const TagCell = ({
+  value,
+  color,
+  fontWeight,
+  tagColor,
+  cellProps,
+}: {
+  value: string | number;
+  color: SystemStyleObject;
+  fontWeight: SystemStyleObject;
+  tagColor: SystemStyleObject;
+  cellProps?: (propGetter?: CellPropGetter<$FixMe>) => TableCellProps;
+}) => (
+  <Box as="td" sx={{ color, fontWeight }} {...cellProps}>
+    <Box
+      as="span"
+      sx={{
+        bg: tagColor,
+        borderRadius: "15px",
+        px: 3,
+        py: 1,
+        my: 1,
+      }}
+    >
+      {value}
+    </Box>
+  </Box>
+);
+
+export const BarCell = ({
+  value,
+  barColor,
+  barBackground,
+  barWidth,
+  cellProps,
+}: {
+  value: string | number;
+  barColor: string | false;
+  barBackground: string | false;
+  barWidth: number; // as percentage
+  cellProps?: (propGetter?: CellPropGetter<$FixMe>) => TableCellProps;
+}) => (
+  <Box as="td" sx={{ textAlign: "right", width: "100%" }} {...cellProps}>
+    <Box>{value}</Box>
+    <Box
+      sx={{
+        width: "100%",
+        height: "16px",
+        position: "relative",
+        bg: barBackground,
+      }}
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: `${barWidth}%`,
+          height: "16px",
+          bg: barColor,
+        }}
+      />
+    </Box>
+  </Box>
 );
