8c4806d591030f3ccfc104f22c07049668c29c38
Luc <luc.guillemot@gmail.com>
2020-11-03 18:44:47 +0100

    fix: simplify and add tags to group headers

--- app/charts/table/group-header.tsx
+++ app/charts/table/group-header.tsx
@@ -1,17 +1,25 @@
-import { SystemStyleObject } from "@styled-system/css";
 import { Box, Flex } from "@theme-ui/components";
 import * as React from "react";
-import { Cell, Row } from "react-table";
+import { Row } from "react-table";
 import { Observation } from "../../domain/data";
 import { Icon } from "../../icons";
 import { useChartState } from "../shared/use-chart-state";
-import { CellContent, Tag } from "./cell";
+import { Tag } from "./cell";
 import { TableChartState } from "./table-state";
 
 export const GroupHeader = ({ row }: { row: Row<Observation> }) => {
+  const { tableColumnsMeta } = useChartState() as TableChartState;
+
   return (
     <>
       {row.cells.map((cell, i) => {
+        const {
+          type,
+          textStyle,
+          textColor,
+          columnColor,
+          colorScale,
+        } = tableColumnsMeta[cell.column.id];
         return (
           <React.Fragment key={i}>
             {cell.isGrouped && (
@@ -23,7 +31,6 @@ export const GroupHeader = ({ row }: { row: Row<Observation> }) => {
                 <Flex
                   sx={{
                     alignItems: "center",
-                    // marginLeft: `${row.depth * 12}px`,
                     borderBottom: "1px solid",
                     borderBottomColor: "monochrome400",
                     m: `0 0 0 ${row.depth * 24}px`,
@@ -38,108 +45,34 @@ export const GroupHeader = ({ row }: { row: Row<Observation> }) => {
                   >
                     <Icon name={row.isExpanded ? "chevrondown" : "chevronup"} />
                   </Box>
-                  {cell.render("Cell")}
+                  {type === "category" ? (
+                    <Tag
+                      tagColor={
+                        colorScale ? colorScale(cell.value) : "primaryLight"
+                      }
+                    >
+                      {cell.render("Cell")}
+                    </Tag>
+                  ) : (
+                    <Box
+                      as="span"
+                      sx={{
+                        color: textColor,
+                        bg: columnColor,
+                        textAlign:
+                          typeof cell.value === "number" ? "right" : "left", // FIXME
+                        fontWeight: textStyle,
+                      }}
+                    >
+                      {cell.render("Cell")}
+                    </Box>
+                  )}
                 </Flex>
               </th>
             )}
           </React.Fragment>
         );
       })}
-      {/* {row.cells.map((cell, i) => {
-        const {
-          type,
-          textStyle,
-          textColor,
-          columnColor,
-          colorScale,
-        } = tableColumnsMeta[i];
-        return (
-          <>
-            {cell.isGrouped && type === "category" ? (
-              <GroupTagCell
-                row={row}
-                cell={cell}
-                fontWeight={textStyle}
-                tagColor={colorScale ? colorScale(cell.value) : "primaryLight"}
-              />
-            ) : cell.isGrouped && type === "text" ? (
-              <GroupTextCell
-                row={row}
-                cell={cell}
-                styles={{
-                  color: textColor,
-                  bg: columnColor,
-                  textAlign: typeof cell.value === "number" ? "right" : "left",
-                  fontWeight: textStyle,
-                }}
-              />
-            ) : cell.isAggregated ? (
-              <PlaceceholderCell>aggregated</PlaceceholderCell>
-            ) : cell.isPlaceholder ? ( // Nothing to display
-              // Nothing to display
-              <PlaceceholderCell>placeholder</PlaceceholderCell>
-            ) : (
-              // Nothing to display
-              <PlaceceholderCell>nothing</PlaceceholderCell>
-            )}
-          </>
-        );
-      })} */}
     </>
-  );
-};
-
-const PlaceceholderCell = ({ children }: { children?: React.ReactNode }) => (
-  <Box as="td" sx={{ borderBottom: 0, fontSize: "6px" }}>
-    {children}
-  </Box>
-);
-const GroupTextCell = ({
-  row,
-  cell,
-  styles,
-}: {
-  row: Row<Observation>;
-  cell: Cell<Observation>;
-  styles: SystemStyleObject;
-}) => {
-  return (
-    <Flex
-      as="td"
-      sx={{ ...styles, width: "max-content" }}
-      {...row.getToggleRowExpandedProps()}
-      {...cell.getCellProps()}
-    >
-      <Box as="span" sx={{ width: 24, mr: 2, color: "monochrome600" }}>
-        <Icon name={row.isExpanded ? "chevrondown" : "chevronup"} />
-      </Box>
-      {cell.render("Cell")}
-    </Flex>
-  );
-};
-const GroupTagCell = ({
-  row,
-  cell,
-  fontWeight,
-  tagColor,
-}: {
-  row: Row<Observation>;
-  cell: Cell<Observation>;
-  fontWeight: string;
-  tagColor?: string;
-}) => {
-  return (
-    <Flex
-      as="td"
-      sx={{ fontWeight, width: "auto" }}
-      {...row.getToggleRowExpandedProps()}
-      {...cell.getCellProps()}
-    >
-      <Box sx={{ width: 24, mr: 2, color: "monochrome600" }}>
-        <Icon name={row.isExpanded ? "chevrondown" : "chevronup"} />
-      </Box>
-
-      <Tag tagColor={tagColor || "primaryLight"}>{cell.render("Cell")}</Tag>
-    </Flex>
   );
 };
