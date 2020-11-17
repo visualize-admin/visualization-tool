f97b97445a7aabc268571787d5600314c7d88603
Luc <luc.guillemot@gmail.com>
2020-11-17 17:43:34 +0100

    fix: don’t aria-hide data table, move to chart container

--- app/charts/shared/containers.tsx
+++ app/charts/shared/containers.tsx
@@ -4,7 +4,11 @@ import { useChartState } from "./use-chart-state";
 export const ChartContainer = ({ children }: { children: ReactNode }) => {
   const { bounds } = useChartState();
   const { width, height } = bounds;
-  return <div style={{ position: "relative", width, height }}>{children}</div>;
+  return (
+    <div aria-hidden="true" style={{ position: "relative", width, height }}>
+      {children}
+    </div>
+  );
 };
 
 export const ChartSvg = ({ children }: { children: ReactNode }) => {
