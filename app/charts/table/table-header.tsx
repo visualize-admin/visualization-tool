456b730453a74aace51f7a91ac1b69f3604f8770
Luc <luc.guillemot@gmail.com>
2020-11-17 14:33:19 +0100

    fix: remove header word breaking

--- app/charts/table/table-header.tsx
+++ app/charts/table/table-header.tsx
@@ -33,7 +33,6 @@ export const TableHeader = ({
                     textAlign: "left",
                     // textAlign:
                     //   columnComponentType === "Measure" ? "right" : "left",
-                    wordBreak: "break-word",
                     borderTop: "1px solid",
                     borderTopColor: "monochrome700",
                     borderBottom: "1px solid",
