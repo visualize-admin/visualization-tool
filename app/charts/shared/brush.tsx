85bd891c6f98046a42f79ae3c1ecf3cc96c20368
Luc <luc.guillemot@gmail.com>
2020-11-20 11:26:11 +0100

    feat: add brush component

---
+++ app/charts/shared/brush.tsx
@@ -0,0 +1,82 @@
+import { bisector } from "d3-array";
+import { brushX } from "d3-brush";
+import { select, Selection } from "d3-selection";
+import { useEffect, useRef } from "react";
+import { useFormatShortDateAuto } from "../../configurator/components/ui-helpers";
+import { Observation } from "../../domain/data";
+import { AreasState } from "../area/areas-state";
+import { LinesState } from "../line/lines-state";
+import { useChartState } from "./use-chart-state";
+
+const BRUSH_HEIGHT = 60;
+
+export const Brush = () => {
+  const ref = useRef<SVGGElement>(null);
+
+  const { xScale, getX, bounds, wide } = useChartState() as
+    | LinesState
+    | AreasState;
+
+  const brushed = ({ selection }: { selection: [number, number] }) => {
+    if (selection) {
+      const [xStart, xEnd] = selection.map((s) => xScale.invert(s));
+      const bisectDate = bisector(
+        (ds: Observation, date: Date) => getX(ds).getTime() - date.getTime()
+      ).left;
+
+      // Start date
+      const startIndex = bisectDate(wide, xStart, 1);
+      // FIXME: should it de just Index (to be encompassed by the brush)
+      const dStartLeft = wide[startIndex - 1];
+      const dStartRight = wide[startIndex] || dStartLeft;
+
+      const startClosestDatum =
+        xStart.getTime() - getX(dStartLeft).getTime() >
+        getX(dStartRight).getTime() - xStart.getTime()
+          ? dStartRight
+          : dStartLeft;
+
+      // End date
+      const endIndex = bisectDate(wide, xEnd, 1);
+      const dEndLeft = wide[endIndex - 1];
+      const dEndRight = wide[endIndex] || dEndLeft;
+      const endClosestDatum =
+        xEnd.getTime() - getX(dEndLeft).getTime() >
+        getX(dEndRight).getTime() - xEnd.getTime()
+          ? dEndRight
+          : dEndLeft;
+      // console.log("startClosestDatum", startClosestDatum);
+      // console.log("endClosestDatum", endClosestDatum);
+    }
+  };
+  const mkBrush = (g: Selection<SVGGElement, unknown, null, undefined>) => {
+    // Creates 1-Dimensional brush
+    const brush = brushX()
+      .extent([
+        [0, 30],
+        [bounds.chartWidth, BRUSH_HEIGHT],
+      ])
+      .on("brush", brushed);
+
+    // Apply brush to selected group
+    g.call(brush);
+
+    // Styling
+    g.select(".overlay").attr("fill", "moccasin");
+    g.select(".selection").attr("fill", "darkorange");
+  };
+
+  useEffect(() => {
+    const g = select(ref.current);
+    mkBrush(g as Selection<SVGGElement, unknown, null, undefined>);
+  });
+
+  return (
+    <g
+      ref={ref}
+      transform={`translate(${bounds.margins.left}, ${
+        bounds.chartHeight + bounds.margins.top
+      })`}
+    />
+  );
+};
