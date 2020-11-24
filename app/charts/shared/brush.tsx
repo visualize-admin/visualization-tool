ebd149e15768527c05f92a89e8595c191d0072e4
Luc <luc.guillemot@gmail.com>
2020-11-24 19:45:03 +0100

    feat: add BrushOrdinal

--- app/charts/shared/brush.tsx
+++ app/charts/shared/brush.tsx
@@ -1,4 +1,4 @@
-import { bisector } from "d3-array";
+import { bisectLeft, bisector, bisectRight } from "d3-array";
 import { brushX } from "d3-brush";
 import { select, Selection } from "d3-selection";
 import "d3-transition";
@@ -6,6 +6,7 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useFormatShortDateAuto } from "../../configurator/components/ui-helpers";
 import { Observation } from "../../domain/data";
 import { AreasState } from "../area/areas-state";
+import { ColumnsState } from "../column/columns-state";
 import { LinesState } from "../line/lines-state";
 import { useChartState } from "./use-chart-state";
 import { useChartTheme } from "./use-chart-theme";
@@ -14,13 +15,14 @@ import { useInteractiveFilters } from "./use-interactive-filters";
 export const HANDLE_HEIGHT = 14;
 export const BRUSH_HEIGHT = 3;
 
-export const Brush = () => {
+export const BrushTime = () => {
   const ref = useRef<SVGGElement>(null);
   const [brushedIsEnded, updateBrushEndedStatus] = useState(true);
+  const formatDateAuto = useFormatShortDateAuto();
 
   const [state, dispatch] = useInteractiveFilters();
+  const { from, to } = state.time;
 
-  const formatDateAuto = useFormatShortDateAuto();
   const {
     brushOverlayColor,
     brushSelectionColor,
@@ -28,7 +30,6 @@ export const Brush = () => {
     brushHandleFillColor,
     labelFontSize,
   } = useChartTheme();
-  const { from, to } = state.time;
   const { xEntireScale, getX, bounds, allDataWide } = useChartState() as
     | LinesState
     | AreasState;
@@ -248,7 +249,7 @@ export const Brush = () => {
         )}
       </g>
       {/* Handle Dates */}
-      <g
+      {/* <g
         transform={`translate(${bounds.margins.left}, ${
           bounds.chartHeight +
           bounds.margins.top +
@@ -267,7 +268,7 @@ export const Brush = () => {
             {formatDateAuto(to)}
           </text>
         )}
-      </g>
+      </g> */}
       {/* Date Start and End */}
 
       {/* <g
@@ -315,6 +316,236 @@ export const Brush = () => {
         // focusable="true"
         transform={`translate(${bounds.margins.left}, ${
           bounds.chartHeight + bounds.margins.top + bounds.margins.bottom / 2
+        })`}
+      />
+    </>
+  );
+};
+
+export const BrushOrdinal = () => {
+  const ref = useRef<SVGGElement>(null);
+  const [brushedIsEnded, updateBrushEndedStatus] = useState(true);
+
+  const [state, dispatch] = useInteractiveFilters();
+  const { from, to } = state.time;
+  console.log("from to initial state", from, to);
+  const {
+    brushOverlayColor,
+    brushSelectionColor,
+    brushHandleStrokeColor,
+    brushHandleFillColor,
+    labelFontSize,
+  } = useChartTheme();
+  const {
+    xEntireScale,
+    getX,
+    bounds,
+    sortedData,
+  } = useChartState() as ColumnsState;
+
+  const updateBrushStatus = (event: $FixMe) => {
+    const selection = event.selection;
+    if (!event.sourceEvent || !selection) {
+      updateBrushEndedStatus(false);
+    } else {
+      updateBrushEndedStatus(true);
+    }
+  };
+
+  const brushed = ({ selection }: { selection: [number, number] }) => {
+    updateBrushEndedStatus(false);
+    if (selection) {
+      const [xStart, xEnd] = selection;
+
+      const range = xEntireScale.domain().map(xEntireScale) as number[];
+      const startIndex = bisectLeft(range, xStart);
+      const endIndex = bisectRight(range, xEnd);
+
+      // Update interactive filters state
+      dispatch({
+        type: "ADD_TIME_FILTER",
+        value: [startIndex, endIndex - 1],
+      });
+    }
+  };
+
+  // Creates a 1-dimensional brush
+  const brush = brushX()
+    .extent([
+      [0, 0],
+      [bounds.chartWidth, BRUSH_HEIGHT],
+    ])
+    .on("start brush", brushed)
+    .on("end", updateBrushStatus);
+
+  /** Keyboard support */
+  // const moveBrushOnKeyPress = useCallback(
+  //   (event: $FixMe, handleDirection: "w" | "e") => {
+  //     updateBrushEndedStatus(false);
+
+  //     const bisectDateLeft = bisector(
+  //       (ds: Observation, date: Date) => getX(ds) - date
+  //     ).left;
+  //     const bisectDateRight = bisector(
+  //       (ds: Observation, date: Date) => getX(ds) - date
+  //     ).right;
+
+  //     if (event.keyCode === 37 && handleDirection === "w") {
+  //       // west handle, moving left
+  //       const index = bisectDateLeft(sortedData, from, 1);
+  //       const indexLeft = sortedData[index - 1];
+
+  //       if (getX(indexLeft) < to) {
+  //         // new lower than "to"
+  //         dispatch({
+  //           type: "ADD_TIME_FILTER",
+  //           value: [getX(indexLeft), to],
+  //         });
+  //       } else {
+  //         // new too high, don't do anything
+  //         dispatch({
+  //           type: "ADD_TIME_FILTER",
+  //           value: [from, to],
+  //         });
+  //       }
+  //     } else if (event.keyCode === 39 && handleDirection === "w") {
+  //       // west handle, moving right
+  //       const index = bisectDateRight(sortedData, from, 1);
+  //       const indexRight = sortedData[index];
+  //       if (getX(indexRight) < to) {
+  //         dispatch({
+  //           type: "ADD_TIME_FILTER",
+  //           value: [getX(indexRight), to],
+  //         });
+  //       } else {
+  //         dispatch({
+  //           type: "ADD_TIME_FILTER",
+  //           value: [from, to],
+  //         });
+  //       }
+  //     } else if (event.keyCode === 37 && handleDirection === "e") {
+  //       // east handle, moving left
+  //       const index = bisectDateLeft(sortedData, to, 1);
+  //       const indexLeft = sortedData[index - 1];
+
+  //       if (getX(indexLeft) > from) {
+  //         dispatch({
+  //           type: "ADD_TIME_FILTER",
+  //           value: [from, getX(indexLeft)],
+  //         });
+  //       } else {
+  //         dispatch({
+  //           type: "ADD_TIME_FILTER",
+  //           value: [from, to],
+  //         });
+  //       }
+  //     } else if (event.keyCode === 39 && handleDirection === "e") {
+  //       // east handle, moving right
+  //       const index = bisectDateRight(sortedData, to, 1);
+  //       const indexLeft = sortedData[index];
+
+  //       if (indexLeft && getX(indexLeft) > from) {
+  //         dispatch({
+  //           type: "ADD_TIME_FILTER",
+  //           value: [from, getX(indexLeft)],
+  //         });
+  //       } else {
+  //         dispatch({
+  //           type: "ADD_TIME_FILTER",
+  //           value: [from, to],
+  //         });
+  //       }
+  //     }
+  //     updateBrushEndedStatus(true);
+  //   },
+  //   [sortedData, dispatch, from, getX, to]
+  // );
+  const mkBrush = useCallback(
+    (g: Selection<SVGGElement, unknown, null, undefined>) => {
+      g.select(".overlay")
+        .attr("fill", brushOverlayColor)
+        .attr("fill-opacity", 0.9);
+      g.select(".selection")
+        .attr("fill", brushSelectionColor)
+        .attr("fill-opacity", 1)
+        .attr("stroke", "none");
+      g.selectAll(".handle")
+        .attr("fill", brushHandleFillColor)
+        .attr("stroke", brushHandleStrokeColor)
+        .attr("stroke-width", 2)
+        .style("y", `-${HANDLE_HEIGHT / 2}px`)
+        // .style("transform", `translateX(-${0}px)`)
+        .style("width", `${HANDLE_HEIGHT}px`)
+        .style("height", `${HANDLE_HEIGHT}px`)
+        .attr("rx", `${HANDLE_HEIGHT}px`);
+
+      // g.select(".handle--w")
+      //   .attr("tabindex", 0)
+      //   .on("keydown", (e: $FixMe) => moveBrushOnKeyPress(e, "w"));
+      // g.select(".handle--e")
+      //   .attr("tabindex", 0)
+      //   .on("keydown", (e: $FixMe) => moveBrushOnKeyPress(e, "e"));
+
+      // Apply brush to selected group
+      g.call(brush);
+    },
+    [
+      brush,
+      brushHandleFillColor,
+      brushHandleStrokeColor,
+      brushOverlayColor,
+      brushSelectionColor,
+      // moveBrushOnKeyPress,
+    ]
+  );
+
+  useEffect(() => {
+    const g = select(ref.current);
+    mkBrush(g as Selection<SVGGElement, unknown, null, undefined>);
+  }, [mkBrush]);
+
+  // This effect allow "snapping" to actual data points
+  // after brush is ended and interactive-filters state is updated
+  useEffect(() => {
+    const g = select(ref.current);
+    if (from && to && brushedIsEnded) {
+      const coord = [
+        xEntireScale(getX(sortedData[from])),
+        xEntireScale(getX(sortedData[to])),
+      ];
+      // @ts-ignore
+      g.transition().call(brush.move, coord);
+    }
+  }, [brush.move, from, to, xEntireScale, brushedIsEnded, getX, sortedData]);
+
+  return (
+    <>
+      {/* Selected Dates */}
+      <g
+        transform={`translate(0, ${
+          bounds.chartHeight + bounds.margins.top + bounds.margins.bottom
+        })`}
+      >
+        {from && to && (
+          <text
+            fontSize={labelFontSize}
+            textAnchor="start"
+            x={0}
+            y={0}
+            dy={labelFontSize / 2}
+          >
+            {`${getX(sortedData[from])} - ${getX(sortedData[to])}`}
+          </text>
+        )}
+      </g>
+
+      {/* Brush */}
+      <g
+        ref={ref}
+        // tabIndex={0}
+        // focusable="true"
+        transform={`translate(${bounds.margins.left}, ${
+          bounds.chartHeight + bounds.margins.top + bounds.margins.bottom
         })`}
       />
     </>
