7ebe571cdf47d5f135fa348e9f9c5759ad354d0e
Luc <luc.guillemot@gmail.com>
2020-12-01 14:26:02 +0100

    fix: remove too specific hook

--- app/configurator/config-form.tsx
+++ app/configurator/config-form.tsx
@@ -218,31 +218,31 @@ export const useActiveFieldField = ({
   };
 };
 
-export const useInteractiveFilterField = ({
-  value,
-}: {
-  value: boolean;
-}): FieldProps & {
-  value: boolean;
-  onClick: (x: string) => void;
-} => {
-  const [, dispatch] = useConfiguratorState();
+// export const useInteractiveFilterField = ({
+//   value,
+// }: {
+//   value: boolean;
+// }): FieldProps & {
+//   value: boolean;
+//   onClick: (x: string) => void;
+// } => {
+//   const [, dispatch] = useConfiguratorState();
 
-  const onClick = useCallback(() => {
-    dispatch({
-      type: "INTERACTIVE_FILTER_LEGEND_CHANGED",
-      value: !value,
-    });
-  }, [dispatch, value]);
+//   const onClick = useCallback(() => {
+//     dispatch({
+//       type: "INTERACTIVE_FILTER_LEGEND_CHANGED",
+//       value: !value,
+//     });
+//   }, [dispatch, value]);
 
-  const checked = value === true;
+//   const checked = value === true;
 
-  return {
-    value,
-    checked,
-    onClick,
-  };
-};
+//   return {
+//     value,
+//     checked,
+//     onClick,
+//   };
+// };
 
 // Specific ------------------------------------------------------------------
 export const useChartTypeSelectorField = ({
