7eb232eb19ef205cc3d478f9fea59b476cb9d15b
Luc <luc.guillemot@gmail.com>
2020-12-01 18:06:58 +0100

    feat: add time toggle in options panel

--- app/configurator/interactive-filters/interactive-filters-options.tsx
+++ app/configurator/interactive-filters/interactive-filters-options.tsx
@@ -1,14 +1,24 @@
 import { Trans } from "@lingui/macro";
-import React, { useEffect, useRef } from "react";
+import React, {
+  ChangeEvent,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useRef,
+} from "react";
 import { Box } from "theme-ui";
+import { Checkbox } from "../../components/form";
 import { DataCubeMetadata } from "../../graphql/types";
 import {
   ControlSection,
   ControlSectionContent,
   SectionTitle,
 } from "../components/chart-controls/section";
-import { getIconName } from "../components/ui-helpers";
+import get from "lodash/get";
+
 import { ConfiguratorStateDescribingChart } from "../config-types";
+import { useConfiguratorState } from "../configurator-state";
+import { toggleInteractiveFilter } from "./interactive-filters-state";
 
 export const InteractiveFiltersOptions = ({
   state,
@@ -39,29 +49,59 @@ export const InteractiveFiltersOptions = ({
       />
     );
   } else if (activeField === "time") {
-    const isActive = chartConfig.interactiveFilters.time.active;
+    // const isActive = chartConfig.interactiveFilters.time.active;
     return (
-      <div
-        key={`control-panel-table-column-${activeField}`}
-        role="tabpanel"
-        id={`control-panel-table-column-${activeField}`}
-        aria-labelledby={`tab-${activeField}`}
-        ref={panelRef}
-        tabIndex={-1}
-      >
-        <ControlSection>
-          <SectionTitle disabled={isActive} iconName="filter">
-            <Trans id="controles.section.interactiveFilters.time">Filter</Trans>
-          </SectionTitle>
-          <ControlSectionContent side="right">woih</ControlSectionContent>
-        </ControlSection>
-      </div>
+      <ControlSection>
+        <SectionTitle iconName="filter">
+          <Trans id="controls.section.interactiveFilters.time">Filter</Trans>
+        </SectionTitle>
+        <ControlSectionContent side="right">
+          <InteractiveFiltersTimeToggle
+            label={
+              <Trans id="controls.interactiveFilters.time.toggleTimeFilter">
+                Show time filter
+              </Trans>
+            }
+            path="time"
+            defaultChecked={false}
+            disabled={false}
+          ></InteractiveFiltersTimeToggle>
+        </ControlSectionContent>
+      </ControlSection>
     );
   } else {
     return <Box>NOTHING</Box>;
   }
 };
 
+// Time
+const InteractiveFiltersTimeToggle = ({
+  label,
+  path,
+  defaultChecked,
+  disabled = false,
+}: // metaData,
+{
+  label: string | ReactNode;
+  path: "legend" | "time";
+  defaultChecked?: boolean;
+  disabled?: boolean;
+  // metaData: DataCubeMetadata;
+}) => {
+  const fieldProps = useInteractiveFiltersToggle({
+    path,
+  });
+
+  return (
+    <Checkbox
+      disabled={disabled}
+      label={label}
+      {...fieldProps}
+      checked={fieldProps.checked ?? defaultChecked}
+    ></Checkbox>
+  );
+};
+// Legend
 const InteractiveFiltersLegendOptions = ({
   isActive,
 }: {
@@ -70,7 +110,7 @@ const InteractiveFiltersLegendOptions = ({
   return (
     <ControlSection>
       <SectionTitle disabled={isActive} iconName="filter">
-        <Trans id="controles.section.interactiveFilters.legend">
+        <Trans id="controls.section.interactiveFilters.legend">
           Interactive Legend
         </Trans>
       </SectionTitle>
@@ -83,4 +123,38 @@ const InteractiveFiltersLegendOptions = ({
       </ControlSectionContent>
     </ControlSection>
   );
+};
+
+const useInteractiveFiltersToggle = ({ path }: { path: "legend" | "time" }) => {
+  const [state, dispatch] = useConfiguratorState();
+  const onChange = useCallback<(e: ChangeEvent<HTMLInputElement>) => void>(
+    (e) => {
+      if (
+        state.state === "DESCRIBING_CHART" &&
+        state.chartConfig.chartType === "line"
+      ) {
+        const newIFConfig = toggleInteractiveFilter(
+          state.chartConfig.interactiveFilters,
+          { path, value: e.currentTarget.checked }
+        );
+
+        dispatch({
+          type: "INTERACTIVE_FILTER_CHANGED",
+          value: newIFConfig,
+        });
+      }
+    },
+    [dispatch, path, state]
+  );
+  const stateValue =
+    state.state === "DESCRIBING_CHART"
+      ? get(state, `chartConfig.interactiveFilters.${path}.active`, "")
+      : "";
+  console.log(stateValue);
+  const checked = stateValue ? stateValue : false;
+  return {
+    name: path,
+    checked,
+    onChange,
+  };
 };
