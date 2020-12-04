c133738ce48f97f11a23f1e03adf93ce099d7630
Luc <luc.guillemot@gmail.com>
2020-12-04 09:12:49 +0100

    feat: add config action to add/remove dimension filter

--- app/configurator/interactive-filters/interactive-filters-actions.tsx
+++ app/configurator/interactive-filters/interactive-filters-actions.tsx
@@ -1,12 +1,16 @@
 import get from "lodash/get";
 import { ChangeEvent, useCallback } from "react";
 import { useConfiguratorState } from "../configurator-state";
-import { toggleInteractiveFilter } from "./interactive-filters-state";
+import { InteractveFilterType } from "./interactive-filters-configurator";
+import {
+  toggleInteractiveFilter,
+  toggleInteractiveFilterDataDimension,
+} from "./interactive-filters-state";
 
 export const useInteractiveFiltersToggle = ({
   path,
 }: {
-  path: "legend" | "time";
+  path: InteractveFilterType;
 }) => {
   const [state, dispatch] = useConfiguratorState();
   const onChange = useCallback<(e: ChangeEvent<HTMLInputElement>) => void>(
@@ -17,7 +21,7 @@ export const useInteractiveFiltersToggle = ({
         state.chartConfig.chartType !== "table"
       ) {
         const newIFConfig = toggleInteractiveFilter(
-          state.chartConfig.interactiveFilters,
+          state.chartConfig.interactiveFiltersConfig,
           { path, value: e.currentTarget.checked }
         );
 
@@ -31,13 +35,51 @@ export const useInteractiveFiltersToggle = ({
   );
   const stateValue =
     state.state === "DESCRIBING_CHART"
-      ? get(state, `chartConfig.interactiveFilters.${path}.active`, "")
+      ? get(state, `chartConfig.interactiveFiltersConfig.${path}.active`, "")
       : "";
 
   const checked = stateValue ? stateValue : false;
   return {
     name: path,
     checked,
+    onChange,
+  };
+};
+
+// Checkbox to remove from filters, add to interactive dataFilters
+// init with value selected in config filter
+export const useInteractiveDataFilterDimensionToggle = () => {
+  const [state, dispatch] = useConfiguratorState();
+  const onChange = useCallback<(e: ChangeEvent<HTMLInputElement>) => void>(
+    (e) => {
+      console.log(e.currentTarget.value);
+      if (
+        state.state === "DESCRIBING_CHART" &&
+        // All charts except "table" can have interactive filters
+        state.chartConfig.chartType !== "table"
+      ) {
+        const newIFConfig = toggleInteractiveFilterDataDimension(
+          state.chartConfig.interactiveFiltersConfig,
+          e.currentTarget.value
+        );
+        console.log("newIFConfig", newIFConfig);
+        dispatch({
+          type: "INTERACTIVE_FILTER_CHANGED",
+          value: newIFConfig,
+        });
+      }
+    },
+    [dispatch, state]
+  );
+  // const stateValue =state.  .interactiveFiltersConfig.dataFilters.componentIris.inclu
+  // state.state === "DESCRIBING_CHART"
+  //   ? get(state, `chartConfig.interactiveFiltersConfig.dataFilters.componentIris`, "")
+  //   : "";
+
+  // const checked = stateValue ? stateValue : false;
+  return {
+    name: "dataFilters",
+    checked: true,
     onChange,
   };
 };
--- app/configurator/interactive-filters/interactive-filters-state.tsx
+++ app/configurator/interactive-filters/interactive-filters-state.tsx
@@ -1,15 +1,51 @@
 import produce from "immer";
 import { InteractiveFiltersConfig } from "../config-types";
+import { InteractveFilterType } from "./interactive-filters-configurator";
 
 export const toggleInteractiveFilter = produce(
   (
     IFConfig: InteractiveFiltersConfig,
-    { path, value }: { path: "legend" | "time"; value: boolean }
+    { path, value }: { path: InteractveFilterType; value: boolean }
   ): InteractiveFiltersConfig => {
     if (!IFConfig[path]) {
       return IFConfig;
     }
     IFConfig[path].active = value;
     return IFConfig;
+  }
+);
+export const toggleInteractiveFilterDataDimension = produce(
+  (
+    IFConfig: InteractiveFiltersConfig,
+    cIri: string
+  ): InteractiveFiltersConfig => {
+    console.log("cIri", cIri);
+    console.log(
+      "IFConfig.dataFilters.componentIris",
+      IFConfig.dataFilters.componentIris
+    );
+    if (!IFConfig.dataFilters || !IFConfig.dataFilters.componentIris) {
+      return IFConfig;
+    }
+    if (IFConfig.dataFilters.componentIris.includes(cIri)) {
+      const newComponentIris = IFConfig.dataFilters.componentIris.filter(
+        (d) => d !== cIri
+      );
+      const newDataFilters = {
+        ...IFConfig.dataFilters,
+        componentIris: newComponentIris,
+      };
+      return { ...IFConfig, dataFilters: newDataFilters };
+    } else if (!IFConfig.dataFilters.componentIris.includes(cIri)) {
+      const newComponentIris = [...IFConfig.dataFilters.componentIris, cIri];
+      const newDataFilters = {
+        ...IFConfig.dataFilters,
+        componentIris: newComponentIris,
+      };
+      console.log("add filter", newDataFilters);
+      return { ...IFConfig, dataFilters: newDataFilters };
+    } else {
+      return IFConfig;
+    }
   }
 );
