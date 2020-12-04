d8e6219264a68dbafaa7edeb1c104999cb7d3dd9
Luc <luc.guillemot@gmail.com>
2020-12-04 11:33:31 +0100

    feat: add interactive dropdowns

---
+++ app/charts/shared/interactive-data-filters.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import { Box, Flex } from "theme-ui";
+import { Select } from "../../components/form";
+import { Loading } from "../../components/hint";
+import {
+  InteractiveFiltersDataConfig,
+  useConfiguratorState,
+} from "../../configurator";
+import { useDimensionValuesQuery } from "../../graphql/query-hooks";
+import { useLocale } from "../../locales/use-locale";
+import { useInteractiveFilters } from "./use-interactive-filters";
+
+export const InteractiveDataFilters = ({
+  dataFilters,
+}: {
+  dataFilters: InteractiveFiltersDataConfig;
+}) => {
+  const [{ dataSet }] = useConfiguratorState();
+
+  return (
+    <Box sx={{ my: 4 }}>
+      {dataSet &&
+        dataFilters.componentIris.map((d, i) => (
+          <DataFilterDropdown key={i} dataSetIri={dataSet} dimensionIri={d} />
+        ))}
+    </Box>
+  );
+};
+
+const DataFilterDropdown = ({
+  dimensionIri,
+  dataSetIri,
+}: {
+  dimensionIri: string;
+  dataSetIri: string;
+}) => {
+  const [state, dispatch] = useInteractiveFilters();
+  const locale = useLocale();
+  const [{ data }] = useDimensionValuesQuery({
+    variables: { dimensionIri, locale, dataCubeIri: dataSetIri },
+  });
+  console.log("interactive state", state.dataFilters);
+  const setDataFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch({
+      type: "ADD_DATA_FILTER",
+      value: { dimensionIri, dimensionValueIri: e.currentTarget.value },
+    });
+  };
+  if (data?.dataCubeByIri?.dimensionByIri) {
+    const dimension = data?.dataCubeByIri?.dimensionByIri;
+
+    return (
+      <Flex
+        sx={{
+          position: "relative",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          flexWrap: "wrap",
+          minHeight: "20px",
+        }}
+      >
+        <Select
+          id="interactiveDataFilter"
+          label={dimension.label}
+          options={dimension.values.map((v) => ({
+            label: v.label,
+            value: v.label,
+          }))}
+          // value={activeSortingType}
+          disabled={false}
+          onChange={setDataFilter}
+        />
+      </Flex>
+    );
+  } else {
+    return <Loading />;
+  }
+};
