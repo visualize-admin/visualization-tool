3da39667c6fa347b97e8f5c40d9714a9bf887a19
Luc <luc.guillemot@gmail.com>
2020-12-08 19:49:53 +0100

    feat: init data filters in component

--- app/charts/shared/interactive-data-filters.tsx
+++ app/charts/shared/interactive-data-filters.tsx
@@ -1,29 +1,73 @@
 import * as React from "react";
-import { Box, Flex } from "theme-ui";
+import { useEffect } from "react";
+import { Flex } from "theme-ui";
 import { Select } from "../../components/form";
 import { Loading } from "../../components/hint";
 import {
+  ConfiguratorStateConfiguringChart,
+  ConfiguratorStateDescribingChart,
+  ConfiguratorStatePublishing,
+  ConfiguratorStateSelectingChartType,
+  FilterValueSingle,
   InteractiveFiltersDataConfig,
-  useConfiguratorState,
 } from "../../configurator";
 import { useDimensionValuesQuery } from "../../graphql/query-hooks";
 import { useLocale } from "../../locales/use-locale";
 import { useInteractiveFilters } from "./use-interactive-filters";
 
 export const InteractiveDataFilters = ({
+  state,
   dataFiltersConfig,
 }: {
+  state:
+    | ConfiguratorStateConfiguringChart
+    | ConfiguratorStateDescribingChart
+    | ConfiguratorStatePublishing
+    | ConfiguratorStateSelectingChartType;
   dataFiltersConfig: InteractiveFiltersDataConfig;
 }) => {
-  const [{ dataSet }] = useConfiguratorState();
+  const [interactiveFiltersState, dispatch] = useInteractiveFilters();
+  const interactiveFiltersIsActive =
+    state.chartConfig.chartType !== "table" &&
+    state.chartConfig.interactiveFiltersConfig.dataFilters.active;
+
+  useEffect(() => {
+    if (
+      (state.state === "CONFIGURING_CHART" ||
+        state.state === "DESCRIBING_CHART") &&
+      state.chartConfig.chartType !== "table" &&
+      interactiveFiltersIsActive &&
+      interactiveFiltersState.dataFilters &&
+      Object.keys(interactiveFiltersState.dataFilters).length === 0 &&
+      interactiveFiltersState.dataFilters.constructor === Object
+    ) {
+      // Use the filters defined in the editor as defaults for the interactive filters
+      const initialInteractiveFilters = state.chartConfig.interactiveFiltersConfig.dataFilters.componentIris.reduce(
+        (f, dimIri) => ({
+          ...f,
+          [dimIri]: state.chartConfig.filters[dimIri],
+        }),
+        {} as FilterValueSingle
+      );
+
+      dispatch({
+        type: "INIT_DATA_FILTER",
+        value: (initialInteractiveFilters as unknown) as FilterValueSingle,
+      });
+    }
+  }, []);
 
   return (
-    <Box>
-      {dataSet &&
+    <Flex sx={{ justifyContent: "space-between" }}>
+      {state.dataSet &&
         dataFiltersConfig.componentIris.map((d, i) => (
-          <DataFilterDropdown key={i} dataSetIri={dataSet} dimensionIri={d} />
+          <DataFilterDropdown
+            key={i}
+            dataSetIri={state.dataSet}
+            dimensionIri={d}
+          />
         ))}
-    </Box>
+    </Flex>
   );
 };
 
@@ -34,7 +78,7 @@ const DataFilterDropdown = ({
   dimensionIri: string;
   dataSetIri: string;
 }) => {
-  const [, dispatch] = useInteractiveFilters();
+  const [state, dispatch] = useInteractiveFilters();
   const locale = useLocale();
   const [{ data }] = useDimensionValuesQuery({
     variables: { dimensionIri, locale, dataCubeIri: dataSetIri },
@@ -46,17 +90,22 @@ const DataFilterDropdown = ({
       value: { dimensionIri, dimensionValueIri: e.currentTarget.value },
     });
   };
-  if (data?.dataCubeByIri?.dimensionByIri) {
+  if (
+    data?.dataCubeByIri?.dimensionByIri &&
+    Object.keys(state.dataFilters).length !== 0
+  ) {
     const dimension = data?.dataCubeByIri?.dimensionByIri;
 
     return (
       <Flex
         sx={{
-          position: "relative",
-          justifyContent: "flex-start",
-          alignItems: "flex-start",
-          flexWrap: "wrap",
-          minHeight: "20px",
+          mr: 3,
+          width: "100%",
+          flex: "1 1 100%",
+          ":last-of-type": {
+            mr: 0,
+          },
+          " > div": { width: "100%" },
         }}
       >
         <Select
@@ -66,6 +115,7 @@ const DataFilterDropdown = ({
             label: v.label,
             value: v.value,
           }))}
+          value={state.dataFilters[dimension.iri].value}
           disabled={false}
           onChange={setDataFilter}
         />
