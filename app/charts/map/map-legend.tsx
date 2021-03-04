d6918e60c73c3934507d54a22e87fe757daf812e
Luc <luc.guillemot@gmail.com>
2021-03-04 18:31:06 +0100

    fix: legend as color ramp

--- app/charts/map/map-legend.tsx
+++ app/charts/map/map-legend.tsx
@@ -1,51 +1,81 @@
-import { interpolateOranges } from "d3";
+import { interpolateOranges, scaleLinear } from "d3";
 import * as React from "react";
 import { useEffect, useRef } from "react";
-import { Box, Text } from "theme-ui";
-import { getColorInterpolator } from "../../configurator/components/ui-helpers";
+import { Box, Flex, Text } from "theme-ui";
+import {
+  getColorInterpolator,
+  useFormatNumber,
+} from "../../configurator/components/ui-helpers";
 import { useChartState } from "../shared/use-chart-state";
 import { MapState } from "./map-state";
 
-export const MapLegend = () => {
-  const { paletteType, palette, nbSteps } = useChartState() as MapState;
+export const MapLegend = ({ legendTitle }: { legendTitle?: string }) => {
+  const {
+    paletteType,
+    palette,
+    nbSteps,
+    dataDomain,
+  } = useChartState() as MapState;
+  const formatNumber = useFormatNumber();
 
+  const WIDTH = 256;
+  const RAMP_HEIGHT = 12;
+  // const linearScale = scaleLinear().domain(dataDomain).range([0, WIDTH]);
   return (
-    <Box sx={{ m: 4 }}>
-      <Text>Mesaure</Text>
-      <ColorRamp
-        colorInterpolator={getColorInterpolator(palette)}
-        nbSteps={paletteType === "discrete" ? nbSteps : 512}
-        height={50}
-      />
-    </Box>
+    <Flex
+      sx={{
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        m: 4,
+        width: WIDTH,
+      }}
+    >
+      {legendTitle && <Text variant="meta">{legendTitle}</Text>}
+      {paletteType === "continuous" && (
+        <>
+          <Box sx={{ my: 1, width: WIDTH, height: RAMP_HEIGHT }}>
+            <ColorRamp
+              colorInterpolator={getColorInterpolator(palette)}
+              nbSteps={WIDTH}
+              width={WIDTH}
+              height={RAMP_HEIGHT}
+            />
+          </Box>
+          <Flex sx={{ justifyContent: "space-between" }}>
+            <Text variant="meta">{formatNumber(dataDomain[0])}</Text>
+            <Text variant="meta">{formatNumber(dataDomain[1])}</Text>
+          </Flex>
+        </>
+      )}
+      {paletteType === "discrete" && <Box></Box>}
+    </Flex>
   );
 };
 
 const ColorRamp = ({
   colorInterpolator = interpolateOranges,
-  nbSteps = 512,
-  width = 288,
-  height = 40,
+  nbSteps,
+  width,
+  height,
 }: {
   colorInterpolator: (t: number) => string;
   nbSteps: number;
-  width?: number;
-  height?: number;
+  width: number;
+  height: number;
 }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  // Clear by returning a function from usEEFFECT?
+
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas && canvas.getContext("2d");
     if (canvas && context) {
-      canvas.style.width = `${width}px`;
-      canvas.style.height = `${height}px`;
+      context.clearRect(0, 0, width, height);
       canvas.style.imageRendering = "-moz-crisp-edges";
       canvas.style.imageRendering = "pixelated";
-      context.clearRect(0, 0, width, height);
+
       for (let i = 0; i < nbSteps; ++i) {
         context.fillStyle = colorInterpolator(i / (nbSteps - 1));
-        context.fillRect((width / nbSteps) * i, 0, width / nbSteps, height);
+        context.fillRect(i, 0, 1, height);
       }
     }
   });
