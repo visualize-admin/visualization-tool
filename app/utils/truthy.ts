ee7df82d6d9fc259a25562bdee1adc5a392950c7
Patrick Browne <pt.browne@gmail.com>
2021-10-20 10:41:56 +0200

    refactor: Clarity

---
+++ app/utils/truthy.ts
@@ -0,0 +1,7 @@
+type Truthy<T> = T extends false | '' | 0 | null | undefined ? never : T // from lodash
+
+function truthy<T>(value: T): value is Truthy<T> {
+  return !!value
+}
+
+export default truthy
