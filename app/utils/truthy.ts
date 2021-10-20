c7013104b3fc5270a67c99b4497a282a2c9d5175
Patrick Browne <pt.browne@gmail.com>
2021-10-20 16:26:35 +0200

    docs: Add docstring to truthy

--- app/utils/truthy.ts
+++ app/utils/truthy.ts
@@ -1,7 +1,14 @@
-type Truthy<T> = T extends false | '' | 0 | null | undefined ? never : T // from lodash
+type Truthy<T> = T extends false | "" | 0 | null | undefined ? never : T; // from lodash
 
+/**
+ * Enables type narrowing through Array::filter
+ *
+ * @example
+ * const a = [1, undefined].filter(Boolean) // here the type of a is (number | undefined)[]
+ * const b = [1, undefined].filter(truthy) // here the type of b is number[]
+ */
 function truthy<T>(value: T): value is Truthy<T> {
-  return !!value
+  return !!value;
 }
 
-export default truthy
+export default truthy;
