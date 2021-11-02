7652a4293bab6279ceec334abcc2a47232760208
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2021-11-02 13:09:30 +0100

    fix: Consider user-selected color in line and area charts when selection contains only one element

--- app/charts/line/lines.tsx
+++ app/charts/line/lines.tsx
@@ -1,9 +1,9 @@
-import { useChartState } from "../shared/use-chart-state";
 import { line } from "d3";
-import { Observation } from "../../domain/data";
-import { LinesState } from "./lines-state";
-import { useTheme } from "../../themes";
 import { Fragment, memo } from "react";
+import { Observation } from "../../domain/data";
+import { useTheme } from "../../themes";
+import { useChartState } from "../shared/use-chart-state";
+import { LinesState } from "./lines-state";
 
 export const Lines = () => {
   const { getX, xScale, getY, yScale, grouped, colors, bounds } =
@@ -23,11 +23,7 @@ export const Lines = () => {
             <Line
               key={index}
               path={lineGenerator(observation[1]) as string}
-              color={
-                Array.from(grouped).length > 1
-                  ? colors(observation[0])
-                  : theme.colors.primary
-              }
+              color={colors(observation[0])}
             />
           </Fragment>
         );
