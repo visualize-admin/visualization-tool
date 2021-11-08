96c0bf074820b7e1db56ebefcc561014ee05fdca
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2021-11-08 17:15:23 +0100

    feat: Replace second chart on the homepage

--- app/homepage/examples.tsx
+++ app/homepage/examples.tsx
@@ -32,7 +32,7 @@ export const Examples = ({
       <HomepageSection>{headline}</HomepageSection>
       <Example headline={example1Headline} description={example1Description}>
         <ChartPublished
-          dataSet="https://environment.ld.admin.ch/foen/ubd0037/3/"
+          dataSet="https://environment.ld.admin.ch/foen/ubd0037/2/"
           meta={{
             title: {
               de: "Lärmbelastung durch Verkehr",
@@ -133,293 +133,73 @@ export const Examples = ({
         reverse
       >
         <ChartPublished
-          dataSet="https://environment.ld.admin.ch/foen/ubd0066/3/"
+          dataSet="https://culture.ld.admin.ch/sfa/StateAccounts_Office/3/"
           meta={{
             title: {
-              de: "Schwermetallbelastung des Bodens",
-              fr: "Pollution des sols par les métaux lourds",
-              it: "Carico di metalli pesanti nel suolo",
-              en: "Heavy metal soil contamination",
+              de: "Entwicklung der Staatsrechnungen nach Ämtern",
+              en: "Evolution of state accounts by office",
+              fr: "Evolution des comptes d'état par office",
+              it: "Evoluzione dei conti dello stato per ufficio",
             },
             description: {
               de: "",
+              en: "",
               fr: "",
               it: "",
-              en: "",
             },
           }}
           chartConfig={{
-            chartType: "column",
             fields: {
               x: {
-                componentIri:
-                  "https://environment.ld.admin.ch/foen/ubd0066/samplingperiod",
+                componentIri: "http://www.w3.org/2006/time#Year",
               },
               y: {
-                componentIri:
-                  "https://environment.ld.admin.ch/foen/ubd0066/wert",
+                componentIri: "http://schema.org/amount",
               },
               segment: {
-                componentIri:
-                  "https://environment.ld.admin.ch/foen/ubd0066/station",
+                type: "stacked",
                 palette: "category10",
-                type: "grouped",
                 sorting: {
                   sortingType: "byDimensionLabel",
                   sortingOrder: "asc",
                 },
                 colorMapping: {
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/1_1":
-                    "#7f7f7f",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/2_1":
-                    "#bcbd22",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/3_1":
-                    "#17becf",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/4_1":
-                    "#ff7f0e",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/5_1":
-                    "#d62728",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/6_1":
-                    "#9467bd",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/7_1":
-                    "#8c564b",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/8_1":
-                    "#e377c2",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/9_1":
-                    "#bcbd22",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/10_1":
-                    "#7f7f7f",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/11_1":
-                    "#bcbd22",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/12_1":
-                    "#17becf",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/13_1":
+                  "https://culture.ld.admin.ch/sfa/StateAccounts_Office/3/OperationCharacter/OC1":
                     "#1f77b4",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/14_1":
+                  "https://culture.ld.admin.ch/sfa/StateAccounts_Office/3/OperationCharacter/OC2":
                     "#ff7f0e",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/15_1":
-                    "#2ca02c",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/16_1":
-                    "#d62728",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/17_1":
-                    "#9467bd",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/18_1":
-                    "#8c564b",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/19_1":
-                    "#e377c2",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/20_1":
-                    "#bcbd22",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/21_1":
-                    "#17becf",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/22_1":
-                    "#1f77b4",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/23_1":
-                    "#ff7f0e",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/24_1":
-                    "#2ca02c",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/25_1":
-                    "#d62728",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/26_1":
-                    "#9467bd",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/27_1":
-                    "#8c564b",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/28_1":
-                    "#e377c2",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/29_1":
-                    "#7f7f7f",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/30_1":
-                    "#17becf",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/31_1":
-                    "#1f77b4",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/32_1":
-                    "#ff7f0e",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/33_1":
-                    "#2ca02c",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/34_1":
-                    "#d62728",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/35_1":
-                    "#9467bd",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/36_1":
-                    "#8c564b",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/37_1":
-                    "#e377c2",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/38_1":
-                    "#7f7f7f",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/39_1":
-                    "#bcbd22",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/40_1":
-                    "#1f77b4",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/41_1":
-                    "#ff7f0e",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/42_1":
-                    "#2ca02c",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/42_2":
-                    "#d62728",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/43_1":
-                    "#9467bd",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/44_1":
-                    "#8c564b",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/45_1":
-                    "#e377c2",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/46_1":
-                    "#7f7f7f",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/47_1":
-                    "#bcbd22",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/48_1":
-                    "#17becf",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/49_1":
-                    "#1f77b4",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/50_1":
-                    "#2ca02c",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/51_1":
-                    "#d62728",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/52_1":
-                    "#bcbd22",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/53_1":
-                    "#8c564b",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/54_1":
-                    "#e377c2",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/55_1":
-                    "#7f7f7f",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/56_1":
-                    "#bcbd22",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/57_1":
-                    "#17becf",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/58_1":
-                    "#1f77b4",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/59_1":
-                    "#ff7f0e",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/59_2":
-                    "#2ca02c",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/60_1":
-                    "#9467bd",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/61_1":
-                    "#8c564b",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/62_1":
-                    "#e377c2",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/63_1":
-                    "#7f7f7f",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/64_1":
-                    "#bcbd22",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/65_1":
-                    "#17becf",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/66_1":
-                    "#1f77b4",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/67_1":
-                    "#ff7f0e",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/68_1":
-                    "#2ca02c",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/69_1":
-                    "#d62728",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/70_1":
-                    "#8c564b",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/71_1":
-                    "#e377c2",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/72_1":
-                    "#7f7f7f",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/73_1":
-                    "#bcbd22",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/74_1":
-                    "#17becf",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/75_1":
-                    "#1f77b4",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/76_1":
-                    "#ff7f0e",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/77_1":
-                    "#2ca02c",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/78_1":
-                    "#d62728",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/79_1":
-                    "#9467bd",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/80_1":
-                    "#e377c2",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/81_1":
-                    "#7f7f7f",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/82_1":
-                    "#bcbd22",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/83_1":
-                    "#17becf",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/84_1":
-                    "#1f77b4",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/85_1":
-                    "#ff7f0e",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/86_1":
-                    "#2ca02c",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/87_1":
-                    "#d62728",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/88_1":
-                    "#9467bd",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/89_1":
-                    "#8c564b",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/90_1":
-                    "#7f7f7f",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/91_1":
-                    "#bcbd22",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/92_1":
-                    "#17becf",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/92_2":
-                    "#1f77b4",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/93_1":
-                    "#ff7f0e",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/94_1":
-                    "#2ca02c",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/95_1":
-                    "#d62728",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/96_1":
-                    "#9467bd",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/97_1":
-                    "#8c564b",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/98_1":
-                    "#e377c2",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/99_1":
-                    "#7f7f7f",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/100_1":
-                    "#1f77b4",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/101_1":
-                    "#ff7f0e",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/102_1":
-                    "#2ca02c",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/103_1":
-                    "#d62728",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/104_1":
-                    "#9467bd",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/105_1":
-                    "#8c564b",
-                  "https://environment.ld.admin.ch/foen/ubd0066/Station/106_1":
-                    "#e377c2",
                 },
+                componentIri:
+                  "https://culture.ld.admin.ch/sfa/StateAccounts_Office/operationcharacter",
               },
             },
-            interactiveFiltersConfig: {
-              legend: {
-                active: true,
-                componentIri: "",
+            filters: {
+              "https://culture.ld.admin.ch/sfa/StateAccounts_Office/office": {
+                type: "single",
+                value:
+                  "https://culture.ld.admin.ch/sfa/StateAccounts_Office/3/Office/O7",
               },
+            },
+            chartType: "area",
+            interactiveFiltersConfig: {
               time: {
-                active: false,
+                active: true,
                 presets: {
-                  to: "",
-                  from: "",
+                  to: "2013-12-31T23:00:00.000Z",
+                  from: "1950-12-31T23:00:00.000Z",
                   type: "range",
                 },
+                componentIri: "",
+              },
+              legend: {
+                active: true,
                 componentIri: "",
               },
               dataFilters: {
                 active: true,
                 componentIris: [
-                  "https://environment.ld.admin.ch/foen/ubd0066/messparameter",
+                  "https://culture.ld.admin.ch/sfa/StateAccounts_Office/office",
                 ],
-              },
-            },
-            filters: {
-              "https://environment.ld.admin.ch/foen/ubd0066/landuse": {
-                type: "single",
-                value:
-                  "https://environment.ld.admin.ch/foen/ubd0066/Landuse/10",
-              },
-              "https://environment.ld.admin.ch/foen/ubd0066/messparameter": {
-                type: "single",
-                value: "https://ld.admin.ch/cube/dimension/el01/cu",
               },
             },
           }}
