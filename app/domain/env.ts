8914b81089a154466fd39f5419f40656c5cf7405
Jeremy Stucki <jeremy@interactivethings.com>
2021-11-10 13:22:20 +0100

    refactor: Rename env variables for clarity

--- app/domain/env.ts
+++ app/domain/env.ts
@@ -1,43 +1,49 @@
 declare global {
   interface Window {
-    __runtimeEnv__: Record<string, string | undefined>;
+    __clientEnv__: Record<string, string | undefined>;
   }
 }
 
-const runtimeEnv =
-  typeof window !== "undefined" ? window.__runtimeEnv__ : undefined;
+/**
+ * Client and server-side **RUNTIME** variables
+ *
+ * These values are exposed in pages/_document.tsx to the browser or read from process.env on the server-side.
+ * Note: we can't destructure process.env because it's mangled in the Next.js runtime
+ */
 
-// These values are exposed in pages/_document.tsx to the browser or read from process.env on the server-side.
-// Note: we can't destructure process.env because it's mangled in the Next.js runtime
+const clientEnv =
+  typeof window !== "undefined" ? window.__clientEnv__ : undefined;
 
 export const PUBLIC_URL = (
-  runtimeEnv?.PUBLIC_URL ??
+  clientEnv?.PUBLIC_URL ??
   process.env.PUBLIC_URL ??
   ""
 ).replace(/\/$/, "");
 
 export const SPARQL_ENDPOINT =
-  runtimeEnv?.SPARQL_ENDPOINT ??
+  clientEnv?.SPARQL_ENDPOINT ??
   process.env.SPARQL_ENDPOINT ??
   "https://int.lindas.admin.ch/query";
 
 export const SPARQL_EDITOR =
-  runtimeEnv?.SPARQL_EDITOR ?? process.env.SPARQL_EDITOR;
+  clientEnv?.SPARQL_EDITOR ?? process.env.SPARQL_EDITOR;
 
 export const GRAPHQL_ENDPOINT =
-  runtimeEnv?.GRAPHQL_ENDPOINT ??
-  process.env.GRAPHQL_ENDPOINT ??
-  "/api/graphql";
+  clientEnv?.GRAPHQL_ENDPOINT ?? process.env.GRAPHQL_ENDPOINT ?? "/api/graphql";
 
 export const GA_TRACKING_ID =
-  runtimeEnv?.GA_TRACKING_ID ?? process.env.GA_TRACKING_ID;
+  clientEnv?.GA_TRACKING_ID ?? process.env.GA_TRACKING_ID;
 
-// Server-side-only values (not exposed through window)
+/**
+ * Server-side-only **RUNTIME** variables (not exposed through window)
+ */
 
 export const DATABASE_URL = process.env.DATABASE_URL;
 
-// Build-time env vars
+/**
+ * Variables set at **BUILD TIME** through `NEXT_PUBLIC_*` variables. Available on the client and server.
+ */
 
-export const VERSION = process.env.NEXT_PUBLIC_VERSION;
-export const COMMIT = process.env.NEXT_PUBLIC_COMMIT;
-export const GITHUB_REPO = process.env.NEXT_PUBLIC_GITHUB_REPO;
+export const BUILD_VERSION = process.env.NEXT_PUBLIC_VERSION;
+export const BUILD_COMMIT = process.env.NEXT_PUBLIC_COMMIT;
+export const BUILD_GITHUB_REPO = process.env.NEXT_PUBLIC_GITHUB_REPO;
