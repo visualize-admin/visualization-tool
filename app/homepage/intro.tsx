9bfba59b037a64a5a2439616ca129fea62f9c61c
Patrick Browne <pt.browne@gmail.com>
2021-11-26 08:55:30 +0100

    feat: Remove icons in homepage intro

--- app/homepage/intro.tsx
+++ app/homepage/intro.tsx
@@ -2,33 +2,6 @@ import NextLink from "next/link";
 import React, { ReactNode } from "react";
 import { Box, Button, Text } from "theme-ui";
 import { HintRed } from "../components/hint";
-import { default as IconAreaChart } from "../icons/components/IcChartArea";
-import { default as IconBarChart } from "../icons/components/IcChartBar";
-import { default as IconColumnChart } from "../icons/components/IcChartColumn";
-import { default as IconLineChart } from "../icons/components/IcChartLine";
-import { default as IconPieChart } from "../icons/components/IcChartPie";
-import { default as IconScatterplot } from "../icons/components/IcChartScatterplot";
-import { default as IconFilter } from "../icons/components/IcFilter";
-import { default as IconSegment } from "../icons/components/IcSegments";
-import { default as IconTable } from "../icons/components/IcTable";
-import { default as IconText } from "../icons/components/IcText";
-import { default as IconX } from "../icons/components/IcXAxis";
-import { default as IconY } from "../icons/components/IcYAxis";
-
-const ICONS = [
-  { Icon: IconX, color: "#375172" },
-  { Icon: IconScatterplot, color: "#32B8DF" },
-  { Icon: IconColumnChart, color: "#F9C16E" },
-  { Icon: IconPieChart, color: "#F38B3C" },
-  { Icon: IconAreaChart, color: "#008F85" },
-  { Icon: IconLineChart, color: "#C97146" },
-  { Icon: IconTable, color: "#928D88" },
-  { Icon: IconY, color: "#8D5A54" },
-  { Icon: IconSegment, color: "#8D5A54" },
-  { Icon: IconFilter, color: "#375172" },
-  { Icon: IconText, color: "#32B8DF" },
-  { Icon: IconBarChart, color: "#008F85" },
-];
 
 export const Intro = ({
   hint,
@@ -50,62 +23,14 @@ export const Intro = ({
       </Box>
       <Box
         sx={{
-          pb: [7, 8],
+          py: 6,
           margin: "0 auto",
-          maxWidth: "105rem",
+          maxWidth: "850px",
           display: "grid",
           alignItems: "center",
           justifyItems: "center",
-          gridTemplateColumns: "repeat(16, minmax(16px, 1fr))",
-          gridTemplateRows: "repeat(5, minmax(72px, auto))",
-          gridTemplateAreas: [
-            // Show 6 icons
-            `'i0 . . . i1 . . . . . . i2 . . . .'
-              't t t t t t t t t t t t t t t t'
-              't t t t t t t t t t t t t t t t'
-              't t t t t t t t t t t t t t t t'
-              '. . i3 . . . . i4 . . . . . i5 . .'`,
-
-            // Show 8 icons
-            `'i0 . . . i1 . . . . . . i2 . . . .'
-              '. t t t t t t t t t t t t t t i7'
-              'i6 t t t t t t t t t t t t t t .'
-              '. t t t t t t t t t t t t t t .'
-              '. . i3 . . . . i4 . . . . . i5 . .'`,
-
-            // Show 12 icons
-            `'i0 . . . i1 . . . . . . i2 . . . i8'
-              '. . i11 t t t t t t t t t t . . .'
-              'i6 . . t t t t t t t t t t i7 . .'
-              '. . . t t t t t t t t t t . . i9'
-              '. i10 . i3 . . . i4 . . . . . i5 . .'`,
-          ],
         }}
       >
-        {ICONS.map(({ Icon, color }, i) => {
-          return (
-            <Box
-              key={i}
-              sx={{
-                gridArea: `i${i}`,
-                display: [
-                  i > 5 ? "none" : "block",
-                  i > 7 ? "none" : "block",
-                  "block",
-                ],
-                placeSelf:
-                  i % 2 === 0
-                    ? "end end"
-                    : i % 3 === 0
-                    ? "start start"
-                    : "center center",
-              }}
-            >
-              <Icon color={color} width={24} height={24} />
-            </Box>
-          );
-        })}
-
         <Box
           sx={{
             p: 4,
