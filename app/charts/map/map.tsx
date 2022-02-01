3235c6c621d2290d363eb63971b1b385afc696fc
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2022-02-01 14:19:41 +0100

    fix: Make sure a map is rendered when trying to constrain zoom

--- app/charts/map/map.tsx
+++ app/charts/map/map.tsx
@@ -34,8 +34,8 @@ const INITIAL_VIEW_STATE = {
 type BBox = [[number, number], [number, number]];
 
 const CH_BBOX: BBox = [
-  [5.956800664952974, 45.81912371940225],
-  [10.493446773955753, 47.80741209797084],
+  [6.02260949059, 45.7769477403],
+  [10.4427014502, 47.8308275417],
 ];
 
 /**
@@ -55,35 +55,40 @@ const constrainZoom = (
 
   const { width, height, zoom, longitude, latitude } = viewState;
 
-  const [x, y] = vp.project([longitude, latitude]);
-  const [x0, y1] = vp.project(bbox[0]);
-  const [x1, y0] = vp.project(bbox[1]);
+  // Make sure the map is rendered before trying to project & fitBounds
+  if (vp.width > 1 && vp.height > 1) {
+    const [x, y] = vp.project([longitude, latitude]);
+    const [x0, y1] = vp.project(bbox[0]);
+    const [x1, y0] = vp.project(bbox[1]);
 
-  const fitted = vp.fitBounds(bbox, { padding });
+    const fitted = vp.fitBounds(bbox, { padding });
 
-  const [cx, cy] = vp.project([fitted.longitude, fitted.latitude]);
+    const [cx, cy] = vp.project([fitted.longitude, fitted.latitude]);
 
-  const h = height - padding * 2;
-  const w = width - padding * 2;
+    const h = height - padding * 2;
+    const w = width - padding * 2;
 
-  const h2 = h / 2;
-  const w2 = w / 2;
+    const h2 = h / 2;
+    const w2 = w / 2;
 
-  const y2 =
-    y1 - y0 < h ? cy : y - h2 < y0 ? y0 + h2 : y + h2 > y1 ? y1 - h2 : y;
-  const x2 =
-    x1 - x0 < w ? cx : x - w2 < x0 ? x0 + w2 : x + w2 > x1 ? x1 - w2 : x;
+    const y2 =
+      y1 - y0 < h ? cy : y - h2 < y0 ? y0 + h2 : y + h2 > y1 ? y1 - h2 : y;
+    const x2 =
+      x1 - x0 < w ? cx : x - w2 < x0 ? x0 + w2 : x + w2 > x1 ? x1 - w2 : x;
 
-  const p = vp.unproject([x2, y2]);
+    const p = vp.unproject([x2, y2]);
 
-  return {
-    ...viewState,
-    transitionDuration: 0,
-    transitionInterpolator: null,
-    zoom: Math.max(zoom, fitted.zoom),
-    longitude: p[0],
-    latitude: p[1],
-  };
+    return {
+      ...viewState,
+      transitionDuration: 0,
+      transitionInterpolator: null,
+      zoom: Math.max(zoom, fitted.zoom),
+      longitude: p[0],
+      latitude: p[1],
+    };
+  } else {
+    return viewState;
+  }
 };
 
 export const MapComponent = () => {
