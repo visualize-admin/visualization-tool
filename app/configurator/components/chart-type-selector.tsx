f5b141b785cf6c0edf67244f7e5b8482d3c764ac
Patrick Browne <pt.browne@gmail.com>
2022-02-14 17:44:45 +0100

    refactor: Remove unused

--- app/configurator/components/chart-type-selector.tsx
+++ app/configurator/components/chart-type-selector.tsx
@@ -8,7 +8,6 @@ import { useDataCubeMetadataWithComponentValuesQuery } from "../../graphql/query
 import { DataCubeMetadata } from "../../graphql/types";
 import { Icon } from "../../icons";
 import { useLocale } from "../../locales/use-locale";
-import { useTheme } from "../../themes";
 import { FieldProps, useChartTypeSelectorField } from "../config-form";
 import { SectionTitle } from "./chart-controls/section";
 import { getFieldLabel, getIconName } from "./ui-helpers";
