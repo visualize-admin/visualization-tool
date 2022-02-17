0cf6f227a22412aba7a23c79c330a0ff7e6de279
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2022-02-17 16:06:56 +0100

    feat: Add an option to hide shapes

--- app/charts/map/chart-map.tsx
+++ app/charts/map/chart-map.tsx
@@ -97,12 +97,27 @@ export const ChartMapVisualization = ({
     const dimension = dimensions?.find((d) => d.iri === areaDimensionIri);
 
     if (isGeoShapesDimension(dimension) && geoShapes && observations) {
-      const { topology } = geoShapes;
+      const activeFilters = chartConfig.filters[areaDimensionIri];
+      const activeFiltersIris = activeFilters
+        ? activeFilters.type === "single"
+          ? [activeFilters.value]
+          : activeFilters.type === "multi"
+          ? Object.keys(activeFilters.values)
+          : undefined
+        : undefined;
 
+      const { topology } = geoShapes;
       const topojson = topojsonFeature(
         topology,
         topology.objects.shapes
       ) as AreaLayer["shapes"];
+
+      // Completely hide unselected shapes (so they don't affect the legend, etc)
+      if (activeFiltersIris) {
+        topojson.features = topojson.features.filter((d) =>
+          activeFiltersIris.includes(d.properties.iri)
+        );
+      }
 
       topojson.features.forEach((d: GeoFeature) => {
         // Should we match by labels?
@@ -118,7 +133,13 @@ export const ChartMapVisualization = ({
         mesh: topojsonMesh(topology, topology.objects.shapes),
       };
     }
-  }, [areaDimensionIri, dimensions, observations, geoShapes]);
+  }, [
+    areaDimensionIri,
+    dimensions,
+    chartConfig.filters,
+    observations,
+    geoShapes,
+  ]);
 
   const symbolLayer: SymbolLayer | undefined = useMemo(() => {
     const dimension = dimensions?.find((d) => d.iri === symbolDimensionIri);
