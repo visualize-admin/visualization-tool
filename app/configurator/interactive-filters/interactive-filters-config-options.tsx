daba6f7c644ed3fb4989700a57fa716e27c77750
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2022-02-24 15:24:18 +0100

    feat: Disable numerical dimensions for interactive filters

--- app/configurator/interactive-filters/interactive-filters-config-options.tsx
+++ app/configurator/interactive-filters/interactive-filters-config-options.tsx
@@ -5,6 +5,7 @@ import { Box } from "theme-ui";
 import { getFieldComponentIri, getFieldComponentIris } from "../../charts";
 import { Checkbox } from "../../components/form";
 import { Loading } from "../../components/hint";
+import { isNumericalDimension } from "../../domain/data";
 import {
   DimensionMetaDataFragment,
   TimeUnit,
@@ -252,12 +253,13 @@ const InteractiveDataFilterOptions = ({
 
     // Dimensions that are not encoded in the visualization
     // excluding temporal dimensions
-    const unMappedDimensionsWithoutTemporalDimensions =
+    const unMappedDimensionsWithoutTemporalAndNumericalDimensions =
       data?.dataCubeByIri.dimensions.filter(
         (dim) =>
           !mappedIris.has(dim.iri) &&
           (dim.__typename !== "TemporalDimension" ||
-            dim.timeUnit === TimeUnit.Year)
+            dim.timeUnit === TimeUnit.Year) &&
+          !isNumericalDimension(dim)
       );
 
     return (
@@ -270,19 +272,21 @@ const InteractiveDataFilterOptions = ({
           path="dataFilters"
           defaultChecked={false}
           disabled={false}
-          dimensions={unMappedDimensionsWithoutTemporalDimensions}
+          dimensions={unMappedDimensionsWithoutTemporalAndNumericalDimensions}
         ></InteractiveDataFiltersToggle>
         <Box sx={{ my: 3 }}>
-          {unMappedDimensionsWithoutTemporalDimensions.map((d, i) => (
-            <InteractiveDataFilterOptionsCheckbox
-              key={i}
-              label={d.label}
-              value={d.iri}
-              disabled={
-                !chartConfig.interactiveFiltersConfig?.dataFilters.active
-              }
-            />
-          ))}
+          {unMappedDimensionsWithoutTemporalAndNumericalDimensions.map(
+            (d, i) => (
+              <InteractiveDataFilterOptionsCheckbox
+                key={i}
+                label={d.label}
+                value={d.iri}
+                disabled={
+                  !chartConfig.interactiveFiltersConfig?.dataFilters.active
+                }
+              />
+            )
+          )}
         </Box>
       </>
     );
