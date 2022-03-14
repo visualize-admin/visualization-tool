596db1c8d30731bf14684b56765f1106cc5bc450
Patrick Browne <pt.browne@gmail.com>
2022-03-14 16:14:39 +0100

    refactor: Export directly useTheme from material ui

--- app/themes/index.ts
+++ app/themes/index.ts
@@ -1,48 +1,4 @@
-import { Theme as ThemeUITheme, ThemeStyles, useThemeUI } from "@mui/material";
-
-/**
- * Adapted/refined from the [Theme UI Theme Specification](https://theme-ui.com/theme-spec)
- *
- * TODO: Types are still a bit wonky because the base types are not the greatest
- */
-
-export interface Theme extends ThemeUITheme {
-  fontSizes: Array<string | number>;
-  lineHeights: Array<string | number>;
-  fonts: {
-    body: string;
-    monospace: string;
-  };
-  colors: Record<string, string>;
-  shadows: {
-    primary: string;
-    rightSide: string;
-    leftSide: string;
-    tooltip: string;
-  };
-  styles: ThemeStyles;
-}
-
-// = Omit<
-//   ThemeUITheme,
-//   "colors" | "buttons" | "links" | "fontSizes" | "lineHeights" | "fonts"
-// > &
-//   Required<Pick<ThemeUITheme, "space" | "breakpoints">> & {
-//     fontSizes: Array<string | number>;
-//     lineHeights: Array<string | number>;
-//     fonts: {
-//       body: string;
-//       monospace: string;
-//     };
-//     colors: Record<string, string>;
-//     shadows: {
-//       primary: string;
-//       rightSide: string;
-//       leftSide: string;
-//       tooltip: string;
-//     };
-//   };
-
+import { Theme, useTheme } from "@mui/material";
 export interface ThemeModule {
   theme: Theme;
   preloadFonts?: string[];
@@ -60,4 +16,4 @@ export const loadTheme = async (theme: string = "federal") => {
   return themeModule;
 };
 
-export const useTheme = () => useThemeUI().theme as unknown as Theme;
+export { useTheme };
