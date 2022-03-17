c64f45dbeeeb6fc86061304c4ab3270b7806bcbb
Patrick Browne <pt.browne@gmail.com>
2022-03-17 17:25:29 +0100

    refactor: Remove unused constant

--- app/charts/table/constants.ts
+++ app/charts/table/constants.ts
@@ -1,24 +1,3 @@
-import { ThemeUICSSObject } from "@mui/material";
-
 export const TABLE_HEIGHT = 600;
 export const BAR_CELL_PADDING = 12;
 export const SORTING_ARROW_WIDTH = 24;
-export const TABLE_STYLES: ThemeUICSSObject = {
-  borderSpacing: 0,
-  border: "none",
-  tableLayout: "fixed",
-
-  fontSize: "0.875rem",
-  color: "grey.700",
-
-  th: {},
-
-  td: {
-    m: 0,
-    py: 2,
-    pr: 3,
-    pl: 3,
-    borderBottom: "1px solid",
-    borderBottomColor: "grey.400",
-  },
-};
