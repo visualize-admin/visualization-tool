58e2c1aeb902e69fc1d6fe44f73016e3dc4caeed
Patrick Browne <pt.browne@gmail.com>
2022-03-22 10:22:50 +0100

    fix: Padding issue on fieldsets due to unnecessary quoting

--- app/themes/federal.tsx
+++ app/themes/federal.tsx
@@ -667,7 +667,7 @@ theme.components = {
     
         fieldset {
           border: 0;
-          padding: "0.01em 0 0 0";
+          padding: 0.01em 0 0 0;
           margin: 0;
           minWidth: 0;
         }
