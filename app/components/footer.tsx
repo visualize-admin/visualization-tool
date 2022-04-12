fca56eed60abf8e92314b52d0d3bfd64cf748d20
Patrick Browne <pt.browne@gmail.com>
2022-04-12 16:50:30 +0200

    feat: Remove status page

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -120,9 +120,9 @@ export const Footer = () => {
           >
             <Version />
           </Typography>
-          <FooterLinkBottom href="https://visualization-tool.status.interactivethings.io/">
+          {/*<FooterLinkBottom href="https://visualization-tool.status.interactivethings.io/">
             <Trans id="footer.status">Status</Trans>
-          </FooterLinkBottom>
+          </FooterLinkBottom>*/}
           <NextLink
             href={contentRoutes.legal[locale].path}
             passHref
