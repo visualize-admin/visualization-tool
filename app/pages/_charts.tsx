a469e47f5fadb4e24c50f049c349518aa9407c1d
Patrick Browne <pt.browne@gmail.com>
2022-04-22 17:40:46 +0200

    feat: Load test charts only when they are visible

--- app/pages/_charts.tsx
+++ app/pages/_charts.tsx
@@ -8,12 +8,67 @@ import Flex from "@/components/flex";
 import { ContentLayout } from "@/components/layout";
 import { Config } from "@/configurator";
 import { getAllConfigs } from "@/db/config";
+import React, { useEffect, useRef, useState } from "react";
 
 type PageProps = {
   configs: {
     key: string;
     data: Config;
   }[];
+};
+
+const useVisible = (
+  initialVisible: boolean,
+  intersectionRatio: number = 0.25,
+  observerOptions?: IntersectionObserverInit
+) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [visible, setVisible] = useState(initialVisible);
+  useEffect(() => {
+    const current = ref.current;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        for (let entry of entries) {
+          if (entry.intersectionRatio > intersectionRatio) {
+            setVisible(true);
+          } else {
+            setVisible(false);
+          }
+        }
+      },
+      {
+        threshold: 0.25,
+        ...observerOptions,
+      }
+    );
+    if (current) {
+      observer.observe(current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, [intersectionRatio, observerOptions]);
+  return [ref, visible] as const;
+};
+
+const useHasBeenVisible = (...options: Parameters<typeof useVisible>) => {
+  const [ref, visible] = useVisible(...options);
+  const hasBeenVisible = useRef(visible);
+  hasBeenVisible.current = hasBeenVisible.current || visible;
+  return [ref, hasBeenVisible.current] as const;
+};
+
+export const HiddenUntilScrolledTo = ({
+  children,
+  initialVisible = false,
+  fallback,
+}: {
+  children: React.ReactNode;
+  initialVisible?: boolean;
+  fallback: React.ReactNode;
+}) => {
+  const [ref, hasBeenVisible] = useHasBeenVisible(initialVisible);
+  return <div ref={ref}>{hasBeenVisible ? children : fallback}</div>;
 };
 
 export const getServerSideProps = async () => {
@@ -32,7 +87,7 @@ const Page: NextPage<PageProps> = ({ configs }) => {
       <ContentLayout>
         <Box px={4} sx={{ backgroundColor: "muted.main" }} mb="auto">
           <Flex sx={{ pt: 4, flexWrap: "wrap" }}>
-            {configs.map(({ key, data: { dataSet, chartConfig, meta } }) => {
+            {configs.map(({ key, data: { dataSet, chartConfig, meta } }, i) => {
               return (
                 <Box
                   key={key}
@@ -46,12 +101,17 @@ const Page: NextPage<PageProps> = ({ configs }) => {
                           <Link> {key}</Link>
                         </NextLink>
                       </Box>
-                      <ChartPublished
-                        dataSet={dataSet}
-                        chartConfig={chartConfig}
-                        meta={meta}
-                        configKey={key}
-                      />
+                      <HiddenUntilScrolledTo
+                        initialVisible={i < 5}
+                        fallback={<div>Loading...</div>}
+                      >
+                        <ChartPublished
+                          dataSet={dataSet}
+                          chartConfig={chartConfig}
+                          meta={meta}
+                          configKey={key}
+                        />
+                      </HiddenUntilScrolledTo>
                     </div>
                   </ChartPanel>
                 </Box>
