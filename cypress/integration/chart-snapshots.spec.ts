b36f3ee6ecb161cc511e9593d20079561a7651fe
Patrick Browne <pt.browne@gmail.com>
2022-04-28 13:55:46 +0200

    feat: Cypress makes screenshots and saves them to cypress/screenshots

--- cypress/integration/chart-snapshots.spec.ts
+++ cypress/integration/chart-snapshots.spec.ts
@@ -1,25 +1,37 @@
 import intConfigs from "../../app/test/__fixtures/int/configs";
+import "cypress-network-idle";
 
 // Right now the CI app server runs connected to int.lindas.admin.ch
 const configs = intConfigs.map((x) => ({ env: "int", ...x }));
 
 describe("Chart Snapshots", () => {
-  configs.forEach(({ slug, env }) => {
-    it(`snapshot ${slug} matches`, () => {
-      cy.on("uncaught:exception", (err, runnable) => {
-        // return false to prevent the error from
-        // failing this test
-        return false;
-      });
+  it(`screenshots`, () => {
+    for (const viewportArgs of [
+      ["ipad-mini", "portrait"],
+      ["iphone-8", "portrait"],
+    ] as [Cypress.ViewportPreset, Cypress.ViewportOrientation][]) {
+      configs.forEach(({ slug, env }) => {
+        cy.on("uncaught:exception", (err, runnable) => {
+          console.log(err);
+          // return false to prevent the error from
+          // failing tests
+          return false;
+        });
+        cy.viewport(...viewportArgs);
+        cy.visit(`/en/__test/${env}/${slug}`);
+        cy.get(`[data-chart-loaded="true"]`, { timeout: 20000 });
+        cy.wait(100);
 
-      cy.viewport("ipad-mini", "portrait");
-      cy.visit(`/en/__test/${env}/${slug}`);
-      cy.get(`[data-chart-loaded="true"]`, { timeout: 20000 });
-      cy.wait(100);
-      cy.document().toMatchImageSnapshot({
-        capture: "fullPage",
-        name: `chart-${env}-${slug}`,
+        if (slug.includes("map")) {
+          // @ts-ignore
+          cy.waitForNetworkIdle(1000);
+        }
+        // Screenshots are not compared by Cypress, they are uploaded to argos
+        cy.screenshot(`chart-${env}-${viewportArgs[0]}-${slug}`, {
+          capture: "fullPage",
+        });
+        cy.wait(100);
       });
-    });
+    }
   });
 });
