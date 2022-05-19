75dab3208a9f0c53d8a490be91e216cc9e048e16
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2022-05-19 13:46:51 +0200

    fix: Lint

--- app/charts/shared/interaction/tooltip.tsx
+++ app/charts/shared/interaction/tooltip.tsx
@@ -15,10 +15,8 @@ export const TOOLTIP_OFFSET = 4;
 
 export const Tooltip = ({ type = "single" }: { type: TooltipType }) => {
   const [state] = useInteraction();
-  const { visible, mouse, d } = state.interaction;
-  return (
-    <>{visible && d && <TooltipInner d={d} mouse={mouse} type={type} />}</>
-  );
+  const { visible, d } = state.interaction;
+  return <>{visible && d && <TooltipInner d={d} type={type} />}</>;
 };
 
 export type Xplacement = "left" | "center" | "right";
