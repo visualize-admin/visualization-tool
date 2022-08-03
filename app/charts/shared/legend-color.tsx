de5db5bc455ae98d67a074a73514c4ef1c5e4770
Patrick Browne <pt.browne@gmail.com>
2022-08-03 16:21:03 +0200

    feat: Add legend groups

--- app/charts/shared/legend-color.tsx
+++ app/charts/shared/legend-color.tsx
@@ -1,4 +1,4 @@
-import { Theme } from "@mui/material";
+import { Theme, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import React, { memo, useMemo } from "react";
 
@@ -9,7 +9,19 @@ import {
 import { useInteractiveFilters } from "@/charts/shared/use-interactive-filters";
 import Flex from "@/components/flex";
 import { Checkbox } from "@/components/form";
+import {
+  ConfiguratorState,
+  isConfiguring,
+  isSegmentInConfig,
+} from "@/configurator";
+import {
+  useDataCubeMetadataWithComponentValuesQuery,
+  useDimensionHierarchyQuery,
+} from "@/graphql/query-hooks";
+import { HierarchyValue } from "@/graphql/resolver-types";
+import { dfs } from "@/lib/dfs";
 import useEvent from "@/lib/use-event";
+import { useConfiguratorState, useLocale } from "@/src";
 
 type LegendSymbol = "square" | "line" | "circle";
 
@@ -21,7 +33,12 @@ const useStyles = makeStyles<Theme>(() => ({
     flexWrap: "wrap",
     minHeight: "20px",
     gap: "0 1.5rem",
+    display: "grid",
+    gridTemplateColumns: "repeat(4, auto)",
+    gridTemplateRows: "repeat(4, auto)",
+    gridAutoFlow: "row dense",
   },
+  legendGroup: {},
 }));
 
 const useItemStyles = makeStyles<
@@ -50,9 +67,16 @@ const useItemStyles = makeStyles<
   },
 }));
 
+type SegmentConfigState<T extends ConfiguratorState = ConfiguratorState> =
+  T extends { chartConfig: { fields: { segment: any } } } ? T : never;
+
+type S = SegmentConfigState<ConfiguratorState>;
+// ^?
+
 export const InteractiveLegendColor = () => {
   const [state, dispatch] = useInteractiveFilters();
   const { categories } = state;
+
   const activeInteractiveFilters = useMemo(() => {
     return new Set(Object.keys(categories));
   }, [categories]);
@@ -79,19 +103,76 @@ export const InteractiveLegendColor = () => {
   const classes = useStyles();
   return (
     <Flex className={classes.legendContainer}>
-      {colors.domain().map((item, i) => (
-        <Checkbox
-          label={item}
-          name={item}
-          value={item}
-          checked={!activeInteractiveFilters.has(item)}
-          onChange={() => setFilter(item)}
-          key={i}
-          color={colors(item)}
-        />
-      ))}
+      {groups.map((group) => {
+        return (
+          <div key={group.value}>
+            <Typography variant="h4">{group.label}</Typography>
+            {colors.domain().map((item, i) => (
+              <Checkbox
+                label={item}
+                name={item}
+                value={item}
+                checked={!activeInteractiveFilters.has(item)}
+                onChange={() => setFilter(item)}
+                key={i}
+                color={colors(item)}
+              />
+            ))}
+          </div>
+        );
+      })}
     </Flex>
   );
+};
+
+const useLegendGroups = (labels: string[]) => {
+  const [configState] = useConfiguratorState(isConfiguring);
+  const segment = isSegmentInConfig(configState.chartConfig)
+    ? configState.chartConfig.fields.segment
+    : null;
+  const locale = useLocale();
+  const cubeIri = configState.dataSet;
+  const [{ data: cubeMetadata }] = useDataCubeMetadataWithComponentValuesQuery({
+    variables: {
+      iri: configState.dataSet,
+      locale: locale,
+    },
+  });
+  const segmentDimension = useMemo(() => {
+    return cubeMetadata?.dataCubeByIri?.dimensions.find(
+      (d) => d.iri === segment?.componentIri
+    );
+  }, [cubeMetadata?.dataCubeByIri?.dimensions, segment?.componentIri]);
+  const [hierarchyResp] = useDimensionHierarchyQuery({
+    variables: {
+      cubeIri: cubeIri,
+      dimensionIri: segmentDimension?.iri!,
+      locale: locale,
+    },
+    pause: !segmentDimension?.iri,
+  });
+  const hierarchy =
+    hierarchyResp?.data?.dataCubeByIri?.dimensionByIri?.hierarchy;
+
+  const groups = useMemo(() => {
+    if (!hierarchy) {
+      return new Map([[[], labels]]);
+    }
+    const labelSet = new Set(labels);
+    const groups = new Map<HierarchyValue[], string[]>();
+    [
+      ...dfs(hierarchy, (node, { depth, parents }) => {
+        if (!labelSet.has(node.label)) {
+          return;
+        }
+        groups.set(parents, groups.get(parents) || []);
+        groups.get(parents)?.push(node.label);
+      }),
+    ];
+    return groups;
+  }, [hierarchy, labels]);
+
+  return groups;
 };
 
 export const LegendColor = memo(function LegendColor({
@@ -101,17 +182,32 @@ export const LegendColor = memo(function LegendColor({
 }) {
   // @ts-ignore
   const { colors, getSegmentLabel } = useChartState() as ColorsChartState;
+  const groups = useLegendGroups(colors.domain());
   const classes = useStyles();
   return (
     <Flex className={classes.legendContainer}>
-      {colors.domain().map((item, i) => (
-        <LegendItem
-          key={i}
-          item={getSegmentLabel(item)}
-          color={colors(item)}
-          symbol={symbol}
-        />
-      ))}
+      {groups
+        ? Array.from(groups.entries()).map(([g, colorValues]) => {
+            return (
+              <div
+                className={classes.legendGroup}
+                key={g.map((n) => n.label).join(">")}
+              >
+                <Typography variant="h5" gutterBottom>
+                  {g.map((n) => n.label).join(">")}
+                </Typography>
+                {colorValues.map((item, i) => (
+                  <LegendItem
+                    key={i}
+                    item={getSegmentLabel(item)}
+                    color={colors(item)}
+                    symbol={symbol}
+                  />
+                ))}
+              </div>
+            );
+          })
+        : null}
     </Flex>
   );
 });
