77398924f5e0ef27d406c65a951244448f7ea471
Patrick Browne <pt.browne@gmail.com>
2022-08-12 10:58:52 +0200

    fix: Interlace would fail if array length was 0

--- app/configurator/components/filters.tsx
+++ app/configurator/components/filters.tsx
@@ -383,7 +383,7 @@ const interlace = <T extends unknown, I extends unknown>(
   arr: T[],
   interlacer: I
 ) => {
-  const res = new Array(arr.length * 2 - 1);
+  const res = new Array(Math.max(arr.length * 2 - 1, 0));
   for (let i = 0; i < arr.length; i++) {
     res[i * 2] = arr[i];
     if (i < arr.length - 1) {
