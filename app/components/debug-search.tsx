d21018f4355bdb5206590d9009f1814aae6fd353
Patrick Browne <pt.browne@gmail.com>
2022-08-16 16:08:40 +0200

    feat: Improve debug page

--- app/components/debug-search.tsx
+++ app/components/debug-search.tsx
@@ -1,41 +1,86 @@
-import { Stack } from "@mui/material";
+import { FormControlLabel, Stack } from "@mui/material";
 import Box from "@mui/material/Box";
 import Chip from "@mui/material/Chip";
+import CircularProgress from "@mui/material/CircularProgress";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import Switch from "@mui/material/Switch";
 import Typography from "@mui/material/Typography";
-import React from "react";
+import React, { useState } from "react";
 
 import { DataCubeSearchFilter, useDataCubesQuery } from "@/graphql/query-hooks";
+
+const territoryTheme = {
+  name: "Territory theme",
+  type: "DataCubeTheme",
+  value: "https://register.ld.admin.ch/opendataswiss/category/territory",
+};
+
+const geographyTheme = {
+  name: "Geography theme",
+  type: "DataCubeTheme",
+  value: "https://register.ld.admin.ch/opendataswiss/category/geography",
+};
+
+const bafuCreator = {
+  name: "BAFU creator",
+  type: "DataCubeOrganization",
+  value:
+    "https://register.ld.admin.ch/opendataswiss/org/bundesamt-fur-umwelt-bafu",
+};
+
+const mobilityTheme = {
+  name: "Mobility theme",
+  type: "DataCubeTheme",
+  value: "https://register.ld.admin.ch/opendataswiss/category/mobility",
+};
 
 const Search = ({
   query,
   locale,
   filters,
   includeDrafts,
+  sourceUrl,
 }: {
   query: string;
   locale: string;
-  filters: DataCubeSearchFilter[];
-  includeDrafts?: boolean;
+  filters: (DataCubeSearchFilter & { name: string })[];
+  includeDrafts: boolean;
+  sourceUrl: string;
 }) => {
   const [cubes] = useDataCubesQuery({
     variables: {
       locale: locale,
       query: query,
-      filters: filters,
-      includeDrafts: !!includeDrafts,
-      sourceUrl: "https://int.lindas.admin.ch/query",
+      filters: filters.map(({ name, type, value }) => ({ type, value })),
+      includeDrafts,
+      sourceUrl,
       sourceType: "sparql",
     },
   });
   return (
     <Box>
-      <Typography variant="h5">&quot;{query}&quot;</Typography>
-      <Typography
-        variant="caption"
-        color={cubes.data?.dataCubes.length === 0 ? "error" : undefined}
-      >
-        {cubes.data?.dataCubes.length} results
+      <Typography variant="h5">
+        &quot;{query}&quot;&nbsp;
+        {filters
+          ? filters.map((f) => (
+              <Chip key={f.value} size="small" label={f.name} />
+            ))
+          : null}
       </Typography>
+      {cubes.fetching ? (
+        <CircularProgress />
+      ) : (
+        <Typography
+          variant="caption"
+          color={cubes.data?.dataCubes.length === 0 ? "error" : undefined}
+        >
+          {cubes.data?.dataCubes.length} results
+        </Typography>
+      )}
+      {cubes.error ? (
+        <Typography color="error">{cubes.error.message}</Typography>
+      ) : null}
       <Stack spacing={4}>
         {cubes.data?.dataCubes.map((c) => {
           return (
@@ -50,6 +95,7 @@ const Search = ({
                 {c?.dataCube.themes.map((t) => (
                   <Chip key={t.iri} size="small" label={t.label} />
                 ))}
+                <Chip size="small" label={c?.dataCube.publicationStatus} />
               </Stack>
             </div>
           );
@@ -59,28 +105,11 @@ const Search = ({
   );
 };
 
-const territoryTheme = {
-  type: "DataCubeTheme",
-  value: "https://register.ld.admin.ch/opendataswiss/category/territory",
-};
-
-const geographyTheme = {
-  type: "DataCubeTheme",
-  value: "https://register.ld.admin.ch/opendataswiss/category/geography",
-};
-
-const bafuCreator = {
-  type: "DataCubeOrganization",
-  value:
-    "https://register.ld.admin.ch/opendataswiss/org/bundesamt-fur-umwelt-bafu",
-};
-
-const mobilityTheme = {
-  type: "DataCubeTheme",
-  value: "https://register.ld.admin.ch/opendataswiss/category/mobility",
-};
-
 const DebugSearch = () => {
+  const [includeDrafts, setIncludeDrafts] = useState(false);
+  const [sourceUrl, setSourceUrl] = useState(
+    "https://int.lindas.admin.ch/query"
+  );
   return (
     <Box
       sx={{
@@ -90,28 +119,100 @@ const DebugSearch = () => {
         gridTemplateColumns: "repeat(auto-fit, minmax(1fr, 300px))",
       }}
     >
+      <FormControlLabel
+        label="Drafts"
+        control={
+          <Switch
+            value={includeDrafts}
+            onChange={(_ev, checked) => setIncludeDrafts(checked)}
+          />
+        }
+      />
+      <Select
+        onChange={(ev) => setSourceUrl(ev.target.value)}
+        value={sourceUrl}
+      >
+        <MenuItem value="https://int.lindas.admin.ch/query">int</MenuItem>
+        <MenuItem value="https://lindas.admin.ch/query">prod</MenuItem>
+      </Select>
       <Search
+        sourceUrl={sourceUrl}
+        includeDrafts={includeDrafts}
         query="bruit"
-        filters={[mobilityTheme]}
+        filters={[]}
         locale="fr"
-        includeDrafts
       />
       <Search
+        sourceUrl={sourceUrl}
+        includeDrafts={includeDrafts}
         query="Bathing"
         filters={[territoryTheme]}
-        includeDrafts
         locale="en"
       />
-      <Search query="Ausgaben" filters={[]} locale="de" />
+      <Search
+        sourceUrl={sourceUrl}
+        includeDrafts={includeDrafts}
+        query="Ausgaben"
+        filters={[]}
+        locale="de"
+      />
 
-      <Search query="" filters={[territoryTheme]} locale="en" />
-      <Search query="SFOE" filters={[geographyTheme]} locale="en" />
-      <Search query="National economy" filters={[]} locale="en" />
-      <Search query="Einmalvergütung" filters={[]} locale="de" />
-      <Search query="zeitverzögert" filters={[]} locale="de" includeDrafts />
-      <Search query="öffentlich" filters={[]} locale="de" includeDrafts />
-      <Search query="WASTA" filters={[]} locale="en" />
-      <Search query="SFA" filters={[]} locale="en" />
+      <Search
+        sourceUrl={sourceUrl}
+        includeDrafts={includeDrafts}
+        query=""
+        filters={[territoryTheme]}
+        locale="en"
+      />
+      <Search
+        sourceUrl={sourceUrl}
+        includeDrafts={includeDrafts}
+        query="SFOE"
+        filters={[geographyTheme]}
+        locale="en"
+      />
+      <Search
+        sourceUrl={sourceUrl}
+        includeDrafts={includeDrafts}
+        query="National economy"
+        filters={[]}
+        locale="en"
+      />
+      <Search
+        sourceUrl={sourceUrl}
+        includeDrafts={includeDrafts}
+        query="Einmalvergütung"
+        filters={[]}
+        locale="de"
+      />
+      <Search
+        sourceUrl={sourceUrl}
+        includeDrafts={includeDrafts}
+        query="zeitverzögert"
+        filters={[]}
+        locale="de"
+      />
+      <Search
+        sourceUrl={sourceUrl}
+        includeDrafts={includeDrafts}
+        query="öffentlich"
+        filters={[]}
+        locale="de"
+      />
+      <Search
+        sourceUrl={sourceUrl}
+        includeDrafts={includeDrafts}
+        query="WASTA"
+        filters={[]}
+        locale="en"
+      />
+      <Search
+        sourceUrl={sourceUrl}
+        includeDrafts={includeDrafts}
+        query="SFA"
+        filters={[]}
+        locale="en"
+      />
     </Box>
   );
 };
