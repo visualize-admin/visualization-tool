c6066cb9687bfd62291339f61aeb64142799bdfe
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2022-08-16 12:23:21 +0200

    chore: Add comment to global map ref

--- app/charts/map/ref.ts
+++ app/charts/map/ref.ts
@@ -1,5 +1,9 @@
 import { Map } from "mapbox-gl";
 
+/**
+ * We need to access the map from the map controls. Until we have a good solution
+ * for sibling components, we use a global non-observable ref.
+ */
 let map: Map | null = null;
 
 const getMap = () => {
