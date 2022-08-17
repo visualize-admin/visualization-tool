4e18006603f43a9e95c6e20ea7d1b363dab6ca7f
Patrick Browne <pt.browne@gmail.com>
2022-08-17 16:48:40 +0200

    fix: Have same docker build push for build and release stages

---
+++ scripts/docker-build-push.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Builds and push the docker image to the registry
+# The DOCKER_IMAGE_TAGS environment variable must be set and will
+# be used to tag the image that is built.
+# Multiple tags can be used and must be separated by spaces.
+
+# Fail on error, fail if environment variable is not set, fail if pipe fails 
+set -euo pipefail
+
+# Logins to docker registry
+docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+
+# Build docker image, passing arguments defined in the CI 
+docker build \
+    --build-arg COMMIT=$CI_COMMIT_SHA \
+    --build-arg MAPTILER_STYLE_KEY=$MAPTILER_STYLE_KEY \
+    --build-arg VECTOR_TILE_URL=$VECTOR_TILE_URL \
+    $(echo $DOCKER_IMAGE_TAGS | tr ' ' '\n' | xargs -L 1 -I {} echo "-t {}" | tr '\n' ' ' ) .
+
+# Push all the tags
+docker push $DOCKER_IMAGE_TAGS
