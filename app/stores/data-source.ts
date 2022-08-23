2b2c9a314beb7c6f4d3c4e753caad77d0d26c9b1
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2022-08-23 16:13:00 +0200

    refactor: Rename & add docs

--- app/stores/data-source.ts
+++ app/stores/data-source.ts
@@ -17,7 +17,13 @@ type DataSourceStore = {
 
 const STORAGE_KEY = "dataSource";
 
-const customPersist =
+/**
+ * Custom middleware that saves data source to localStorage.
+ *
+ * On initialization it tries to first retrieve data source from URL; if it's
+ * not here, tries with localStorage, otherwise uses a default data source.
+ */
+const dataSourceStoreMiddleware =
   (config: StateCreator<DataSourceStore>) =>
   (
     set: StoreApi<DataSourceStore>["setState"],
@@ -42,16 +48,19 @@ const customPersist =
 
     let dataSource = DEFAULT_DATA_SOURCE;
 
+    // Triggered on init.
     if (isRunningInBrowser()) {
       const urlDataSourceLabel = getURLParam("dataSource");
       const urlDataSource = urlDataSourceLabel
         ? parseSourceByLabel(urlDataSourceLabel)
         : undefined;
+
       if (urlDataSourceLabel && urlDataSource) {
         dataSource = urlDataSource;
         localStorage.setItem(STORAGE_KEY, stringifyDataSource(urlDataSource));
       } else {
         const storageDataSource = localStorage.getItem(STORAGE_KEY);
+
         if (storageDataSource) {
           dataSource = parseDataSource(storageDataSource);
         } else {
@@ -64,7 +73,7 @@ const customPersist =
   };
 
 export const useDataSourceStore = create<DataSourceStore>(
-  customPersist((set) => ({
+  dataSourceStoreMiddleware((set) => ({
     dataSource: DEFAULT_DATA_SOURCE,
     setDataSource: (value) => {
       set({ dataSource: parseDataSource(value) });
