2787e965476e98f2c38da4131cc65b6966880fc8
Patrick Browne <pt.browne@gmail.com>
2022-09-19 10:22:32 +0200

    feat: Cube search does not use full text search engine

--- app/rdf/query-search-score-utils.ts
+++ app/rdf/query-search-score-utils.ts
@@ -1,5 +1,3 @@
-import { ResultRow } from "sparql-http-client/ResultParser";
-
 export const parseFloatZeroed = (s: string) => {
   const n = parseFloat(s);
   if (Number.isNaN(n)) {
@@ -9,60 +7,47 @@ export const parseFloatZeroed = (s: string) => {
   }
 };
 
-const parseScoreRow = (x: ResultRow) => {
-  return {
-    cubeIri: x.cube.value,
-    scoreName: parseFloatZeroed(x.scoreName?.value),
-    scoreDescription: parseFloatZeroed(x.scoreDescription?.value),
-    scoreTheme: parseFloatZeroed(x.scoreTheme?.value),
-    scorePublisher: parseFloatZeroed(x.scorePublisher?.value),
-    scoreCreator: parseFloatZeroed(x.scoreCreator?.value),
-  };
+export const weights: Record<string, number> = {
+  name: 5,
+  description: 2,
+  themeName: 1,
+  publisher: 1,
+  creatorLabel: 1,
 };
-type ScoreKey = Exclude<keyof ReturnType<typeof parseScoreRow>, "cubeIri">;
-const weights: Record<ScoreKey, number> = {
-  scoreName: 5,
-  scoreDescription: 2,
-  scoreTheme: 1,
-  scorePublisher: 1,
-  scoreCreator: 1,
-};
-/**
- * From a list of scores where each row contains only one score,
- * computes an index from cubeIri to weighted score.
- */
 
+/**
+ * From a list of cube rows containing weighted fields
+ */
 export const computeScores = (
   scoresRaw: any[],
-  { keepZeros }: { keepZeros: boolean }
+  { query }: { query?: string }
 ) => {
-  const scores = scoresRaw.map((r) => parseScoreRow(r));
-
-  const infoPerCube = scores.reduce(
-    (acc, scoreRow) => {
-      let cubeScore = acc[scoreRow.cubeIri]?.score ?? 0;
-      for (let [key, weight] of Object.entries(weights)) {
-        const attrScore = scoreRow[key as ScoreKey] ?? 0;
-        if (attrScore > 0) {
-          cubeScore = cubeScore + scoreRow[key as ScoreKey] * weight;
+  const infoPerCube = {} as Record<string, { score: number }>;
+  if (query) {
+    for (let scoreRow of scoresRaw) {
+      let score = 0;
+      for (let [field, weight] of Object.entries(weights)) {
+        const val = scoreRow[field]?.value;
+        if (!val) {
+          continue;
+        }
+        for (let tok of query.split(" ")) {
+          if (val.toLowerCase().includes(tok.toLowerCase())) {
+            score += weight;
+          }
         }
       }
-      if (cubeScore > 0 || keepZeros) {
-        acc[scoreRow.cubeIri] = acc[scoreRow.cubeIri] || {
-          score: 0,
-        };
-        acc[scoreRow.cubeIri].score = cubeScore;
+      infoPerCube[scoreRow.cube.value] = { score };
+    }
+    for (let k of Object.keys(infoPerCube)) {
+      if (infoPerCube[k]?.score === 0) {
+        delete infoPerCube[k];
       }
-      return acc;
-    },
-    {} as Record<
-      string,
-      {
-        score: number;
-        highlights: Record<ScoreKey, string>;
-      }
-    >
-  );
-
+    }
+  } else {
+    for (let scoreRow of scoresRaw) {
+      infoPerCube[scoreRow.cube.value] = { score: 1 };
+    }
+  }
   return infoPerCube;
 };
--- app/rdf/query-search.spec.ts
+++ app/rdf/query-search.spec.ts
@@ -1,6 +1,6 @@
 import mapValues from "lodash/mapValues";
 
-import { computeScores } from "./query-search-score-utils";
+import { computeScores, weights } from "./query-search-score-utils";
 
 // jest.mock("rdf-ext", () => ({}));
 // jest.mock("@rdf-esm/data-model", () => ({}));
@@ -13,30 +13,19 @@ jest.mock("@tpluscode/sparql-builder", () => ({}));
 
 describe("compute scores", () => {
   const scores = [
-    { cube: "a", scoreName: 1 },
-    { cube: "a", scoreDescription: 1 },
-    { cube: "b", scoreName: 5 },
-    { cube: "c", scoreCreator: 1 },
-    { cube: "d", scoreCreator: 0 },
+    { cube: "a", name: "national" },
+    { cube: "b", name: "national", description: "economy" },
+    { cube: "c", creatorLabel: "national" },
+    { cube: "d", creatorLabel: "" },
   ].map((x) => mapValues(x, (v) => ({ value: v })));
 
-  it("should compute weighted score per cube from score rows, and discard cubes with score: 0", () => {
+  it("should compute weighted score per cube from score rows", () => {
     const reduced = computeScores(scores, {
-      keepZeros: true,
+      query: "national economy",
     });
-    expect(reduced["a"].score).toEqual(7);
-    expect(reduced["b"].score).toEqual(25);
-    expect(reduced["c"].score).toEqual(1);
-    expect(reduced["d"].score).toEqual(0);
-  });
-
-  it("should compute weighted score per cube from score rows, , and keep cube with score: 0", () => {
-    const reduced = computeScores(scores, {
-      keepZeros: false,
-    });
-    expect(reduced["a"].score).toEqual(7);
-    expect(reduced["b"].score).toEqual(25);
-    expect(reduced["c"].score).toEqual(1);
+    expect(reduced["a"].score).toEqual(weights.name);
+    expect(reduced["b"].score).toEqual(weights.name + weights.description);
+    expect(reduced["c"].score).toEqual(weights.creatorLabel);
     expect(reduced["d"]).toBeUndefined();
   });
 });
