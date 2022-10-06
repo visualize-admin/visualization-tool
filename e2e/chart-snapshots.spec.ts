ae60e31fa3ce6a57a13588bf5284082195d80652
Patrick Browne <pt.browne@gmail.com>
2022-10-06 10:45:33 +0200

    fix: Diminish timeout since now there is a test for each chart

--- e2e/chart-snapshots.spec.ts
+++ e2e/chart-snapshots.spec.ts
@@ -19,7 +19,7 @@ const viewports = {
 for (let [viewportName, viewportSize] of Object.entries(viewports)) {
   for (let { slug, env } of configs) {
     test(`Chart Snapshots ${slug} ${env} ${viewportName}`, async ({ page }) => {
-      test.setTimeout(5 * 60 * 1000);
+      test.setTimeout(1 * 60 * 1000);
       await page.setViewportSize(viewportSize);
       await page.goto(`/en/__test/${env}/${slug}?dataSource=Int`);
       await page
