f4b2ab7cf3942b6b466b85f13ee5e52be5ff283c
Patrick Browne <pt.browne@gmail.com>
2022-10-11 18:29:33 +0200

    refactor: Remove unused

--- app/rdf/create-source.ts
+++ app/rdf/create-source.ts
@@ -1,14 +1,9 @@
-import { sparql } from "@tpluscode/sparql-builder";
 import { Source } from "rdf-cube-view-query";
 import rdf from "rdf-ext";
 
 export const pragmas = `#pragma describe.strategy cbd
 #pragma join.hash off
 `;
-
-export const withPragma = (t: any) => {
-  return sparql`${pragmas}${t.toString()}`;
-};
 
 export const createSource = ({ endpointUrl }: { endpointUrl: string }) => {
   return new Source({
