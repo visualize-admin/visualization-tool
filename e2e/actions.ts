5059ab5447ba69786e282c0df7e903482477822b
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2022-10-14 13:51:48 +0200

    refactor: Improve e2e actions

--- e2e/actions.ts
+++ e2e/actions.ts
@@ -1,7 +1,7 @@
 import selectors from "./selectors";
 import { makeActions, TestContext } from "./types";
 
-export const loadDatasetPreview = async (
+const loadDatasetPreview = async (
   ctx: TestContext,
   iri: string,
   dataSource: "Int" | "Prod"
@@ -14,9 +14,23 @@ export const loadDatasetPreview = async (
   await selectors.datasetPreview.loaded(ctx);
 };
 
+const waitForPanelRightToLoad = async (ctx: TestContext, field: string) =>
+  (await selectors.panels.right(ctx))
+    .locator(`h5 >> text=${field}`)
+    .waitFor({ timeout: 3000 });
+
+const selectActiveEditorField = async (ctx: TestContext, field: string) => {
+  (await selectors.panels.left(ctx)).locator(`button >> text=${field}`).click();
+  return waitForPanelRightToLoad(ctx, field);
+};
+
 const actions = makeActions({
   datasetPreview: {
     load: loadDatasetPreview,
+  },
+  editor: {
+    waitForPanelRightToLoad,
+    selectActiveField: selectActiveEditorField,
   },
 });
 
