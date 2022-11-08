97af247b8f2215456acefc2b6fb934eb8df105e0
Patrick Browne <pt.browne@gmail.com>
2022-11-08 12:54:13 +0100

    fix: Keep a singleton Prisma across hot reloads

--- app/db/client.ts
+++ app/db/client.ts
@@ -1,5 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+/**
+ * Global Prisma client
+ *
+ * Only one instance of Prisma is kept across hot reloads to prevent
+ * "FATAL: sorry, too many clients already" error.
+ * @see https://github.com/prisma/prisma/issues/1983
+ */
+let prisma: PrismaClient;
+
+const g = global as unknown as { prisma: PrismaClient | undefined };
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!g.prisma) {
+    g.prisma = new PrismaClient();
+  }
+
+  prisma = g.prisma;
+}
 
 export default prisma;
