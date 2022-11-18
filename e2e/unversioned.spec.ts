23d6c42e814e9ec80b082d79d80f58cf4972ccde
Patrick Browne <pt.browne@gmail.com>
2022-11-18 13:30:15 +0100

    fix: Update test

--- e2e/unversioned.spec.ts
+++ e2e/unversioned.spec.ts
@@ -3,11 +3,13 @@ import { test } from "./common";
 test("Unversioned dataset > should be possible to open a link to an unversioned dataset", async ({
   page,
   screen,
+  actions,
 }) => {
-  await page.goto(
-    `/en/browse/dataset/https%3A%2F%2Fculture.ld.admin.ch%2Fsfa%2FStateAccounts_Function?dataSource=Int`
+  await actions.datasetPreview.load(
+    "https://culture.ld.admin.ch/sfa/StateAccounts_Function",
+    "Int"
   );
-  await screen.findByText("State accounts - Function", undefined, {
+  await screen.findAllByText("State accounts - Function", undefined, {
     timeout: 10 * 1000,
   });
 });
