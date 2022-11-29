bb6dbd344f2c38dd83f9eb0bcb53dc07d1476e9b
Patrick Browne <pt.browne@gmail.com>
2022-11-29 11:03:17 +0100

    fix: Dataset has changed column order

--- e2e/filters.spec.ts
+++ e2e/filters.spec.ts
@@ -22,9 +22,9 @@ describe("Filters", () => {
       "1. Production region",
       "2. Stand structure",
       // ---
-      "3. Evaluation type",
-      "4. Reference area",
-      "5. Grid",
+      "3. Inventory",
+      "4. Evaluation type",
+      "5. Reference area",
     ]);
 
     const productionRegionFilter = selectors.edition.dataFilterInput(
