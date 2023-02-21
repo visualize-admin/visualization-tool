2027512e0bfb438c0781603f1bf21bd7cb528491
Patrick Browne <pt.browne@gmail.com>
2023-02-21 14:23:05 +0100

    fix: Disable end to end tests for cube that have been removed

--- e2e/query-hierarchies.spec.ts
+++ e2e/query-hierarchies.spec.ts
@@ -73,8 +73,7 @@ export const DimensionHierarchyDocument = gql`
 `;
 
 const cubeIris = {
-  "C-96-r":
-    "https://environment.ld.admin.ch/foen/nfi/C-96-r-multilingual/cube/1",
+  "C-96-r": "https://environment.ld.admin.ch/foen/nfi/C-96-r/cube/1",
   "C-96-r-multilingual":
     "https://environment.ld.admin.ch/foen/nfi/C-96-r-multilingual/cube/1",
   "C-96": "https://environment.ld.admin.ch/foen/nfi/C-96/cube/1",
@@ -112,7 +111,26 @@ const runTest = async ({
 };
 
 describe("multi root hierarchy retrieval", () => {
-  test("should work for C-96-r", async () => {
+  test("should work for C-96", async () => {
+    await runTest({
+      cubeIri: cubeIris["C-96"],
+      locale: "en",
+      expected: {
+        root: "Switzerland",
+        children: [
+          "Production region - Economic Region",
+          "Canton",
+          "Protection Forest region - Economic Region",
+        ],
+      },
+    });
+  });
+
+  /**
+   * Tests below are skipped since cubes seem to have changed
+   * @see https://zulip.zazuko.com/#narrow/stream/40-bafu-ext/topic/c96-cubes/near/321453
+   */
+  test.skip("should work for C-96-r", async () => {
     await runTest({
       cubeIri: cubeIris["C-96-r"],
       locale: "de",
@@ -127,7 +145,7 @@ describe("multi root hierarchy retrieval", () => {
     });
   });
 
-  test("should work for C-96-r-multilingual", async ({}) => {
+  test.skip("should work for C-96-r-multilingual", async ({}) => {
     await runTest({
       cubeIri: cubeIris["C-96-r-multilingual"],
       locale: "en",
@@ -142,22 +160,7 @@ describe("multi root hierarchy retrieval", () => {
     });
   });
 
-  test("should work for C-96", async () => {
-    await runTest({
-      cubeIri: cubeIris["C-96"],
-      locale: "en",
-      expected: {
-        root: "Switzerland",
-        children: [
-          "Canton",
-          "Production Region - Economic Region",
-          "Protection Region - Economic Region",
-        ],
-      },
-    });
-  });
-
-  test("should work for C-96-multilingual", async () => {
+  test.skip("should work for C-96-multilingual", async () => {
     await runTest({
       cubeIri: cubeIris["C-96-multilingual"],
       locale: "en",
