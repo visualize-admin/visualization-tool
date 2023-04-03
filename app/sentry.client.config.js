6e3b5bbe48624d1654f1ab2d6099325afe6d0227
Patrick Browne <pt.browne@gmail.com>
2023-04-03 15:57:45 +0200

    feat: Add sentry next js integration
    With this we will get informed and have basic instrumentation in Sentry

---
+++ app/sentry.client.config.js
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry on the browser.
+// The config you add here will be used whenever a page is visited.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+import { SENTRY_DSN } from "./domain/env";
+
+Sentry.init({
+  enabled: process.env.NODE_ENV === "production",
+  dsn: SENTRY_DSN,
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+
+  ignoreErrors: [
+    // The ResizeObserver error is actually not problematic
+    // @see https://forum.sentry.io/t/resizeobserver-loop-limit-exceeded/8402
+    "ResizeObserver loop limit exceeded",
+  ],
+});
---
+++ app/sentry.edge.config.js
@@ -0,0 +1,13 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever middleware or an Edge route handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+import { SENTRY_DSN } from "./domain/env";
+
+Sentry.init({
+  enabled: process.env.NODE_ENV === "production",
+  dsn: SENTRY_DSN,
+  tracesSampleRate: 1.0,
+});
