35a399f4cac9510caa78e80bfa0d6cf47bc124a8
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-06-20 16:31:45 +0200

    refactor: Keep GQL-related tests in a folder

--- k6/index.ts
+++ k6/index.ts
@@ -1,1 +1,4 @@
-export * from "./components";
+// @ts-nocheck - TS complains about duplicate options exports. This is necessary
+// to have one entry point to generate all JS files.
+export * from "./graphql/components";
+export * from "./graphql/observations";
