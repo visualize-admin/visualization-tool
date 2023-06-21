5909dfbea8ce77f59fbe945daa5467d4b21e63c9
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-06-21 14:15:39 +0200

    test: Add an option to override default e2e test context options

--- e2e/common.ts
+++ e2e/common.ts
@@ -2,26 +2,31 @@ import {
   locatorFixtures as fixtures,
   LocatorFixtures as TestingLibraryFixtures,
 } from "@playwright-testing-library/test/fixture";
-import { test as base } from "@playwright/test";
+import { test as base, PlaywrightTestOptions } from "@playwright/test";
 
-import { createActions, Actions } from "./actions";
+import { Actions, createActions } from "./actions";
 import { createSelectors, Selectors } from "./selectors";
 
-const test = base.extend<TestingLibraryFixtures>(fixtures).extend<{
-  selectors: Selectors;
-  actions: Actions;
-}>({
-  selectors: async ({ page, screen, within }, use) => {
-    const ctx = { page, screen, within };
-    const selectors = createSelectors(ctx);
-    await use(selectors);
-  },
-  actions: async ({ page, screen, selectors, within }, use) => {
-    const ctx = { page, screen, selectors, within };
-    const actions = createActions(ctx);
-    await use(actions);
-  },
-});
+export const makeTest = (
+  contextOptions?: PlaywrightTestOptions["contextOptions"]
+) =>
+  base.extend<TestingLibraryFixtures>(fixtures).extend<{
+    selectors: Selectors;
+    actions: Actions;
+  }>({
+    selectors: async ({ page, screen, within }, use) => {
+      const ctx = { page, screen, within };
+      const selectors = createSelectors(ctx);
+      await use(selectors);
+    },
+    actions: async ({ page, screen, selectors, within }, use) => {
+      const ctx = { page, screen, selectors, within };
+      const actions = createActions(ctx);
+      await use(actions);
+    },
+    contextOptions,
+  });
+const test = makeTest();
 const { expect, describe } = test;
 const it = test;
 
@@ -41,4 +46,4 @@ test.afterEach(async ({ page }, testInfo) => {
 
 export const sleep = (dur: number) => new Promise((r) => setTimeout(r, dur));
 
-export { expect, test, describe, it };
+export { describe, expect, it, test };
