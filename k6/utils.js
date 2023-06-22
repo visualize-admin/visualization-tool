d201018eda39df195e20bd949d06287499df0f66
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-06-22 15:45:49 +0200

    fix: Only one distribution zone is allowed

--- k6/utils.js
+++ k6/utils.js
@@ -1,7 +1,5 @@
 export const PROJECT_ID = 3643757;
 
 export const DISTRIBUTION = {
-  frankfurt: { loadZone: "amazon:de:frankfurt", percent: 25 },
-  milan: { loadZone: "amazon:it:milan", percent: 50 },
-  paris: { loadZone: "amazon:fr:paris", percent: 25 },
+  paris: { loadZone: "amazon:fr:paris", percent: 100 },
 };
