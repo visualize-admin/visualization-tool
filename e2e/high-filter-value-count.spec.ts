867c5ed48f0d42f1f5307b9611d283b0adc383ef
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-06-23 13:06:07 +0200

    fix: High filter value e2e test

--- e2e/high-filter-value-count.spec.ts
+++ e2e/high-filter-value-count.spec.ts
@@ -10,18 +10,17 @@ const testFn = process.env.CI ? test.skip : test;
 testFn(
   "should be able to load a map with a dimension with a large number of values",
   async ({ page, selectors, actions }) => {
-    test.setTimeout(90 * 1000);
+    test.setTimeout(300_000);
     await actions.chart.createFrom(
       "https://environment.ld.admin.ch/foen/fab_hierarchy_test13_switzerland_canton_municipality/3",
       "Int",
       { timeout: 60 * 1000 }
     );
-
     await page
       .locator("h5", { hasText: "Chart Type" })
       .waitFor({ timeout: 30_000 });
     await actions.editor.changeChartType("Map");
-    await selectors.chart.loaded();
+    await selectors.chart.loaded({ timeout: 240_000 });
     await page.screenshot({
       path: `e2e-screenshots/chart-map-high-filter-value-count.png`,
       fullPage: true,
