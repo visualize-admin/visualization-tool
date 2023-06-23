6523c0e24acf96dcc496b0a6ec5634dd0a24deb5
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-06-23 11:31:02 +0200

    fix: Abbreviations e2e test

--- e2e/abbreviations.spec.ts
+++ e2e/abbreviations.spec.ts
@@ -7,6 +7,7 @@ test("it should be possible to enable abbreviations for colors & x field (column
   selectors,
 }) => {
   test.slow();
+
   await actions.chart.createFrom(
     "https://energy.ld.admin.ch/sfoe/bfe_ogd84_einmalverguetung_fuer_photovoltaikanlagen/6",
     "Prod"
@@ -24,7 +25,6 @@ test("it should be possible to enable abbreviations for colors & x field (column
 
   await actions.mui.selectOption("Kanton");
 
-  // Wait for the data to load.
   await selectors.chart.loaded();
   await selectors.edition.filtersLoaded();
 
@@ -32,7 +32,7 @@ test("it should be possible to enable abbreviations for colors & x field (column
 
   await checkbox.click();
 
-  await sleep(5_000);
+  await sleep(3_000);
 
   const xAxis = await selectors.chart.axisWidthBand();
   const ticks = (await xAxis.textContent()) as string;
@@ -42,7 +42,7 @@ test("it should be possible to enable abbreviations for colors & x field (column
 
   await actions.mui.selectOption("Jahr der Vergütung");
 
-  await sleep(5_000);
+  await sleep(3_000);
 
   await actions.drawer.close();
 
@@ -58,16 +58,18 @@ test("it should be possible to enable abbreviations for colors & x field (column
   // Wait for the data to load.
   await selectors.chart.loaded();
   await selectors.edition.filtersLoaded();
-  await selectors.chart.colorLegend(undefined, { timeout: 5_000 });
+  await selectors.chart.colorLegend(undefined, { timeout: 3_000 });
 
-  const legendItems = await (
+  await sleep(3_000);
+
+  const colorLegendItems = await (
     await selectors.chart.colorLegendItems()
   ).allInnerTexts();
 
-  expect([legendItems[0], legendItems[legendItems.length - 1]]).toEqual([
-    "ZH",
-    "JU",
-  ]);
+  expect([
+    colorLegendItems[0],
+    colorLegendItems[colorLegendItems.length - 1],
+  ]).toEqual(["ZH", "JU"]);
 });
 
 test("hierarchies: it should be possible to enable abbreviations for colors", async ({
@@ -75,8 +77,8 @@ test("hierarchies: it should be possible to enable abbreviations for colors", as
   selectors,
 }) => {
   await actions.chart.createFrom(
-    "https://environment.ld.admin.ch/foen/ubd000502_sad_01/7",
-    "Int"
+    "https://environment.ld.admin.ch/foen/ubd000502/4",
+    "Prod"
   );
 
   await actions.editor.selectActiveField("Color");
@@ -92,7 +94,9 @@ test("hierarchies: it should be possible to enable abbreviations for colors", as
   // Wait for the data to load.
   await selectors.chart.loaded();
   await selectors.edition.filtersLoaded();
-  await selectors.chart.colorLegend(undefined, { timeout: 5_000 });
+  await selectors.chart.colorLegend(undefined, { timeout: 3_000 });
+
+  await sleep(3_000);
 
   const legendItems = await (
     await selectors.chart.colorLegendItems()
@@ -117,11 +121,13 @@ test("localized abbreviations", async ({ actions, selectors }) => {
   // Wait for the data to load.
   await selectors.chart.loaded();
   await selectors.edition.filtersLoaded();
-  await selectors.chart.colorLegend(undefined, { timeout: 5_000 });
+  await selectors.chart.colorLegend(undefined, { timeout: 3_000 });
+
+  await sleep(3_000);
 
   const legendItems = await (
     await selectors.chart.colorLegendItems()
   ).allInnerTexts();
 
-  expect(legendItems).toEqual(["No measures", "Warning"]);
+  expect(legendItems.slice(0, 2)).toEqual(["No measures", "Warning"]);
 });
--- e2e/selectors.ts
+++ e2e/selectors.ts
@@ -81,10 +81,20 @@ export const createSelectors = ({ screen, page, within }: Ctx) => {
     },
     chart: {
       axisWidthBand: async () => screen.findByTestId("axis-width-band"),
-      colorLegend: (
+      colorLegend: async (
         options?: MatcherOptions,
         waitForOptions?: waitForOptions
-      ) => screen.findByTestId("colorLegend", options, waitForOptions),
+      ) => {
+        // There can be multiple color legends for hierarchical dimensions.
+        // Generally, we want the first one.
+        const colorLegend = await screen.findAllByTestId(
+          "colorLegend",
+          options,
+          waitForOptions
+        );
+
+        return colorLegend.first();
+      },
       colorLegendItems: async () =>
         (await selectors.chart.colorLegend()).locator("div"),
       legendTicks: async () => {},
