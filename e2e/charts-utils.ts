840e94b2aee7519b93c1e2d7faafc5fb7f89adc9
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-08-09 12:03:00 +0200

    fix: Loading chart into local storage

--- e2e/charts-utils.ts
+++ e2e/charts-utils.ts
@@ -1,14 +1,12 @@
 import { Page } from "@playwright/test";
 
-import { migrateChartConfig } from "../app/utils/chart-config/versioning";
-
 export const loadChartInLocalStorage = async (
   page: Page,
   chartKey: string,
   config: any
 ) => {
   const lsKey = `vizualize-configurator-state:${chartKey}`;
-  const lsValue = `${JSON.stringify(migrateChartConfig(config))}`;
+  const lsValue = `${JSON.stringify(config)}`;
   return page.addInitScript(
     `window.localStorage.setItem(\`${lsKey}\`, \`${lsValue}\`);`
   );
