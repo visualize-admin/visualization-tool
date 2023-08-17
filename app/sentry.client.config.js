ccc5021e88bea966f07e9da463c9a914ac115c49
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-08-17 10:34:33 +0200

    chore: Correctly set Sentry environments

--- app/sentry.client.config.js
+++ app/sentry.client.config.js
@@ -4,14 +4,14 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-import { SENTRY_DSN, BUILD_VERSION } from "./domain/env";
+import { BUILD_VERSION, SENTRY_DSN } from "./domain/env";
+import { getSentryEnv } from "./sentry-utils";
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  release: "visualization-tool@" + BUILD_VERSION,
-  // Adjust this value in production, or use tracesSampler for greater control
+  release: `visualization-tool@${BUILD_VERSION}`,
   tracesSampleRate: 1.0,
-
+  environment: getSentryEnv(),
   ignoreErrors: [
     // The ResizeObserver error is actually not problematic
     // @see https://forum.sentry.io/t/resizeobserver-loop-limit-exceeded/8402
