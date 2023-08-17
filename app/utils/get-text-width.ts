3acc10e83aa2184d7c1599c75f91f31be8f1c6f0
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-08-17 13:01:52 +0200

    fix: Build error

--- app/utils/get-text-width.ts
+++ app/utils/get-text-width.ts
@@ -1,10 +1,16 @@
 import { theme } from "@/themes/federal";
 
-const canvas = document.createElement("canvas");
-const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+let canvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D;
 const fontFamily = theme.typography.fontFamily as string;
 
 export const getTextWidth = (text: string, options: { fontSize: number }) => {
+  if (canvas === undefined && ctx === undefined) {
+    canvas = document.createElement("canvas");
+    ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+  }
+
   ctx.font = `${options.fontSize}px ${fontFamily}`;
+
   return ctx.measureText(text).width;
 };
