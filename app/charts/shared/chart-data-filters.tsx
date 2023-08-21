a2372a950a45c6d7dbca9b059d9d97facf456bed
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-08-21 10:10:32 +0200

    feat: Add cascading filters behavior to interactive filters

--- app/charts/shared/chart-data-filters.tsx
+++ app/charts/shared/chart-data-filters.tsx
@@ -1,7 +1,11 @@
 import { t, Trans } from "@lingui/macro";
-import { Box, Button, SelectChangeEvent } from "@mui/material";
+import { Box, Button, SelectChangeEvent, Typography } from "@mui/material";
+import isEmpty from "lodash/isEmpty";
+import isEqual from "lodash/isEqual";
 import * as React from "react";
+import { useClient } from "urql";
 
+import { useQueryFilters } from "@/charts/shared/chart-helpers";
 import { ChartFiltersList } from "@/components/chart-filters-list";
 import Flex from "@/components/flex";
 import { Select } from "@/components/form";
@@ -11,9 +15,13 @@ import SelectTree from "@/components/select-tree";
 import {
   ChartConfig,
   DataSource,
-  InteractiveFiltersDataConfig,
+  Filters,
+  getFiltersByMappingStatus,
+  QueryFilters,
+  useConfiguratorState,
 } from "@/configurator";
-import { TimeInput } from "@/configurator/components/field";
+import { orderedIsEqual } from "@/configurator/components/chart-configurator";
+import { FieldLabel, TimeInput } from "@/configurator/components/field";
 import {
   getTimeIntervalFormattedSelectOptions,
   getTimeIntervalWithProps,
@@ -25,26 +33,34 @@ import {
   Dimension,
   DimensionMetadataFragment,
   HierarchyValue,
+  PossibleFiltersDocument,
+  PossibleFiltersQuery,
+  PossibleFiltersQueryVariables,
   TemporalDimension,
   TimeUnit,
   useDimensionValuesQuery,
 } from "@/graphql/query-hooks";
 import { Icon } from "@/icons";
 import { useLocale } from "@/locales/use-locale";
-import { useInteractiveFiltersStore } from "@/stores/interactive-filters";
+import {
+  DataFilters,
+  useInteractiveFiltersStore,
+} from "@/stores/interactive-filters";
 import { hierarchyToOptions } from "@/utils/hierarchy";
 
 type ChartDataFiltersProps = {
   dataSet: string;
   dataSource: DataSource;
   chartConfig: ChartConfig;
-  dataFiltersConfig: InteractiveFiltersDataConfig;
 };
 
 export const ChartDataFilters = (props: ChartDataFiltersProps) => {
-  const { dataSet, dataSource, chartConfig, dataFiltersConfig } = props;
+  const { dataSet, dataSource, chartConfig } = props;
+  const dataFilters = useInteractiveFiltersStore((d) => d.dataFilters);
+  const queryFilters = useQueryFilters({ chartConfig });
+  const componentIris = chartConfig.interactiveFiltersConfig?.dataFilters
+    .componentIris as string[];
   const [filtersVisible, setFiltersVisible] = React.useState(false);
-  const { componentIris } = dataFiltersConfig;
 
   React.useEffect(() => {
     if (componentIris.length === 0) {
@@ -52,76 +68,103 @@ export const ChartDataFilters = (props: ChartDataFiltersProps) => {
     }
   }, [componentIris.length]);
 
-  return (
-    <>
-      {dataSet && (
-        <Flex sx={{ flexDirection: "column", my: 4 }}>
-          <Flex
+  const { interactiveFilters, mappedFilters } = React.useMemo(() => {
+    const filtersByMappingStatus = getFiltersByMappingStatus(chartConfig);
+    const { mappedFilters, unmappedFilters } = filtersByMappingStatus;
+    const unmappedKeys = Object.keys(unmappedFilters);
+
+    return {
+      interactiveFilters: Object.fromEntries(
+        Object.entries(queryFilters).filter(([k]) => unmappedKeys.includes(k))
+      ),
+      mappedFilters,
+      unmappedFilters,
+    };
+  }, [chartConfig, queryFilters]);
+
+  const { fetching, error } = useEnsurePossibleInteractiveFilters({
+    dataSet,
+    dataSource,
+    chartConfig,
+    dataFilters,
+    interactiveFilters,
+    mappedFilters,
+  });
+
+  return error ? (
+    <Typography variant="body2" color="error">
+      <Trans id="controls.section.data.filters.possible-filters-error">
+        An error happened while fetching possible filters, please retry later or
+        reload the page.
+      </Trans>
+    </Typography>
+  ) : dataSet ? (
+    <Flex sx={{ flexDirection: "column", my: 4 }}>
+      <Flex
+        sx={{
+          justifyContent: "space-between",
+          alignItems: "flex-start",
+          gap: 3,
+          minHeight: 20,
+        }}
+      >
+        {filtersVisible ? (
+          <Box />
+        ) : (
+          <ChartFiltersList
+            dataSetIri={dataSet}
+            dataSource={dataSource}
+            chartConfig={chartConfig}
+          />
+        )}
+
+        {componentIris.length > 0 && (
+          <Button
+            variant="text"
+            endIcon={<Icon name={filtersVisible ? "close" : "add"} size={15} />}
             sx={{
-              justifyContent: "space-between",
-              alignItems: "flex-start",
-              gap: 3,
-              minHeight: 20,
+              display: "flex",
+              fontSize: ["0.75rem", "0.75rem", "0.75rem"],
+              alignItems: "center",
+              minWidth: "fit-content",
             }}
+            onClick={() => setFiltersVisible(!filtersVisible)}
           >
             {filtersVisible ? (
-              <Box />
+              <Trans id="interactive.data.filters.hide">Hide Filters</Trans>
             ) : (
-              <ChartFiltersList
-                dataSetIri={dataSet}
-                dataSource={dataSource}
-                chartConfig={chartConfig}
-              />
+              <Trans id="interactive.data.filters.show">Show Filters</Trans>
             )}
+          </Button>
+        )}
+      </Flex>
 
-            {componentIris.length > 0 && (
-              <Button
-                variant="text"
-                endIcon={
-                  <Icon name={filtersVisible ? "close" : "add"} size={15} />
-                }
-                sx={{
-                  display: "flex",
-                  fontSize: ["0.75rem", "0.75rem", "0.75rem"],
-                  alignItems: "center",
-                  minWidth: "fit-content",
-                }}
-                onClick={() => setFiltersVisible(!filtersVisible)}
-              >
-                {filtersVisible ? (
-                  <Trans id="interactive.data.filters.hide">Hide Filters</Trans>
-                ) : (
-                  <Trans id="interactive.data.filters.show">Show Filters</Trans>
-                )}
-              </Button>
-            )}
-          </Flex>
-
-          {componentIris.length > 0 && (
-            <Box
-              data-testid="published-chart-interactive-filters"
-              sx={{
-                display: filtersVisible ? "grid" : "none",
-                columnGap: 3,
-                rowGap: 2,
-                gridTemplateColumns: "repeat(auto-fit, minmax(240px, 1fr))",
-              }}
-            >
-              {componentIris.map((d) => (
-                <DataFilter
-                  key={d}
-                  dataSetIri={dataSet}
-                  dataSource={dataSource}
-                  chartConfig={chartConfig}
-                  dimensionIri={d}
-                />
-              ))}
-            </Box>
-          )}
-        </Flex>
+      {componentIris.length > 0 && (
+        <Box
+          data-testid="published-chart-interactive-filters"
+          sx={{
+            display: filtersVisible ? "grid" : "none",
+            columnGap: 3,
+            rowGap: 2,
+            gridTemplateColumns: "repeat(auto-fit, minmax(240px, 1fr))",
+          }}
+        >
+          {componentIris.map((iri) => (
+            <DataFilter
+              key={iri}
+              dimensionIri={iri}
+              dataSetIri={dataSet}
+              dataSource={dataSource}
+              chartConfig={chartConfig}
+              dataFilters={dataFilters}
+              interactiveFilters={interactiveFilters}
+              fetching={fetching}
+            />
+          ))}
+        </Box>
       )}
-    </>
-  );
+    </Flex>
+  ) : null;
 };
 
 type DataFilterProps = {
@@ -129,24 +172,47 @@ type DataFilterProps = {
   dataSetIri: string;
   dataSource: DataSource;
   chartConfig: ChartConfig;
+  dataFilters: DataFilters;
+  interactiveFilters: QueryFilters;
+  fetching: boolean;
 };
 
 const DataFilter = (props: DataFilterProps) => {
-  const { dimensionIri, dataSetIri, dataSource, chartConfig } = props;
-  const dataFilters = useInteractiveFiltersStore((d) => d.dataFilters);
+  const {
+    dimensionIri,
+    dataSetIri,
+    dataSource,
+    chartConfig,
+    dataFilters,
+    interactiveFilters,
+    fetching,
+  } = props;
+  const locale = useLocale();
   const updateDataFilter = useInteractiveFiltersStore(
     (d) => d.updateDataFilter
   );
-  const locale = useLocale();
+  const keys = Object.keys(interactiveFilters);
   const [{ data }] = useDimensionValuesQuery({
     variables: {
       dimensionIri,
+      dataCubeIri: dataSetIri,
       sourceType: dataSource.type,
       sourceUrl: dataSource.url,
       locale,
-      dataCubeIri: dataSetIri,
+      ...(keys.length > 0
+        ? {
+            filters: interactiveFilters,
+            // This is important for urql not to think that filters
+            // are the same  while the order of the keys has changed.
+            // If this is not present, we'll have outdated dimension
+            // values after we change the filter order
+            filterKeys: Object.keys(keys).join(", "),
+          }
+        : {}),
     },
   });
+
+  const dimension = data?.dataCubeByIri?.dimensionByIri;
   const hierarchy = data?.dataCubeByIri?.dimensionByIri?.hierarchy;
 
   const setDataFilter = (
@@ -155,59 +221,65 @@ const DataFilter = (props: DataFilterProps) => {
     updateDataFilter(dimensionIri, e.target.value as string);
   };
 
-  if (data?.dataCubeByIri?.dimensionByIri) {
-    const dimension = data?.dataCubeByIri?.dimensionByIri;
+  const configFilter = dimension
+    ? chartConfig.filters[dimension.iri]
+    : undefined;
+  const configFilterValue =
+    configFilter && configFilter.type === "single"
+      ? configFilter.value
+      : undefined;
+  const value =
+    (dimension && dataFilters?.[dimension.iri]?.value) ??
+    configFilterValue ??
+    FIELD_VALUE_NONE;
 
-    const configFilter = chartConfig.filters[dimension.iri];
-    const configFilterValue =
-      configFilter && configFilter.type === "single"
-        ? configFilter.value
-        : undefined;
+  React.useEffect(() => {
+    if (dimension?.values) {
+      updateDataFilter(dimension.iri, value);
+    }
+  }, [dimension?.iri, dimension?.values, updateDataFilter, value]);
 
-    const value =
-      dataFilters?.[dimension.iri]?.value ??
-      configFilterValue ??
-      FIELD_VALUE_NONE;
-
-    return (
-      <Flex
-        sx={{
-          mr: 3,
-          width: "100%",
-          flex: "1 1 100%",
-          ":last-of-type": {
-            mr: 0,
-          },
-          " > div": { width: "100%" },
-        }}
-      >
-        {isTemporalDimension(dimension) ? (
-          dimension.timeUnit === TimeUnit.Year ? (
-            <DataFilterTemporalDimension
-              value={value as string}
-              dimension={dimension}
-              onChange={setDataFilter}
-            />
-          ) : null
-        ) : hierarchy ? (
-          <DataFilterHierarchyDimension
+  return dimension ? (
+    <Flex
+      sx={{
+        mr: 3,
+        width: "100%",
+        flex: "1 1 100%",
+        ":last-of-type": {
+          mr: 0,
+        },
+        " > div": { width: "100%" },
+      }}
+    >
+      {isTemporalDimension(dimension) ? (
+        dimension.timeUnit === TimeUnit.Year ? (
+          <DataFilterTemporalDimension
+            value={value as string}
             dimension={dimension}
             onChange={setDataFilter}
-            hierarchy={hierarchy}
-            value={value as string}
+            fetching={fetching}
           />
-        ) : (
-          <DataFilterGenericDimension
-            dimension={dimension}
-            onChange={setDataFilter}
-            value={value as string}
-          />
-        )}
-      </Flex>
-    );
-  } else {
-    return <Loading />;
-  }
+        ) : null
+      ) : hierarchy ? (
+        <DataFilterHierarchyDimension
+          dimension={dimension}
+          onChange={setDataFilter}
+          hierarchy={hierarchy}
+          value={value as string}
+          fetching={fetching}
+        />
+      ) : (
+        <DataFilterGenericDimension
+          dimension={dimension}
+          onChange={setDataFilter}
+          value={value as string}
+          fetching={fetching}
+        />
+      )}
+    </Flex>
+  ) : (
+    <Loading />
+  );
 };
 
 type DataFilterGenericDimensionProps = {
@@ -215,14 +287,15 @@ type DataFilterGenericDimensionProps = {
   value: string;
   onChange: (e: SelectChangeEvent<unknown>) => void;
   options?: Array<{ label: string; value: string }>;
+  fetching: boolean;
 };
 
 const DataFilterGenericDimension = (props: DataFilterGenericDimensionProps) => {
-  const { dimension, value, onChange, options: propOptions } = props;
+  const { dimension, value, onChange, options: propOptions, fetching } = props;
   const { label, isKeyDimension } = dimension;
   const noneLabel = t({
     id: "controls.dimensionvalue.none",
-    message: `No Filter`,
+    message: "No Filter",
   });
   const options = propOptions ?? dimension.values;
   const allOptions = React.useMemo(() => {
@@ -242,13 +315,21 @@ const DataFilterGenericDimension = (props: DataFilterGenericDimensionProps) => {
     <Select
       id="dataFilterBaseDimension"
       label={
-        <OpenMetadataPanelWrapper dim={dimension as DimensionMetadataFragment}>
-          {label}
-        </OpenMetadataPanelWrapper>
+        <FieldLabel
+          label={
+            <OpenMetadataPanelWrapper
+              dim={dimension as DimensionMetadataFragment}
+            >
+              {label}
+            </OpenMetadataPanelWrapper>
+          }
+          isFetching={fetching}
+        />
       }
       options={allOptions}
       value={value}
       onChange={onChange}
+      disabled={fetching}
     />
   );
 };
@@ -258,12 +339,13 @@ type DataFilterHierarchyDimensionProps = {
   value: string;
   onChange: (e: { target: { value: string } }) => void;
   hierarchy?: HierarchyValue[];
+  fetching: boolean;
 };
 
 const DataFilterHierarchyDimension = (
   props: DataFilterHierarchyDimensionProps
 ) => {
-  const { dimension, value, onChange, hierarchy } = props;
+  const { dimension, value, onChange, hierarchy, fetching } = props;
   const { label, isKeyDimension, values: dimensionValues } = dimension;
   const noneLabel = t({
     id: "controls.dimensionvalue.none",
@@ -295,10 +377,18 @@ const DataFilterHierarchyDimension = (
       options={options}
       onChange={onChange}
       label={
-        <OpenMetadataPanelWrapper dim={dimension as DimensionMetadataFragment}>
-          {label}
-        </OpenMetadataPanelWrapper>
+        <FieldLabel
+          label={
+            <OpenMetadataPanelWrapper
+              dim={dimension as DimensionMetadataFragment}
+            >
+              {label}
+            </OpenMetadataPanelWrapper>
+          }
+          isFetching={fetching}
+        />
       }
+      disabled={fetching}
     />
   );
 };
@@ -307,10 +397,12 @@ const DataFilterTemporalDimension = ({
   dimension,
   value,
   onChange,
+  fetching,
 }: {
   dimension: TemporalDimension;
   value: string;
   onChange: (e: SelectChangeEvent<unknown>) => void;
+  fetching: boolean;
 }) => {
   const {
     isKeyDimension,
@@ -319,7 +411,6 @@ const DataFilterTemporalDimension = ({
     timeUnit,
     timeFormat,
   } = dimension;
-
   const formatLocale = useTimeFormatLocale();
   const timeIntervalWithProps = React.useMemo(() => {
     return getTimeIntervalWithProps(
@@ -342,6 +433,7 @@ const DataFilterTemporalDimension = ({
         options={timeIntervalOptions}
         value={value}
         onChange={onChange}
+        fetching={fetching}
       />
     );
   }
@@ -361,4 +453,111 @@ const DataFilterTemporalDimension = ({
       onChange={onChange}
     />
   );
+};
+
+type EnsurePossibleInteractiveFiltersProps = {
+  dataSet: string;
+  dataSource: DataSource;
+  chartConfig: ChartConfig;
+  dataFilters: DataFilters;
+  interactiveFilters: Filters;
+  mappedFilters: Filters;
+};
+
+/**
+ * This runs every time the state changes and it ensures that the selected interactive
+ * filters return at least 1 observation. Otherwise they are reloaded.
+ */
+const useEnsurePossibleInteractiveFilters = (
+  props: EnsurePossibleInteractiveFiltersProps
+) => {
+  const {
+    dataSet,
+    dataSource,
+    chartConfig,
+    dataFilters,
+    interactiveFilters,
+    mappedFilters,
+  } = props;
+  const [, dispatch] = useConfiguratorState();
+  const [fetching, setFetching] = React.useState(false);
+  const [error, setError] = React.useState<Error>();
+  const lastFilters = React.useRef<Filters>();
+  const client = useClient();
+  const setDataFilters = useInteractiveFiltersStore((d) => d.setDataFilters);
+
+  React.useEffect(() => {
+    const run = async () => {
+      if (
+        lastFilters.current &&
+        orderedIsEqual(lastFilters.current, interactiveFilters)
+      ) {
+        return;
+      }
+      lastFilters.current = interactiveFilters;
+
+      setFetching(true);
+      const { data, error } = await client
+        .query<PossibleFiltersQuery, PossibleFiltersQueryVariables>(
+          PossibleFiltersDocument,
+          {
+            iri: dataSet,
+            sourceType: dataSource.type,
+            sourceUrl: dataSource.url,
+            filters: interactiveFilters,
+            // @ts-ignore This is to make urql requery
+            filterKey: Object.keys(interactiveFilters).join(", "),
+          }
+        )
+        .toPromise();
+
+      if (error || !data) {
+        setError(error);
+        setFetching(false);
+        console.error("Could not fetch possible filters", error);
+
+        return;
+      }
+
+      setError(undefined);
+      setFetching(false);
+
+      const filters = Object.assign(
+        Object.fromEntries(
+          data.possibleFilters.map((d) => [
+            d.iri,
+            { type: d.type, value: d.value },
+          ])
+        ) as Filters,
+        mappedFilters
+      );
+
+      if (!isEqual(filters, chartConfig.filters) && !isEmpty(filters)) {
+        for (const [k, v] of Object.entries(filters)) {
+          if (k in dataFilters && v.type === "single") {
+            dataFilters[k] = v;
+          }
+        }
+
+        setDataFilters(dataFilters);
+      }
+    };
+
+    run();
+  }, [
+    client,
+    dispatch,
+    chartConfig.fields,
+    chartConfig.filters,
+    dataSet,
+    dataSource.type,
+    dataSource.url,
+    chartConfig,
+    setDataFilters,
+    dataFilters,
+    interactiveFilters,
+    mappedFilters,
+  ]);
+
+  return { error, fetching };
 };
