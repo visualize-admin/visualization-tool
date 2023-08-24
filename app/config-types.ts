2fcd8eafdb31c4c8fbab9e41aed5ffbce768d79e
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-08-24 10:21:50 +0200

    fix: Guarantee interactive filters order to match filters order

--- app/config-types.ts
+++ app/config-types.ts
@@ -20,22 +20,32 @@ const ComponentType = t.union([
 export type ComponentType = t.TypeOf<typeof ComponentType>;
 
 // Filters
-const FilterValueMulti = t.type(
-  {
-    type: t.literal("multi"),
-    values: t.record(t.string, t.literal(true)), // undefined values will be removed when serializing to JSON
-  },
-  "FilterValueMulti"
-);
+const FilterValueMulti = t.intersection([
+  t.type(
+    {
+      type: t.literal("multi"),
+      values: t.record(t.string, t.literal(true)), // undefined values will be removed when serializing to JSON
+    },
+    "FilterValueMulti"
+  ),
+  t.partial({
+    position: t.number,
+  }),
+]);
 export type FilterValueMulti = t.TypeOf<typeof FilterValueMulti>;
 
-const FilterValueSingle = t.type(
-  {
-    type: t.literal("single"),
-    value: t.union([t.string, t.number]),
-  },
-  "FilterValueSingle"
-);
+const FilterValueSingle = t.intersection([
+  t.type(
+    {
+      type: t.literal("single"),
+      value: t.union([t.string, t.number]),
+    },
+    "FilterValueSingle"
+  ),
+  t.partial({
+    position: t.number,
+  }),
+]);
 export type FilterValueSingle = t.TypeOf<typeof FilterValueSingle>;
 
 export const isFilterValueSingle = (
@@ -44,14 +54,19 @@ export const isFilterValueSingle = (
   return filterValue.type === "single";
 };
 
-const FilterValueRange = t.type(
-  {
-    type: t.literal("range"),
-    from: t.string,
-    to: t.string,
-  },
-  "FilterValueRange"
-);
+const FilterValueRange = t.intersection([
+  t.type(
+    {
+      type: t.literal("range"),
+      from: t.string,
+      to: t.string,
+    },
+    "FilterValueRange"
+  ),
+  t.partial({
+    position: t.number,
+  }),
+]);
 export type FilterValueRange = t.TypeOf<typeof FilterValueRange>;
 
 const FilterValue = t.union(
