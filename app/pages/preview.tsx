9b1da156d7b8f9b5c4d9c9cd17c3edefb57c81e0
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-08-31 10:30:04 +0200

    feat: Add GET preview

--- app/pages/preview.tsx
+++ app/pages/preview.tsx
@@ -1,57 +1,45 @@
-import "core-js/features/array/flat-map";
-
 import { I18nProvider } from "@lingui/react";
 import { ThemeProvider } from "@mui/material";
-import { GetServerSideProps } from "next";
+import create, { useStore } from "zustand";
 
 import { ChartPublished } from "@/components/chart-published";
+import { ConfiguratorStatePublishing } from "@/config-types";
 import { GraphqlProvider } from "@/graphql/GraphqlProvider";
 import { Locale, i18n } from "@/locales/locales";
 import { LocaleProvider } from "@/locales/use-locale";
 import * as federalTheme from "@/themes/federal";
 
-const streamToString = async (stream: any) => {
-  if (stream) {
-    const chunks = [];
+const chartStateStore = create<{
+  state: ConfiguratorStatePublishing | null;
+  setState: (state: ConfiguratorStatePublishing) => void;
+}>((set) => ({
+  state: null,
+  setState: (state) => set({ state }),
+}));
 
-    for await (const chunk of stream) {
-      chunks.push(Buffer.from(chunk));
+if (typeof window !== "undefined") {
+  window.addEventListener("message", (event) => {
+    if (event.data.state === "CONFIGURING_CHART") {
+      chartStateStore.setState({ state: event.data });
     }
+  });
+}
 
-    return Buffer.concat(chunks).toString("utf-8");
-  }
-
-  return null;
-};
-
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  let state: any | null = null;
-
-  if (req.method === "POST") {
-    const body = (await streamToString(req)) as string;
-    state = JSON.parse(body);
-  }
-
-  return {
-    props: {
-      ...state,
-    },
-  };
-};
-
-export default function Preview(props: any) {
+export default function Preview() {
   const locale: Locale = "en";
   i18n.activate(locale);
 
-  return (
+  const state = useStore(chartStateStore, (d) => d.state);
+
+  return state ? (
     <LocaleProvider value={locale}>
       <I18nProvider i18n={i18n}>
         <GraphqlProvider>
           <ThemeProvider theme={federalTheme.theme}>
-            <ChartPublished configKey="preview" {...props} />
+            <ChartPublished configKey="preview" {...state} />
           </ThemeProvider>
         </GraphqlProvider>
       </I18nProvider>
     </LocaleProvider>
-  );
+  ) : null;
 }
