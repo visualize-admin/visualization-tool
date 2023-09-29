383450a2357a3dfe4b2536a847e5295cbef6a034
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-09-29 14:13:44 +0200

    feat: Initial ComboLineSingle

--- app/charts/combo/combo-line-single.tsx
+++ app/charts/combo/combo-line-single.tsx
@@ -1,9 +1,42 @@
+import * as d3 from "d3";
+import React from "react";
+
 import { ComboLineSingleState } from "@/charts/combo/combo-line-single-state";
 import { useChartState } from "@/charts/shared/chart-state";
+import { Observation } from "@/domain/data";
 
 export const ComboLineSingle = () => {
-  const state = useChartState() as ComboLineSingleState;
-  console.log(state);
+  const { chartData, xScale, getX, yScale, y, colors, bounds } =
+    useChartState() as ComboLineSingleState;
 
-  return null;
+  return (
+    <g transform={`translate(${bounds.margins.left} ${bounds.margins.top})`}>
+      {y.lines.map(({ iri, label, getY }) => {
+        const line = d3
+          .line<Observation>()
+          .defined((d) => getY(d) !== null)
+          .x((d) => xScale(getX(d)))
+          .y((d) => yScale(getY(d) as number));
+
+        return (
+          <Line
+            key={iri}
+            path={line(chartData) as string}
+            color={colors(label)}
+          />
+        );
+      })}
+    </g>
+  );
 };
+
+type LineProps = {
+  path: string;
+  color: string;
+};
+
+const Line = React.memo(function Line(props: LineProps) {
+  const { path, color } = props;
+
+  return <path d={path} stroke={color} fill="none" />;
+});
