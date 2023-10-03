d129ec4208a39eb372928a324855160edeb876df
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-10-03 13:57:31 +0200

    chore: Remove unused

--- app/utils/hierarchy.ts
+++ app/utils/hierarchy.ts
@@ -1,8 +1,4 @@
-import { ascending } from "d3";
-
 import { SelectTreeProps } from "@/components/select-tree";
-import { Option, OptionGroup } from "@/configurator";
-import { HierarchyParents } from "@/configurator/components/use-hierarchy-parents";
 import { DimensionValue } from "@/domain/data";
 import { truthy } from "@/domain/types";
 import { HierarchyValue } from "@/graphql/resolver-types";
@@ -15,23 +11,6 @@ const asGroup = (
     label: parents.map((p) => p.label).join(" > "),
     value: parents.map((p) => p.value).join("$"),
   };
-};
-
-export const makeOptionGroups = (
-  hierarchyParents: HierarchyParents | undefined
-) => {
-  if (!hierarchyParents) {
-    return undefined;
-  }
-  return hierarchyParents
-    .map(
-      ([parents, dfsRes]) =>
-        [asGroup(parents), dfsRes.map((d) => d.node)] as [
-          OptionGroup,
-          (Option & { depth: number })[]
-        ]
-    )
-    .sort((a, b) => ascending(a[1][0].depth, b[1][0].depth));
 };
 
 export const hierarchyToOptions = (
