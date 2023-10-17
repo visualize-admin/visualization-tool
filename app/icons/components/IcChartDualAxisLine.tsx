8251a3548e06e97e969d943db44a25846b51d042
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-10-17 11:48:02 +0200

    fix: Highlighting of combo chart column icons

--- app/icons/components/IcChartDualAxisLine.tsx
+++ app/icons/components/IcChartDualAxisLine.tsx
@@ -8,20 +8,34 @@ function SvgIcChartDualAxisLine(props: React.SVGProps<SVGSVGElement>) {
       height="1em"
       {...props}
     >
+      <filter id="grayscale">
+        <feColorMatrix
+          type="matrix"
+          values="0.3333 0.3333 0.3333 0 0
+    0.3333 0.3333 0.3333 0 0
+    0.3333 0.3333 0.3333 0 0
+    0 0 0 1 0"
+        />
+      </filter>
       <path
         d="M20.346 12.923l.078-.073-.078-.073-1.057-.99-.068-.064-.069.064-4.633 4.34-3.95-3.7-.069-.064-.068.064-1.057.99-.078.073.078.073 5.075 4.755.069.064.068-.064zM6.875 11.064l.068.064.069-.064 1.056-.99.078-.072-.078-.073-2.086-1.955-.069-.064-.068.064-2.023 1.895-.078.073.078.073 1.057.99.068.064.069-.064.897-.841z"
-        fill="#757575"
-        stroke="#757575"
+        fill="currentColor"
+        stroke="currentColor"
         strokeWidth={0.2}
+        filter="url(#grayscale)"
       />
       <path
         d="M4.988 16.163l.069.064.068-.064 5.754-5.391 2.547 2.386.068.064.069-.064 6.505-6.095.078-.073-.078-.073-1.056-.99-.069-.064-.068.064-5.38 5.04-2.547-2.385-.069-.064-.068.064-6.88 6.445-.077.073.078.073z"
-        fill="#069"
-        stroke="#069"
+        fill="currentColor"
+        stroke="currentColor"
         strokeWidth={0.2}
       />
-      <path d="M1 2h1.396v20H1z" fill="#069" />
-      <path d="M21.602 2h1.396v20h-1.396z" fill="#757575" />
+      <path d="M1 2h1.396v20H1z" fill="currentColor" />
+      <path
+        d="M21.602 2h1.396v20h-1.396z"
+        fill="currentColor"
+        filter="url(#grayscale)"
+      />
     </svg>
   );
 }
--- app/icons/components/IcChartMultiLine.tsx
+++ app/icons/components/IcChartMultiLine.tsx
@@ -8,14 +8,24 @@ function SvgIcChartMultiLine(props: React.SVGProps<SVGSVGElement>) {
       height="1em"
       {...props}
     >
+      <filter id="grayscale">
+        <feColorMatrix
+          type="matrix"
+          values="0.3333 0.3333 0.3333 0 0
+    0.3333 0.3333 0.3333 0 0
+    0.3333 0.3333 0.3333 0 0
+    0 0 0 1 0"
+        />
+      </filter>
       <g fillRule="evenodd">
         <path
           d="M23.003 12.848l-1.452-1.405-6.465 6.249-5.526-5.34-1.453 1.403 6.979 6.745zM3.785 9.574l1.416 1.368 1.453-1.404-2.869-2.772-2.781 2.688 1.453 1.404 1.328-1.284z"
-          fill="#757575"
+          fill="currentColor"
+          filter="url(#grayscale)"
         />
         <path
           d="M10.463 10.074l-8.006 7.738-1.453-1.404 9.459-9.142 3.596 3.475L21.55 3.5l1.453 1.404-8.945 8.646z"
-          fill="#006799"
+          fill="currentColor"
         />
       </g>
     </svg>
