3e468c826f88ca47cad9bd662ea19bda74a38b8f
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-11-13 14:06:06 +0100

    docs: Add docstring

--- app/rdf/extended-cube.ts
+++ app/rdf/extended-cube.ts
@@ -3,6 +3,17 @@ import { Cube, CubeOptions } from "rdf-cube-view-query";
 import rdf from "rdf-ext";
 import DatasetExt from "rdf-ext/lib/Dataset";
 
+/** Extended `rdf-cube-view-query` Cube created to address some of the issues with
+ * the original Cube class.
+ *
+ * The main with the original Cube is that it stores the shape in the same dataset
+ * as the cube data. This means we can't distinguish between the two sources,
+ * which is a problem when we want to query hierarchies (as they could be duplicated
+ * in the shape and the data).
+ *
+ * ExtendedCube is envisioned to be enhanced with additional methods to support
+ * e.g. querying hierarchies and preventing overfetching in the future.
+ */
 export class ExtendedCube extends Cube {
   private shapeDataset: DatasetExt;
   public shapePtr: AnyPointer;
