b7fd1620907a45a69479e95d71c78fcd61568a3c
Bartosz Prusinowski <bartoszprusinowski1@gmail.com>
2023-11-16 10:07:43 +0100

    chore: Remove unused

--- app/rdf/query-cube-metadata.ts
+++ app/rdf/query-cube-metadata.ts
@@ -1,9 +1,9 @@
 import RDF from "@rdfjs/data-model";
-import { SELECT, sparql } from "@tpluscode/sparql-builder";
+import { SELECT } from "@tpluscode/sparql-builder";
 import keyBy from "lodash/keyBy";
 import { ParsingClient } from "sparql-http-client/ParsingClient";
 
-import { schema, dcat, dcterms, cube } from "../../app/rdf/namespace";
+import { cube, schema } from "../../app/rdf/namespace";
 import { DataCubeOrganization, DataCubeTheme } from "../graphql/query-hooks";
 
 import { pragmas } from "./create-source";
@@ -54,8 +54,7 @@ export const createThemeLoader =
       });
     }
 
-    const parsed = results.map(parseSparqlTheme);
-    return parsed;
+    return results.map(parseSparqlTheme);
   };
 
 export const createOrganizationLoader =
@@ -80,118 +79,8 @@ export const createOrganizationLoader =
       });
     }
 
-    const parsed = results.map(parseSparqlOrganization);
-    return parsed;
+    return results.map(parseSparqlOrganization);
   };
-
-export const loadThemes = ({
-  locale,
-  sparqlClient,
-}: {
-  locale: string;
-  sparqlClient: ParsingClient;
-}) => {
-  return createThemeLoader({ locale, sparqlClient })();
-};
-
-export const loadOrganizations = ({
-  locale,
-  sparqlClient,
-}: {
-  locale: string;
-  sparqlClient: ParsingClient;
-}) => {
-  return createOrganizationLoader({ locale, sparqlClient })();
-};
-
-export const queryDatasetCountByOrganization = async ({
-  sparqlClient,
-  theme,
-  includeDrafts,
-}: {
-  sparqlClient: ParsingClient;
-  theme?: string;
-  includeDrafts?: boolean;
-}) => {
-  const query = SELECT`(count(?iri) as ?count) ?creator`.WHERE`
-    ?iri ${dcterms.creator} ?creator.
-    ${theme ? sparql`?iri ${dcat.theme} <${theme}>.` : ``}
-    ${makeVisualizeDatasetFilter({ includeDrafts })}
-  `.GROUP().BY`?creator`.build();
-  const results = await sparqlClient.query.select(query, {
-    operation: "postUrlencoded",
-  });
-  return results
-    .map((r) => {
-      return {
-        count: parseInt(r.count.value, 10),
-        iri: r.creator?.value,
-      };
-    })
-    .filter((r) => r.iri);
-};
-
-export const queryDatasetCountByTheme = async ({
-  sparqlClient,
-  organization,
-  includeDrafts,
-}: {
-  sparqlClient: ParsingClient;
-  organization?: string;
-  includeDrafts?: boolean;
-}) => {
-  const query = SELECT`(count(?iri) as ?count) ?theme`.WHERE`
-    ?iri ${dcat.theme} ?theme.
-    ${organization ? sparql`?iri ${dcterms.creator} <${organization}>.` : ``}
-    ?theme ${
-      schema.inDefinedTermSet
-    } <https://register.ld.admin.ch/opendataswiss/category>.
-    ${makeVisualizeDatasetFilter({ includeDrafts })}
-  `.GROUP().BY`?theme`.build();
-  const results = await sparqlClient.query.select(query, {
-    operation: "postUrlencoded",
-  });
-  return results
-    .map((r) => {
-      return {
-        count: parseInt(r.count.value, 10),
-        iri: r.theme?.value,
-      };
-    })
-    .filter((r) => r.iri);
-};
-
-export const queryDatasetCountBySubTheme = async ({
-  sparqlClient,
-  organization,
-  theme,
-  includeDrafts,
-}: {
-  sparqlClient: ParsingClient;
-  organization?: string;
-  theme?: string;
-  includeDrafts?: boolean;
-}) => {
-  const baseQuery = SELECT`(count(?iri) as ?count) ?subtheme`.WHERE`
-    ?iri ${schema.about} ?subtheme.
-    ${organization ? sparql`?iri ${dcterms.creator} <${organization}>.` : ``}
-    ${theme ? sparql`?iri ${dcat.theme} <${theme}>.` : ``}
-    ?iri ${schema.about} ?subtheme. 
-    ${makeVisualizeDatasetFilter({ includeDrafts })}
-  `.build();
-  const query = `${baseQuery} GROUP BY ?subtheme`;
-  const results = await sparqlClient.query.select(query, {
-    operation: "postUrlencoded",
-  });
-  return results
-    .map((r) => {
-      return {
-        count: parseInt(r.count.value, 10),
-        iri: r.subtheme?.value,
-      };
-    })
-    .filter((r) => r.iri);
-};
 
 export const queryLatestPublishedCubeFromUnversionedIri = async (
   sparqlClient: ParsingClient,
@@ -227,28 +116,4 @@ export const queryLatestPublishedCubeFromUnversionedIri = async (
   return {
     iri: results[0].iri.value,
   };
-};
-
-export const loadSubthemes = async ({
-  sparqlClient,
-  parentIri,
-  locale,
-}: {
-  sparqlClient: ParsingClient;
-  parentIri: string;
-  locale: string;
-}) => {
-  const query = SELECT`?iri ?label`.WHERE`
-    ?iri ${schema.inDefinedTermSet} <${parentIri}>;
-    ${makeLocalesFilter("?iri", schema.name, "?label", locale)}
-  `.build();
-  const results = await sparqlClient.query.select(query, {
-    operation: "postUrlencoded",
-  });
-  return results.map((r) => {
-    return {
-      iri: r.iri.value,
-      label: r.label.value,
-    };
-  });
 };
