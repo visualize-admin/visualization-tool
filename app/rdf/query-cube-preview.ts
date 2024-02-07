68a9515cb2e805e269fe07ce9a7174ce7afcee68
Bartosz Prusinowski <bartosz@interactivethings.com>
2024-02-07 14:28:41 +0100

    refactor: Names

--- app/rdf/query-cube-preview.ts
+++ app/rdf/query-cube-preview.ts
@@ -34,98 +34,98 @@ export const getCubePreview = async (
   const qs: ShortQuad[] = await sparqlClient.query
     .construct(
       `PREFIX cube: <https://cube.link/>
-    PREFIX meta: <https://cube.link/meta/>
-    PREFIX qudt: <http://qudt.org/schema/qudt/>
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX schema: <http://schema.org/>
-    PREFIX sh: <http://www.w3.org/ns/shacl#>
-    PREFIX time: <http://www.w3.org/2006/time#>
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX meta: <https://cube.link/meta/>
+PREFIX qudt: <http://qudt.org/schema/qudt/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX schema: <http://schema.org/>
+PREFIX sh: <http://www.w3.org/ns/shacl#>
+PREFIX time: <http://www.w3.org/2006/time#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
     
-    CONSTRUCT {
-      ?cube schema:version ?cubeVersion .
-      ?cube cube:observationSet ?observationSet .
-      ?observationSet cube:observation ?observation .
-    
-      ?dimension sh:path ?dimensionIri .
-      ?dimension sh:datatype ?dimensionDataType .
-      ?dimension rdf:type ?dimensionType .
-      ?dimension qudt:scaleType ?dimensionScaleType .
-      ?dimension qudt:unit ?dimensionUnit .
-      ?dimensionUnit schema:name ?dimensionUnitLabel .
-      ?dimensionUnit qudt:CurrencyUnit ?dimensionUnitIsCurrency .
-      ?dimensionUnit qudt:currencyExponent ?dimensionUnitCurrencyExponent .
-      ?dimension sh:order ?dimensionOrder .
+CONSTRUCT {
+  ?cube schema:version ?cubeVersion .
+  ?cube cube:observationSet ?observationSet .
+  ?observationSet cube:observation ?observation .
+
+  ?dimension sh:path ?dimensionIri .
+  ?dimension sh:datatype ?dimensionDataType .
+  ?dimension rdf:type ?dimensionType .
+  ?dimension qudt:scaleType ?dimensionScaleType .
+  ?dimension qudt:unit ?dimensionUnit .
+  ?dimensionUnit schema:name ?dimensionUnitLabel .
+  ?dimensionUnit qudt:CurrencyUnit ?dimensionUnitIsCurrency .
+  ?dimensionUnit qudt:currencyExponent ?dimensionUnitCurrencyExponent .
+  ?dimension sh:order ?dimensionOrder .
+  ?dimension meta:dataKind ?dimensionDataKind .
+  ?dimensionDataKind rdf:type ?dimensionDataKindType .
+  ?dimensionDataKind time:unitType ?dimensionTimeUnitType .
+  ?dimension schema:name ?dimensionLabel .
+  ?dimension schema:description ?dimensionDescription .
+
+  ?observation ?observationPredicate ?observationValue .
+  ?observation ?observationPredicate ?observationValueLabel .
+} WHERE {
+  VALUES ?cube { <${iri}> }
+  FILTER(EXISTS { ?cube a cube:Cube . }) {}
+  UNION {
+    ?cube cube:observationConstraint/sh:property ?dimension .
+    ?dimension sh:path ?dimensionIri .
+    OPTIONAL { ?dimension sh:datatype ?dimensionDataType . }
+    OPTIONAL { ?dimension rdf:type ?dimensionType . }
+    OPTIONAL { ?dimension qudt:scaleType ?dimensionScaleType . }
+    OPTIONAL {
+      { ?dimension qudt:unit ?dimensionUnit . }
+      UNION { ?dimension qudt:hasUnit ?dimensionUnit . }
+      OPTIONAL { ?dimensionUnit rdfs:label ?dimensionUnitRdfsLabel . }
+      OPTIONAL { ?dimensionUnit qudt:symbol ?dimensionUnitSymbol . }
+      OPTIONAL { ?dimensionUnit qudt:ucumCode ?dimensionUnitUcumCode . }
+      OPTIONAL { ?dimensionUnit qudt:expression ?dimensionUnitExpression . }
+      OPTIONAL { ?dimensionUnit ?dimensionUnitIsCurrency qudt:CurrencyUnit . }
+      OPTIONAL { ?dimensionUnit qudt:currencyExponent ?dimensionUnitCurrencyExponent . }
+      BIND(STR(COALESCE(STR(?dimensionUnitSymbol), STR(?dimensionUnitUcumCode), STR(?dimensionUnitExpression), STR(?dimensionUnitRdfsLabel))) AS ?dimensionUnitLabel)
+      FILTER (LANG(?dimensionUnitRdfsLabel) = "en")
+    }
+    OPTIONAL { ?dimension sh:order ?dimensionOrder . }
+    OPTIONAL {
       ?dimension meta:dataKind ?dimensionDataKind .
       ?dimensionDataKind rdf:type ?dimensionDataKindType .
+    }
+    OPTIONAL {
+      ?dimension meta:dataKind ?dimensionDataKind .
       ?dimensionDataKind time:unitType ?dimensionTimeUnitType .
-      ?dimension schema:name ?dimensionLabel .
-      ?dimension schema:description ?dimensionDescription .
-
-      ?observation ?observationPredicate ?observationValue .
-      ?observation ?observationPredicate ?observationValueLabel .
-    } WHERE {
-      VALUES ?cube { <${iri}> }
-      FILTER(EXISTS { ?cube a cube:Cube . }) {}
-      UNION {
-        ?cube cube:observationConstraint/sh:property ?dimension .
-        ?dimension sh:path ?dimensionIri .
-        OPTIONAL { ?dimension sh:datatype ?dimensionDataType . }
-        OPTIONAL { ?dimension rdf:type ?dimensionType . }
-        OPTIONAL { ?dimension qudt:scaleType ?dimensionScaleType . }
-        OPTIONAL {
-          { ?dimension qudt:unit ?dimensionUnit . }
-      UNION { ?dimension qudt:hasUnit ?dimensionUnit . }
-          OPTIONAL { ?dimensionUnit rdfs:label ?dimensionUnitRdfsLabel . }
-          OPTIONAL { ?dimensionUnit qudt:symbol ?dimensionUnitSymbol . }
-          OPTIONAL { ?dimensionUnit qudt:ucumCode ?dimensionUnitUcumCode . }
-          OPTIONAL { ?dimensionUnit qudt:expression ?dimensionUnitExpression . }
-          OPTIONAL { ?dimensionUnit ?dimensionUnitIsCurrency qudt:CurrencyUnit . }
-          OPTIONAL { ?dimensionUnit qudt:currencyExponent ?dimensionUnitCurrencyExponent . }
-          BIND(STR(COALESCE(STR(?dimensionUnitSymbol), STR(?dimensionUnitUcumCode), STR(?dimensionUnitExpression), STR(?dimensionUnitRdfsLabel))) AS ?dimensionUnitLabel)
-          FILTER (LANG(?dimensionUnitRdfsLabel) = "en")
-        }
-        OPTIONAL { ?dimension sh:order ?dimensionOrder . }
-        OPTIONAL {
-          ?dimension meta:dataKind ?dimensionDataKind .
-          ?dimensionDataKind rdf:type ?dimensionDataKindType .
-        }
-        OPTIONAL {
-          ?dimension meta:dataKind ?dimensionDataKind .
-          ?dimensionDataKind time:unitType ?dimensionTimeUnitType .
-        }
-        ${buildLocalizedSubQuery("dimension", "schema:name", "dimensionLabel", {
-          locale,
-        })}
-        ${buildLocalizedSubQuery(
-          "dimension",
-          "schema:description",
-          "dimensionDescription",
-          { locale }
-        )}
+    }
+    ${buildLocalizedSubQuery("dimension", "schema:name", "dimensionLabel", {
+      locale,
+    })}
+    ${buildLocalizedSubQuery(
+      "dimension",
+      "schema:description",
+      "dimensionDescription",
+      { locale }
+    )}
     FILTER(?dimensionIri != cube:observedBy && ?dimensionIri != rdf:type)
-      } UNION {
-        ?cube cube:observationConstraint/sh:property/sh:path ?observationPredicate .
-    
-        { SELECT * WHERE {
-          ?cube cube:observationSet ?observationSet .
-          ?observationSet cube:observation ?observation .
-          FILTER(NOT EXISTS { ?cube cube:observationConstraint/sh:property/sh:datatype cube:Undefined . } && NOT EXISTS { ?observation ?p ""^^cube:Undefined . })
-        } LIMIT 10 }
-    
-        { SELECT * WHERE {
-          ?observation ?observationPredicate ?observationValue .
-          ${buildLocalizedSubQuery(
-            "observationValue",
-            "schema:name",
-            "observationValueLabel",
-            { locale }
-          )}
-          FILTER(?observationPredicate != cube:observedBy)
-        }}
-      }
-    }`,
+  } UNION {
+    ?cube cube:observationConstraint/sh:property/sh:path ?observationPredicate .
+
+    { SELECT * WHERE {
+      ?cube cube:observationSet ?observationSet .
+      ?observationSet cube:observation ?observation .
+      FILTER(NOT EXISTS { ?cube cube:observationConstraint/sh:property/sh:datatype cube:Undefined . } && NOT EXISTS { ?observation ?p ""^^cube:Undefined . })
+    } LIMIT 10 }
+
+    { SELECT * WHERE {
+      ?observation ?observationPredicate ?observationValue .
+      ${buildLocalizedSubQuery(
+        "observationValue",
+        "schema:name",
+        "observationValueLabel",
+        { locale }
+      )}
+      FILTER(?observationPredicate != cube:observedBy)
+    }}
+  }
+}`,
       { operation: "postUrlencoded" }
     )
     .then((qs) => {
@@ -246,32 +246,30 @@ export const getCubePreview = async (
     }
   });
 
-  const observationQuads = qs.filter(({ p }) => p.equals(ns.cube.observation));
-  const observations = uniqBy(observationQuads, ({ o }) => o.value).map(
-    ({ o }) => {
-      const dimeValueQuads = qsDims.map((dimensionQuad) => {
-        return qs.find((q) => q.s.equals(o) && q.p.equals(dimensionQuad.o));
-      });
+  const qsObs = qs.filter(({ p }) => p.equals(ns.cube.observation));
+  const observations = uniqBy(qsObs, ({ o }) => o.value).map(({ o }) => {
+    const sqDimValue = qsDims.map((quad) => {
+      return qs.find((q) => q.s.equals(o) && q.p.equals(quad.o));
+    });
 
-      return dimeValueQuads.reduce((acc, quad) => {
-        if (!quad) return acc;
+    return sqDimValue.reduce((acc, quad) => {
+      if (!quad) return acc;
 
-        if (!acc[quad.p.value]) {
-          const rootObservationId = qs.find((q) => q.o.equals(quad.o));
+      if (!acc[quad.p.value]) {
+        const rootObservationId = qs.find((q) => q.o.equals(quad.o));
 
-          acc[quad.p.value] =
-            qs.find(
-              (q) =>
-                q.s.equals(rootObservationId?.s) &&
-                q.p.equals(quad.p) &&
-                q.o.termType === "Literal"
-            )?.o.value ?? quad.o.value;
-        }
+        acc[quad.p.value] =
+          qs.find(
+            (q) =>
+              q.s.equals(rootObservationId?.s) &&
+              q.p.equals(quad.p) &&
+              q.o.termType === "Literal"
+          )?.o.value ?? quad.o.value;
+      }
 
-        return acc;
-      }, {} as Record<string, string>);
-    }
-  );
+      return acc;
+    }, {} as Record<string, string>);
+  });
 
   return { dimensions, measures, observations };
 };
