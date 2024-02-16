07aded36483e6b1c4a31cf0ceec14f5f2a57072d
Patrick Browne <pt.browne@gmail.com>
2024-02-16 14:56:50 +0100

    refactor: Extract Confirmation Dialog from actions

--- app/login/components/profile-tables.tsx
+++ app/login/components/profile-tables.tsx
@@ -481,11 +481,9 @@ const ActionButton = (props: ActionButtonProps) => {
     confirmationTitle,
     confirmationText,
     onClick,
-    onDialogClose,
     onSuccess,
   } = props;
   const { isOpen, open, close } = useDisclosure();
-  const [loading, setLoading] = React.useState(false);
 
   return (
     <>
@@ -513,57 +511,84 @@ const ActionButton = (props: ActionButtonProps) => {
         <Typography variant="body2">{label}</Typography>
       </MenuItem>
       {requireConfirmation && (
-        <Dialog
+        <ConfirmationDialog
           open={isOpen}
-          // To prevent the click away listener from closing the dialog.
-          onClick={(e) => e.stopPropagation()}
           onClose={close}
-          maxWidth="xs"
-        >
-          <DialogTitle>
-            <Typography variant="h3">
-              {confirmationTitle ??
-                t({
-                  id: "login.profile.chart.confirmation.default",
-                  message: "Are you sure you want to perform this action?",
-                })}
-            </Typography>
-          </DialogTitle>
-          {confirmationText && (
-            <DialogContent>
-              <DialogContentText>{confirmationText}</DialogContentText>
-            </DialogContent>
-          )}
-          <DialogActions
-            sx={{
-              "& > .MuiButton-root": {
-                justifyContent: "center",
-                pointerEvents: loading ? "none" : "auto",
-              },
-            }}
-          >
-            <Button variant="text" onClick={close}>
-              <Trans id="no">No</Trans>
-            </Button>
-            <Button
-              variant="text"
-              onClick={async (e) => {
-                e.stopPropagation();
-                setLoading(true);
-
-                await onClick();
-                close();
-                await new Promise((r) => setTimeout(r, 100));
-
-                onDialogClose?.();
-                onSuccess?.();
-              }}
-            >
-              {loading ? <CircularProgress /> : <Trans id="yes">Yes</Trans>}
-            </Button>
-          </DialogActions>
-        </Dialog>
+          title={confirmationTitle}
+          text={confirmationText}
+          onClick={onClick}
+          onSuccess={onSuccess}
+        />
       )}
     </>
+  );
+};
+
+const ConfirmationDialog = ({
+  title,
+  text,
+  onClick,
+  onSuccess,
+  onConfirm,
+  ...props
+}: DialogProps & {
+  title?: string;
+  text?: string;
+  onSuccess?: () => Promise<unknown> | void;
+  onConfirm?: () => Promise<unknown> | void;
+  onClick: () => Promise<unknown> | void;
+}) => {
+  const [loading, setLoading] = React.useState(false);
+
+  return (
+    <Dialog
+      // To prevent the click away listener from closing the dialog.
+      onClick={(e) => e.stopPropagation()}
+      onClose={close}
+      maxWidth="xs"
+      {...props}
+    >
+      <DialogTitle>
+        <Typography variant="h3">
+          {title ??
+            t({
+              id: "login.profile.chart.confirmation.default",
+              message: "Are you sure you want to perform this action?",
+            })}
+        </Typography>
+      </DialogTitle>
+      {text && (
+        <DialogContent>
+          <DialogContentText>{text}</DialogContentText>
+        </DialogContent>
+      )}
+      <DialogActions
+        sx={{
+          "& > .MuiButton-root": {
+            justifyContent: "center",
+            pointerEvents: loading ? "none" : "auto",
+          },
+        }}
+      >
+        <Button variant="text" onClick={close}>
+          <Trans id="no">No</Trans>
+        </Button>
+        <Button
+          variant="text"
+          onClick={async (e) => {
+            e.stopPropagation();
+            setLoading(true);
+
+            await onClick();
+            await new Promise((r) => setTimeout(r, 100));
+
+            props.onClose?.({}, "escapeKeyDown");
+            onSuccess?.();
+          }}
+        >
+          {loading ? <CircularProgress /> : <Trans id="yes">Yes</Trans>}
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
