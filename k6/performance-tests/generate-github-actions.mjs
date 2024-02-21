6dbe4fbc3f59aaa38c10a2f546476f6a23ef6058
Bartosz Prusinowski <bartosz@interactivethings.com>
2024-02-21 12:00:43 +0100

    fix: GQL performance tests
    ...by switching to using binary file on PR tests to solve issues with accessing env variables from Docker container.

--- k6/performance-tests/generate-github-actions.mjs
+++ k6/performance-tests/generate-github-actions.mjs
@@ -23,6 +23,7 @@ const generateAutoTests = () => {
             `https://${
               env === "prod" ? "" : `${env}.`
             }visualize.admin.ch/api/graphql`,
+            "/root/",
             true,
             false
           )
@@ -66,25 +67,27 @@ jobs:
 generateAutoTests();
 
 const generatePRTests = () => {
-  const commands = queries
-    .flatMap((query) =>
-      cubes.map((cube) =>
-        getRunCommand(
-          "PR",
-          query,
-          cube,
-          "${{ github.event.deployment_status.target_url }}/api/graphql",
-          false,
-          true
-        )
+  const commands = queries.flatMap((query) =>
+    cubes.map((cube) =>
+      getRunCommand(
+        "PR",
+        query,
+        cube,
+        "${{ github.event.deployment_status.target_url }}/api/graphql",
+        "",
+        false,
+        true
       )
     )
-    .join(" && ");
+  );
   const file = `# GENERATED FILE, DO NOT EDIT MANUALLY - use yarn run github:codegen instead
 
 name: GraphQL performance tests (PR)
 
 on: [deployment_status]
+
+env:
+  SUMMARY: ''
 
 jobs:
   run_tests:
@@ -97,17 +100,20 @@ jobs:
       - name: Send an HTTP request to start up the server
         run: |
           curl -s '\${{ github.event.deployment_status.target_url }}/api/graphql' -X 'POST' -H 'Content-Type: application/json' -d '{"operationName":"DataCubeObservations","variables":{"locale":"en","sourceType":"sparql","sourceUrl":"https://lindas.admin.ch/query","cubeFilter":{"iri":"https://energy.ld.admin.ch/sfoe/bfe_ogd84_einmalverguetung_fuer_photovoltaikanlagen/9","filters":{"https://energy.ld.admin.ch/sfoe/bfe_ogd84_einmalverguetung_fuer_photovoltaikanlagen/Kanton":{"type":"single","value":"https://ld.admin.ch/canton/1"}}}},"query":"query DataCubeObservations($sourceType: String!, $sourceUrl: String!, $locale: String!, $cubeFilter: DataCubeObservationFilter!) { dataCubeObservations(sourceType: $sourceType, sourceUrl: $sourceUrl, locale: $locale, cubeFilter: $cubeFilter) }"}' > /dev/null
-      - name: Run k6
-        uses: addnab/docker-run-action@v3
-        with:
-          image: grafana/k6:latest
-          options: -v \${{ github.workspace }}:/root -u root
-          run: |
-            touch /root/summary.txt
-            echo "$(${commands})" > /root/summary.txt
-      - name: Set env variable for easier access
+      - name: Download, unzip and install k6 binary from https://github.com/grafana/k6/releases/download/v0.49.0/k6-v0.49.0-linux-arm64.tar.gz
         run: |
-          echo "SUMMARY=$(< summary.txt)" >> $GITHUB_ENV
+            wget https://github.com/grafana/k6/releases/download/v0.49.0/k6-v0.49.0-linux-amd64.tar.gz
+            tar -xzf k6-v0.49.0-linux-amd64.tar.gz
+            sudo cp k6-v0.49.0-linux-amd64/k6 /usr/local/bin/k6
+            export PATH=$PATH:/usr/local/bin
+${commands
+  .map(
+    (command, i) => `      - name: Run k6 test (iteration ${i + 1})
+        run: |
+          echo "SUMMARY=\${{ env.SUMMARY }}$(${command})" >> $GITHUB_ENV
+      `
+  )
+  .join("\n")}
       - name: GQL performance tests ❌
         if: \${{ env.SUMMARY != '' }}
         run: |
@@ -132,6 +138,7 @@ function getRunCommand(
   query,
   cube,
   endpoint,
+  rootPath,
   sendToPrometheus = true,
   checkTiming = true
 ) {
@@ -139,7 +146,9 @@ function getRunCommand(
     sendToPrometheus ? " -o experimental-prometheus-rw" : ""
   } --tag testid=${query} --env ENV=${env} --env ENDPOINT=${endpoint} --env CUBE_IRI=${
     cube.iri
-  } --env CUBE_LABEL=${cube.label} --env ROOT_PATH=/root/ --env CHECK_TIMING=${
+  } --env CUBE_LABEL=${cube.label} --env CHECK_TIMING=${
     checkTiming ? "true" : "false"
-  } --quiet - </root/k6/performance-tests/graphql/${query}.js`;
+  } --env CUBES='${JSON.stringify(
+    cubes
+  )}' --quiet - <${rootPath}k6/performance-tests/graphql/${query}.js`;
 }
