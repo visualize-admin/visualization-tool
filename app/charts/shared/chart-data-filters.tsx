8595929777d061e035223506ea6b2bd4cdc75e5a
Bartosz Prusinowski <bartosz@interactivethings.com>
2024-02-22 16:57:57 +0100

    fix: Load values

--- app/charts/shared/chart-data-filters.tsx
+++ app/charts/shared/chart-data-filters.tsx
@@ -243,6 +243,7 @@ const DataFilter = (props: DataFilterProps) => {
           iri: cubeIri,
           componentIris: [dimensionIri],
           filters: keys.length > 1 ? interactiveFilters : undefined,
+          loadValues: true,
         },
       ],
       // This is important for urql not to think that filters
