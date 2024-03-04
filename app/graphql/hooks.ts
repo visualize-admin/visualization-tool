29d0a1c188ade40cb439508517dbe6ada165b95e
Patrick Browne <pt.browne@gmail.com>
2024-03-04 17:35:28 +0100

    fix: Add pause to effect

--- app/graphql/hooks.ts
+++ app/graphql/hooks.ts
@@ -65,7 +65,7 @@ const makeUseQuery =
 
       executeQuery(options);
       // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [queryKey]);
+    }, [queryKey, options.pause]);
 
     return [result, executeQuery] as const;
   };
