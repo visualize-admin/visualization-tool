ceade1b8ca10d3f7fcc25a295108f8caf0699ec0
Bartosz Prusinowski <bartosz@interactivethings.com>
2024-03-08 14:08:42 +0100

    refactor: Move CubeDimension mock to RDFCubeViewQueryMock

--- app/test/cube-view-query-mock.ts
+++ app/test/cube-view-query-mock.ts
@@ -1,3 +1,9 @@
+import clownface, { AnyPointer } from "clownface";
+import rdf from "rdf-ext";
+import { NamedNode } from "rdf-js";
+
+import * as ns from "@/rdf/namespace";
+
 export const RDFCubeViewQueryMock = jest.mock("rdf-cube-view-query", () => ({
   Node: class {
     constructor() {}
@@ -7,5 +13,66 @@ export const RDFCubeViewQueryMock = jest.mock("rdf-cube-view-query", () => ({
   },
   Cube: class {
     constructor() {}
+  },
+  CubeDimension: class {
+    ptr: AnyPointer;
+
+    constructor({
+      term = rdf.blankNode(),
+      dataset = rdf.dataset(),
+      graph = rdf.defaultGraph(),
+    } = {}) {
+      this.ptr = clownface({ term, dataset, graph });
+    }
+
+    get path() {
+      return this.ptr.out(ns.sh.path).term;
+    }
+
+    get optional() {
+      const optionalDatatype = this.ptr
+        .out(ns.sh.or)
+        .out(ns.sh.datatype)
+        .filter((d) => ns.cube.Undefined.equals(d.term)).term;
+      const optionalValue = this.in.some((v) => ns.cube.Undefined.equals(v));
+
+      return Boolean(optionalDatatype || optionalValue);
+    }
+
+    get datatype() {
+      const nonOptional = this.ptr.out(ns.sh.datatype).term;
+      const withOptional = this.ptr
+        .out(ns.sh.or)
+        .out(ns.sh.datatype)
+        .filter((d) => !ns.cube.Undefined.equals(d.term)).term;
+
+      return nonOptional || withOptional;
+    }
+
+    get minExclusive() {
+      return this.ptr.out(ns.sh.minExclusive).term;
+    }
+
+    get minInclusive() {
+      return this.ptr.out(ns.sh.minInclusive).term;
+    }
+
+    get maxExclusive() {
+      return this.ptr.out(ns.sh.maxExclusive).term;
+    }
+
+    get maxInclusive() {
+      return this.ptr.out(ns.sh.maxInclusive).term;
+    }
+
+    get in() {
+      return [...(this.ptr.out(ns.sh.in).list() ?? [])].map(
+        (item) => item.term
+      );
+    }
+
+    out(...args: NamedNode[]) {
+      return this.ptr.out(...args);
+    }
   },
 }));
