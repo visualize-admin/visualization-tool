ba6098bd6e0ffe53ff578846d38c3a6ce4ecc58c
Patrick Browne <pt.browne@gmail.com>
2024-05-15 09:53:16 +0200

    feat: Add react-grid

---
+++ app/components/react-grid.stories.tsx
@@ -0,0 +1,106 @@
+import {
+  Checkbox,
+  FormControlLabel,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
+import { useMemo, useState } from "react";
+import { Layouts } from "react-grid-layout";
+
+import ShowcaseLayout, {
+  ChartGridLayout,
+  GridLayout,
+  availableHandles,
+  generateLayout,
+} from "@/components/react-grid";
+import useLocalState from "@/utils/use-local-state";
+
+const generateDOM = ({ count }: { count: number }): React.ReactNode => {
+  return Array.from({ length: count }).map(function (_l, i) {
+    return (
+      <div key={i}>
+        <span className="text">{i}</span>
+      </div>
+    );
+  });
+};
+
+export const Example = () => {
+  const [count] = useState(5);
+
+  const [allowResize, setAllowResize] = useState(false);
+
+  const resizeHandles = useMemo(
+    () => (allowResize ? availableHandles : []),
+    [allowResize]
+  );
+  const [layouts, setLayouts] = useLocalState<Layouts>(
+    "react-grid-story-layouts",
+    {
+      lg: generateLayout({
+        resizeHandles,
+        count,
+        layout: "horizontal",
+      }),
+    }
+  );
+
+  const [gridLayout, setGridLayout] = useState<GridLayout>("horizontal");
+
+  const onChangeGridLayout = (gridLayout: GridLayout) => {
+    setGridLayout(gridLayout);
+    setLayouts({
+      lg: generateLayout({
+        resizeHandles,
+        count,
+        layout: gridLayout,
+      }),
+    });
+  };
+
+  return (
+    <>
+      <Stack gap="1rem" direction="row">
+        <ToggleButtonGroup
+          color="primary"
+          value={gridLayout}
+          onChange={(ev) =>
+            onChangeGridLayout(
+              (ev.target as unknown as { value: GridLayout }).value
+            )
+          }
+        >
+          <ToggleButton value="horizontal">Horizontal</ToggleButton>
+          <ToggleButton value="vertical">Vertical</ToggleButton>
+          <ToggleButton value="wide">Wide</ToggleButton>
+          <ToggleButton value="tall">Tall</ToggleButton>
+        </ToggleButtonGroup>
+        <FormControlLabel
+          label={"Allow Resize"}
+          control={
+            <Checkbox
+              checked={allowResize}
+              onChange={(ev, value) => setAllowResize(value)}
+            />
+          }
+        ></FormControlLabel>
+      </Stack>
+      <ChartGridLayout
+        className={"layout"}
+        onLayoutChange={(_layouts, allLayouts) => setLayouts(allLayouts)}
+        layouts={layouts}
+        resize
+      >
+        {generateDOM({ count })}
+      </ChartGridLayout>
+    </>
+  );
+};
+
+const meta = {
+  title: "Components/ReactGrid",
+  component: ShowcaseLayout,
+};
+
+export default meta;
