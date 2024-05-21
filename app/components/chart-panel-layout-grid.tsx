43de2c4e0566bd5ffcb02ad1be651ecb8cf8f2c6
Patrick Browne <pt.browne@gmail.com>
2024-05-21 10:08:39 +0200

    feat: Dispatch change of layouts and save chart config with it

--- app/components/chart-panel-layout-grid.tsx
+++ app/components/chart-panel-layout-grid.tsx
@@ -1,16 +1,24 @@
-import { useEffect, useState } from "react";
+import { fold } from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/function";
+import { useState } from "react";
 import { Layouts } from "react-grid-layout";
+import { ObjectInspector } from "react-inspector";
 
 import { ChartPanelLayoutTypeProps } from "@/components/chart-panel";
-import ChartGridLayout, {
-  availableHandles,
-  generateLayout,
-} from "@/components/react-grid";
+import ChartGridLayout, { generateLayout } from "@/components/react-grid";
+import { ReactGridLayoutsType, isLayouting } from "@/configurator";
+import { useConfiguratorState } from "@/src";
 
 const ChartPanelLayoutGrid = (props: ChartPanelLayoutTypeProps) => {
   const { chartConfigs } = props;
-  const [resizeHandles, setResizeHandles] = useState(() => availableHandles);
+  const [config, dispatch] = useConfiguratorState(isLayouting);
   const [layouts, setLayouts] = useState<Layouts>(() => {
+    if (
+      config.layout.type === "dashboard" &&
+      config.layout.layout === "tiles"
+    ) {
+      return config.layout.layouts;
+    }
     return {
       lg: generateLayout({
         count: props.chartConfigs.length,
@@ -18,25 +26,52 @@ const ChartPanelLayoutGrid = (props: ChartPanelLayoutTypeProps) => {
       }),
     };
   });
-  useEffect(() => {
-    setLayouts({
-      lg: generateLayout({
-        count: chartConfigs.length,
-        layout: "tiles",
-      }),
+
+  const handleChangeLayouts = (layouts: Layouts) => {
+    const layout = config.layout;
+    if (layout.type !== "dashboard" || layout.layout !== "tiles") {
+      // Should not happen
+      return;
+    }
+
+    const parsedLayouts = pipe(
+      ReactGridLayoutsType.decode(layouts),
+      fold(
+        (err) => {
+          console.error("Error while decoding react-grid-layout", err);
+          return undefined;
+        },
+        (d) => {
+          return d;
+        }
+      )
+    );
+    if (!parsedLayouts) {
+      return;
+    }
+
+    dispatch({
+      type: "LAYOUT_CHANGED",
+      value: {
+        ...layout,
+        layouts: parsedLayouts,
+      },
     });
-  }, [chartConfigs.length, resizeHandles]);
+    setLayouts(layouts);
+  };
 
   return (
-    <ChartGridLayout
-      className={"layout"}
-      layouts={layouts}
-      resize
-      resizeHandles={resizeHandles}
-      onLayoutChange={(_l, allLayouts) => setLayouts(allLayouts)}
-    >
-      {chartConfigs.map((chartConfig) => props.renderChart(chartConfig))}
-    </ChartGridLayout>
+    <>
+      <ChartGridLayout
+        className={"layout"}
+        layouts={layouts}
+        resize
+        onLayoutChange={(_l, allLayouts) => handleChangeLayouts(allLayouts)}
+      >
+        {chartConfigs.map((chartConfig) => props.renderChart(chartConfig))}
+      </ChartGridLayout>
+      <ObjectInspector data={layouts} />
+    </>
   );
 };
 
