3221d63f875bb1e560d76048ed1bf9d1b01de931
Patrick Browne <pt.browne@gmail.com>
2024-05-22 09:58:30 +0200

    fix: Remove ObjectInspector

--- app/components/chart-panel-layout-grid.tsx
+++ app/components/chart-panel-layout-grid.tsx
@@ -3,7 +3,6 @@ import { fold } from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/function";
 import { useState } from "react";
 import { Layouts } from "react-grid-layout";
-import { ObjectInspector } from "react-inspector";
 
 import { ChartPanelLayoutTypeProps } from "@/components/chart-panel";
 import ChartGridLayout from "@/components/react-grid";
@@ -66,18 +65,15 @@ const ChartPanelLayoutGrid = (props: ChartPanelLayoutTypeProps) => {
   };
 
   return (
-    <>
-      <ChartGridLayout
-        className={clsx("layout", chartPanelLayoutGridClasses.root)}
-        layouts={layouts}
-        resize
-        draggableHandle={`.${chartPanelLayoutGridClasses.dragHandle}`}
-        onLayoutChange={(_l, allLayouts) => handleChangeLayouts(allLayouts)}
-      >
-        {chartConfigs.map((chartConfig) => props.renderChart(chartConfig))}
-      </ChartGridLayout>
-      <ObjectInspector data={layouts} />
-    </>
+    <ChartGridLayout
+      className={clsx("layout", chartPanelLayoutGridClasses.root)}
+      layouts={layouts}
+      resize
+      draggableHandle={`.${chartPanelLayoutGridClasses.dragHandle}`}
+      onLayoutChange={(_l, allLayouts) => handleChangeLayouts(allLayouts)}
+    >
+      {chartConfigs.map((chartConfig) => props.renderChart(chartConfig))}
+    </ChartGridLayout>
   );
 };
 
