f6ce7e4f2eba9aeddc29033c2142ead5c0e291b6
Patrick Browne <pt.browne@gmail.com>
2024-05-24 13:35:28 +0200

    fix: Bad SVG props triggered warning in console

--- app/icons/components/IcInfoOutline.tsx
+++ app/icons/components/IcInfoOutline.tsx
@@ -9,14 +9,14 @@ function SvgIcInfoOutline(props: React.SVGProps<SVGSVGElement>) {
       {...props}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M1.21875 7.98438C1.21875 4.26157 4.26157 1.21875 7.98438 1.21875C11.7072 1.21875 14.75 4.26157 14.75 7.98438C14.75 11.7072 11.7072 14.75 7.98438 14.75C4.26157 14.75 1.21875 11.7072 1.21875 7.98438ZM7.98438 2.71875C5.08999 2.71875 2.71875 5.08999 2.71875 7.98438C2.71875 10.8788 5.08999 13.25 7.98438 13.25C10.8788 13.25 13.25 10.8788 13.25 7.98438C13.25 5.08999 10.8788 2.71875 7.98438 2.71875Z"
         fill="currentColor"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M8.68125 11.4812H7.28125V7.28125H8.68125V11.4812Z"
         fill="currentColor"
       />
