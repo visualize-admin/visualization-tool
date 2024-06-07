a52942b6ebafada1f200fa42292079142bb96380
Patrick Browne <pt.browne@gmail.com>
2024-06-07 23:48:17 +0200

    feat: Any graphql should abort if not mocked when replaying from HAR

--- e2e/common.ts
+++ e2e/common.ts
@@ -37,6 +37,7 @@ const setup = (contextOptions?: PlaywrightTestOptions["contextOptions"]) => {
         if (process.env.E2E_HAR !== "false") {
           await page.routeFromHAR(`./e2e/har/${name}.zip`, {
             url: /api\/graphql/,
+            notFound: "abort",
             ...routeFromHAROptions
           });
         }
