169b5adc5dbf7f8afb0225ed0f1526406a66822b
Patrick Browne <pt.browne@gmail.com>
2024-06-11 12:09:03 +0200

    feat: Allow a tab not to have a label

--- app/components/chart-selection-tabs.tsx
+++ app/components/chart-selection-tabs.tsx
@@ -123,7 +123,7 @@ export const ChartSelectionTabs = () => {
           ? d.meta.title[locale]
           : d.meta.title[defaultLocale] !== ""
             ? d.meta.title[defaultLocale]
-            : t({ id: "annotation.add.title" }),
+            : undefined,
     };
   });
 
@@ -356,7 +356,7 @@ type TabDatum = {
   key: string;
   chartType: ChartType;
   active: boolean;
-  label: string;
+  label?: string | undefined;
 };
 
 type TabsFixedProps = {
@@ -757,7 +757,7 @@ const TabsInner = (props: TabsInnerProps) => {
                             sx={{
                               px: 0,
                               flexShrink: 1,
-                              minWidth: 180,
+                              minWidth: d.label ? 180 : 0,
                               flexBasis: "100%",
                             }}
                             label={
