e7353f3f07460cb8c9b8037e5b1a046bc31dfbb1
Bartosz Prusinowski <bartosz@interactivethings.com>
2024-06-12 14:28:49 +0200

    chore: Add GitHub Action to preload Varnish cache

---
+++ e2e/most-recent-charts.spec.ts
@@ -0,0 +1,27 @@
+import { PROD_DATA_SOURCE_URL } from "../app/domain/datasource/constants";
+import { locales } from "../app/locales";
+
+import { setup } from "./common";
+
+const { test } = setup();
+
+// @noci as it's a special test that's supposed to preload varnish cache
+// for most recent charts
+test("it should preload most recent charts @noci", async ({
+  page,
+  selectors,
+}) => {
+  const fetchedConfigs = await fetch(
+    "https://visualize.admin.ch/api/config/all?limit=25"
+  ).then((res) => res.json());
+  const keys = fetchedConfigs.map((config) => config.key);
+
+  // Slice to 25 until we have a PROD deployment that actually makes use of
+  // the limit query param
+  for (const key of keys.slice(0, 25)) {
+    for (const locale of locales) {
+      await page.goto(`${PROD_DATA_SOURCE_URL}/${locale}/v/${key}`);
+      await selectors.chart.loaded();
+    }
+  }
+});
