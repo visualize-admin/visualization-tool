c7d29513a9eca1a7f784b16758f3c9c5a3b4d747
Bartosz Prusinowski <bartosz@interactivethings.com>
2024-06-21 13:43:15 +0200

    perf: Optimize all termsets fetching

--- app/rdf/query-termsets.ts
+++ app/rdf/query-termsets.ts
@@ -15,30 +15,22 @@ export const queryAllTermsets = async (options: {
   const qs = await sparqlClient.query.select(
     `PREFIX cube: <https://cube.link/>
 PREFIX meta: <https://cube.link/meta/>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX schema: <http://schema.org/>
-PREFIX sh: <http://www.w3.org/ns/shacl#>
 
-SELECT DISTINCT (COUNT(distinct ?iri) as ?count) ?termsetIri ?termsetLabel WHERE {
-    ?iri cube:observationConstraint/sh:property ?dimension .
-    ?dimension a cube:KeyDimension .
-    ?dimension sh:in/rdf:rest*/rdf:first ?value.
-    ?value schema:inDefinedTermSet ?termsetIri .
-    ?termsetIri a meta:SharedDimension .
-    ${buildLocalizedSubQuery("termsetIri", "schema:name", "termsetLabel", { locale })}
+SELECT DISTINCT (COUNT(DISTINCT ?cubeIri) as ?count) ?termsetIri ?termsetLabel WHERE {
+  ?termsetIri meta:isUsedIn ?cubeIri .
+  ${makeVisualizeDatasetFilter({
+    includeDrafts: !!includeDrafts,
+    cubeIriVar: "?cubeIri",
+  })}
+  ${buildLocalizedSubQuery("termsetIri", "schema:name", "termsetLabel", { locale })}
 
-    ${makeVisualizeDatasetFilter({
-      includeDrafts: !!includeDrafts,
-      cubeIriVar: "?iri",
-    }).toString()}
-
-  }      GROUP BY ?termsetIri ?termsetLabel`, {
-    operation: "postUrlencoded",
-  }
+} GROUP BY ?termsetIri ?termsetLabel`,
+    { operation: "postUrlencoded" }
   );
 
   return qs.map((result) => ({
-    count: Number(result.count.value),
+    count: +result.count.value,
     termset: {
       __typename: "Termset",
       iri: result.termsetIri.value,
