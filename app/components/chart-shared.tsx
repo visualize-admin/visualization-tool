0c5461d4cf1f2595d20c708f744d120525c5822f
Bartosz Prusinowski <bartosz@interactivethings.com>
2024-06-24 17:27:10 +0200

    refactor: Simplify condition

--- app/components/chart-shared.tsx
+++ app/components/chart-shared.tsx
@@ -93,45 +93,50 @@ export const ChartMoreButton = ({ chartKey }: { chartKey: string }) => {
         transformOrigin={{ horizontal: "center", vertical: "top" }}
       >
         {isPublished(state) ? null : (
-          <DuplicateChartMenuActionItem
-            chartKey={chartKey}
-            onSuccess={handleClose}
-          />
+          <>
+            <DuplicateChartMenuActionItem
+              chartKey={chartKey}
+              onSuccess={handleClose}
+            />
+            {isConfiguring(state) ? null : (
+              <MenuActionItem
+                type="button"
+                as="menuitem"
+                onClick={() => {
+                  dispatch({ type: "CONFIGURE_CHART", value: { chartKey } });
+                  handleClose();
+                }}
+                iconName="edit"
+                label={<Trans id="chart-controls.edit">Edit</Trans>}
+              />
+            )}
+            {state.chartConfigs.length > 1 ? (
+              <MenuActionItem
+                type="button"
+                as="menuitem"
+                color="error"
+                requireConfirmation
+                confirmationTitle={t({
+                  id: "chart-controls.delete.title",
+                  message: "Delete chart?",
+                })}
+                confirmationText={t({
+                  id: "chart-controls.delete.confirmation",
+                  message: "Are you sure you want to delete this chart?",
+                })}
+                onClick={() => {
+                  dispatch({
+                    type: "CHART_CONFIG_REMOVE",
+                    value: { chartKey },
+                  });
+                  handleClose();
+                }}
+                iconName="trash"
+                label={<Trans id="chart-controls.delete">Delete</Trans>}
+              />
+            ) : null}
+          </>
         )}
-        {isPublished(state) || isConfiguring(state) ? null : (
-          <MenuActionItem
-            type="button"
-            as="menuitem"
-            onClick={() => {
-              dispatch({ type: "CONFIGURE_CHART", value: { chartKey } });
-              handleClose();
-            }}
-            iconName="edit"
-            label={<Trans id="chart-controls.edit">Edit</Trans>}
-          />
-        )}
-        {isPublished(state) ? null : state.chartConfigs.length > 1 ? (
-          <MenuActionItem
-            type="button"
-            as="menuitem"
-            color="error"
-            requireConfirmation
-            confirmationTitle={t({
-              id: "chart-controls.delete.title",
-              message: "Delete chart?",
-            })}
-            confirmationText={t({
-              id: "chart-controls.delete.confirmation",
-              message: "Are you sure you want to delete this chart?",
-            })}
-            onClick={() => {
-              dispatch({ type: "CHART_CONFIG_REMOVE", value: { chartKey } });
-              handleClose();
-            }}
-            iconName="trash"
-            label={<Trans id="chart-controls.delete">Delete</Trans>}
-          />
-        ) : null}
       </ArrowMenu>
     </>
   );
