ea45e304939f3244241d3b4ddd8cd9e58a0005f4
Patrick Browne <pt.browne@gmail.com>
2024-08-13 13:19:25 +0200

    fix: Make varnish cache script more resilient

--- e2e/varnish-cache-preload.spec.ts
+++ e2e/varnish-cache-preload.spec.ts
@@ -23,19 +23,35 @@ const fetchMetadata = ({
   );
 };
 
+const TEST_TIMEOUT_MS = 5 * 60 * 1000;
+const NAVIGATION_TIMEOUT_MS = 10_1000;
+
+const formatError = (e: unknown) => {
+  return e instanceof Error ? e.message : JSON.stringify(e);
+};
+
 // @noci as it's a special test that's supposed to preload varnish cache
 // for most recent charts
-test("@noci it should preload most recent charts", async ({ page }) => {
-  test.setTimeout(5 * 60 * 1000);
+test("@noci it should preload most recent charts", async ({
+  page,
+  browser,
+}) => {
+  test.setTimeout(TEST_TIMEOUT_MS);
   const fetchedConfigs = await fetchMetadata({ limit: 25 });
   const keys = fetchedConfigs.data.map((config) => config.key);
 
   for (const key of keys) {
     for (const locale of locales) {
-      await page.goto(`https://visualize.admin.ch/${locale}/v/${key}`);
+      await page
+        .goto(`https://visualize.admin.ch/${locale}/v/${key}`, {
+          timeout: NAVIGATION_TIMEOUT_MS,
+        })
+        .catch((e) => {
+          console.warn(`Error during goto for ${key}: ${formatError(e)}`);
+        });
       // Wait for all the queries to finish
       await page.waitForLoadState("networkidle").catch(() => {
-        console.warn("Timeout for", key);
+        console.warn("Timeout waitForLoadState for", key);
       });
     }
   }
@@ -44,7 +60,7 @@ test("@noci it should preload most recent charts", async ({ page }) => {
 // @noci as it's a special test that's supposed to preload varnish cache
 // for most viewed charts
 test("@noci it should preload most viewed charts", async ({ page }) => {
-  test.setTimeout(5 * 60 * 1000);
+  test.setTimeout(TEST_TIMEOUT_MS);
 
   const fetchedConfigs = await fetchMetadata({
     limit: 25,
@@ -54,7 +70,13 @@ test("@noci it should preload most viewed charts", async ({ page }) => {
 
   for (const key of keys) {
     for (const locale of locales) {
-      await page.goto(`https://visualize.admin.ch/${locale}/v/${key}`);
+      await page
+        .goto(`https://visualize.admin.ch/${locale}/v/${key}`, {
+          timeout: NAVIGATION_TIMEOUT_MS,
+        })
+        .catch((e) => {
+          console.warn(`Error during goto for ${key}: ${formatError(e)}`);
+        });
       // Wait for all the queries to finish
       await page.waitForLoadState("networkidle").catch(() => {
         console.warn("Timeout for", key);
