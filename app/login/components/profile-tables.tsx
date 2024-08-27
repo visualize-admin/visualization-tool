a8cbe02af50a63819b84c202dd79cab7c90693ee
Bartosz Prusinowski <bartosz@interactivethings.com>
2024-08-27 14:19:29 +0200

    fix: Use correct logic to retrieve chart title

--- app/login/components/profile-tables.tsx
+++ app/login/components/profile-tables.tsx
@@ -301,17 +301,16 @@ const ProfileVisualizationsRow = (props: {
 
   const isSingleChart = config.data.chartConfigs.length === 1;
   const chartTitle = useMemo(() => {
-    const title =
-      config.data.layout.meta.title?.[locale] ||
-      config.data.chartConfigs
-        .map((chartConfig) => chartConfig.meta.title?.[locale] ?? false)
-        .filter(truthy)
-        .join(", ");
-
-    return title
-      ? title
-      : t({ id: "annotation.add.title", message: "[ No Title ]" });
-  }, [config.data.chartConfigs, config.data.layout.meta.title, locale]);
+    const title = isSingleChart
+      ? config.data.chartConfigs[0].meta.title[locale]
+      : config.data.layout.meta.title[locale];
+    return title || t({ id: "annotation.add.title", message: "[ No Title ]" });
+  }, [
+    config.data.chartConfigs,
+    config.data.layout.meta.title,
+    isSingleChart,
+    locale,
+  ]);
 
   return (
     <TableRow>
