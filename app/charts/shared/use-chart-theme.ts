012b12997091c8fb1519d295c2cb40b2839841a7
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2024-09-02 15:03:12 +0200

    fix: Changed the fontsize from 10 to 12px

--- app/charts/shared/use-chart-theme.ts
+++ app/charts/shared/use-chart-theme.ts
@@ -10,7 +10,7 @@ export const useChartTheme = () => {
   const domainColor = theme.palette.grey[800];
   const gridColor = theme.palette.grey[300];
   const fontFamily = theme.typography.fontFamily as string;
-  const axisLabelFontSize = 10;
+  const axisLabelFontSize = 12;
   const axisLabelFontWeight = 500;
   const axisLabelColor = theme.palette.grey[800];
   const markBorderColor = theme.palette.grey[100];
