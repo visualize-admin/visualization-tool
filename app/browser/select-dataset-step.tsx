c34f19a3597ebc4d7816c17b35a1de1ac4e3cb5d
Bartosz Prusinowski <bartosz@interactivethings.com>
2024-09-26 09:41:28 +0200

    style: Improve paddings

--- app/browser/select-dataset-step.tsx
+++ app/browser/select-dataset-step.tsx
@@ -391,7 +391,7 @@ const SelectDatasetStepContent = ({
         <PanelBodyWrapper
           type="M"
           className={classes.panelMiddle}
-          sx={{ maxWidth: 1040, p: 5 }}
+          sx={{ maxWidth: 1040, p: 6 }}
         >
           <AnimatePresence mode="wait">
             {dataset ? (
