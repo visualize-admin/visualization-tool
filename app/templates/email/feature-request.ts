7e39d701dc8fff2700932a9dc579a4f5ebbda3ec
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2024-11-12 10:11:34 +0100

    fix: Adjust Email body formatting

--- app/templates/email/feature-request.ts
+++ app/templates/email/feature-request.ts
@@ -1,140 +1,128 @@
 export const featureRequestTemplates = {
   en: `
-Feature Request
-
 Is your feature request related to a problem?
----------------------------------------------
 A clear and concise description of what the problem is. Ex. I'm always frustrated when [...]
 Please describe...
 
+----------------------------------------------
 Describe the solution you'd like
---------------------------------
 A clear and concise description of what you want to happen.
 Please describe...
 
+----------------------------------------------
 Describe alternatives you've considered
----------------------------------------
 A clear and concise description of any alternative solutions or features you've considered.
 Please describe...
 
+----------------------------------------------
 Use cases and impact
---------------------
 Examples of how the feature would be beneficial and an estimation of how much impact it would have.
 Please describe...
 
+----------------------------------------------
 Environment
------------
 Please complete the following information.
 - Visualize environment and version: [e.g., INT v3.12.0]
 - Browser and version [e.g., Chrome 107]
 
+----------------------------------------------
 Additional context
-------------------
 Add any other context or screenshots about the feature request here.
 Please describe...
 `,
 
   de: `
-Funktionsanfrage
-
 Bezieht sich Ihre Feature-Anfrage auf ein Problem?
--------------------------------------------------
 Eine klare und präzise Beschreibung des Problems. Z.B. Es frustriert mich immer, wenn [...]
 Bitte beschreiben Sie...
 
+----------------------------------------------
 Beschreiben Sie die gewünschte Lösung
--------------------------------------
 Eine klare und präzise Beschreibung dessen, was passieren soll.
 Bitte beschreiben Sie...
 
+----------------------------------------------
 Beschreiben Sie Alternativen, die Sie in Betracht gezogen haben
---------------------------------------------------------------
 Eine klare und präzise Beschreibung aller alternativen Lösungen oder Funktionen, die Sie in Betracht gezogen haben.
 Bitte beschreiben Sie...
 
+----------------------------------------------
 Anwendungsfälle und Auswirkungen
---------------------------------
 Beispiele dafür, wie die Funktion nützlich wäre und eine Einschätzung der Auswirkungen.
 Bitte beschreiben Sie...
 
+----------------------------------------------
 Umgebung
---------
 Bitte vervollständigen Sie die folgenden Informationen.
 - Visualize-Umgebung und Version: [z.B., INT v3.12.0]
 - Browser und Version [z.B., Chrome 107]
 
+----------------------------------------------
 Zusätzlicher Kontext
---------------------
 Fügen Sie hier weitere Kontextinformationen oder Screenshots zur Feature-Anfrage hinzu.
 Bitte beschreiben Sie...
 `,
 
   fr: `
-Demande de Fonctionnalité
-
 Votre demande de fonctionnalité est-elle liée à un problème ?
-------------------------------------------------------------
 Une description claire et concise du problème. Ex. Je suis toujours frustré quand [...]
 Veuillez décrire...
 
+----------------------------------------------
 Décrivez la solution que vous souhaitez
----------------------------------------
 Une description claire et concise de ce que vous voulez qu'il se passe.
 Veuillez décrire...
 
+----------------------------------------------
 Décrivez les alternatives que vous avez envisagées
---------------------------------------------------
 Une description claire et concise des solutions ou fonctionnalités alternatives que vous avez envisagées.
 Veuillez décrire...
 
+----------------------------------------------
 Cas d'utilisation et impact
----------------------------
 Exemples de l'utilité de la fonctionnalité et estimation de son impact.
 Veuillez décrire...
 
+----------------------------------------------
 Environnement
--------------
 Veuillez compléter les informations suivantes.
 - Environnement et version Visualize : [ex. INT v3.12.0]
 - Navigateur et version [ex. Chrome 107]
 
+----------------------------------------------
 Contexte supplémentaire
------------------------
 Ajoutez ici tout autre contexte ou captures d'écran concernant la demande de fonctionnalité.
 Veuillez décrire...
 `,
 
   it: `
-Richiesta di Funzionalità
-
 La sua richiesta di funzionalità è legata a un problema?
---------------------------------------------------------
 Una descrizione chiara e concisa del problema. Es. Sono sempre frustrato quando [...]
 Si prega di descrivere...
 
-Descriva la soluzione che vorrebbe
 ----------------------------------
+Descriva la soluzione che vorrebbe
 Una descrizione chiara e concisa di ciò che vuole che accada.
 Si prega di descrivere...
 
+----------------------------------------------
 Descriva le alternative considerate
------------------------------------
 Una descrizione chiara e concisa di eventuali soluzioni o funzionalità alternative che ha considerato.
 Si prega di descrivere...
 
+----------------------------------------------
 Casi d'uso e impatto
---------------------
 Esempi di come la funzionalità sarebbe utile e una stima del suo impatto.
 Si prega di descrivere...
 
+----------------------------------------------
 Ambiente
---------
 Si prega di completare le seguenti informazioni.
 - Ambiente e versione Visualize: [es. INT v3.12.0]
 - Browser e versione [es. Chrome 107]
 
+----------------------------------------------
 Contesto aggiuntivo
--------------------
 Aggiunga qui qualsiasi altro contesto o screenshot sulla richiesta di funzionalità.
 Si prega di descrivere...
 `,
