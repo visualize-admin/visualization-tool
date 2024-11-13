fc405cbb42a45ba909db8ca0cabaf883f0f35a5e
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2024-11-13 07:37:59 +0100

    feat: Extended MenuActionItem w/ leading and trailingIcon

--- app/components/publish-actions.tsx
+++ app/components/publish-actions.tsx
@@ -1,11 +1,3 @@
-import { CHART_RESIZE_EVENT_TYPE } from "@/charts/shared/use-size";
-import { CopyToClipboardTextInput } from "@/components/copy-to-clipboard-text-input";
-import Flex from "@/components/flex";
-import { Radio } from "@/components/form";
-import { IconLink } from "@/components/links";
-import { Icon } from "@/icons";
-import useEvent from "@/utils/use-event";
-import { useI18n } from "@/utils/use-i18n";
 import { t, Trans } from "@lingui/macro";
 import {
   Box,
@@ -20,15 +12,14 @@ import {
 } from "@mui/material";
 import { ChangeEvent, ReactNode, RefObject, useEffect, useState } from "react";
 
-
 import { CHART_RESIZE_EVENT_TYPE } from "@/charts/shared/use-size";
 import { CopyToClipboardTextInput } from "@/components/copy-to-clipboard-text-input";
-import Flex from "@/components/flex";
+import { default as Flex } from "@/components/flex";
 import { Radio } from "@/components/form";
 import { IconLink } from "@/components/links";
 import { ConfiguratorStatePublished } from "@/configurator";
 import { Icon } from "@/icons";
-import useEvent from "@/utils/use-event";
+import { default as useEvent } from "@/utils/use-event";
 import { useI18n } from "@/utils/use-i18n";
 
 type PublishActionProps = {
