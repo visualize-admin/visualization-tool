9c611ce9039fff5d2d0e8600940657e681b1ff94
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2024-11-14 12:15:56 +0100

    fix: Added Newsletter Body & Subject

---
+++ app/templates/email/newsletter.ts
@@ -0,0 +1,17 @@
+export const newsletterTemplate = {
+  en: `
+    I would like to subscribe to the Visualize newsletter. Please add my email address to the mailing list
+  `,
+
+  de: `
+  Ich möchte mich für den Visualize Newsletter anmelden. Bitte fügen Sie meine E-Mail-Adresse zur Verteilerliste hinzu.
+  `,
+
+  fr: `
+    Je souhaite m'abonner à la newsletter de Visualize. Veuillez ajouter mon adresse e-mail à la liste de diffusion.
+  `,
+
+  it: `
+    Vorrei iscrivermi alla newsletter di Visualize. Si prega di aggiungere il mio indirizzo e-mail alla mailing list.
+  `,
+};
