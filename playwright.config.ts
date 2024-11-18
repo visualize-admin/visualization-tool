afc221ed7a624fca40d2123e31ceb505ae579868
Bartosz Prusinowski <bartosz@interactivethings.com>
2024-11-18 17:38:44 +0100

    fix: Try to use sharding for e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,9 +28,9 @@ const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
-  workers: 4,
+  workers: process.env.CI ? 1 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "list" : "line",
+  reporter: process.env.CI ? "blob" : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     contextOptions: {
