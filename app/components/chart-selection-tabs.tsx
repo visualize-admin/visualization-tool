700dbeac74bfa925246e8ba24321a51199c9e04d
Bartosz Prusinowski <bartosz@interactivethings.com>
2024-11-26 14:04:42 +0100

    refactor: Extract AddButton

--- app/components/chart-selection-tabs.tsx
+++ app/components/chart-selection-tabs.tsx
@@ -13,6 +13,7 @@ import clsx from "clsx";
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
+import { AddButton } from "@/components/add-button";
 import { ArrowMenuTopCenter } from "@/components/arrow-menu";
 import { DuplicateChartMenuActionItem } from "@/components/chart-shared";
 import { DragHandle } from "@/components/drag-handle";
@@ -524,15 +525,9 @@ const TabsInner = ({
         </DragDropContext>
       </TabContext>
       {addable && (
-        <Button
-          color="primary"
-          variant="contained"
-          startIcon={<Icon name="add" />}
-          onClick={onChartAdd}
-          sx={{ minWidth: "fit-content", ml: "0.5rem", px: 3 }}
-        >
+        <AddButton onClick={onChartAdd}>
           <Trans id="chart-selection-tabs.add-chart">Add chart</Trans>
-        </Button>
+        </AddButton>
       )}
     </div>
   );
