8a39e65fbb008abee7c6f9e31fdd5f801a056918
Bartosz Prusinowski <bartosz@interactivethings.com>
2024-12-12 13:12:54 +0100

    refactor: Extract useOrderedLocales

--- app/locales/use-locale.ts
+++ app/locales/use-locale.ts
@@ -1,6 +1,6 @@
 import { createContext, useContext } from "react";
 
-import { defaultLocale, Locale } from "./locales";
+import { defaultLocale, Locale, locales } from "./locales";
 
 const LocaleContext = createContext<Locale>(defaultLocale);
 
@@ -8,4 +8,10 @@ export const LocaleProvider = LocaleContext.Provider;
 
 export const useLocale = () => {
   return useContext(LocaleContext);
+};
+
+/** Returns ordered locales, with the current locale being first. */
+export const useOrderedLocales = () => {
+  const locale = useLocale();
+  return [locale, ...locales.filter((l) => l !== locale)];
 };
