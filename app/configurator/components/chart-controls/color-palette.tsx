78263c560875e7be2572172ae0c853fa4f7b2a9d
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-01-27 14:32:57 +0100

    refactor: moved Drawer into its own file and made it compatible for all types

--- app/configurator/components/chart-controls/color-palette.tsx
+++ app/configurator/components/chart-controls/color-palette.tsx
@@ -1,9 +1,9 @@
-import { t, Trans } from "@lingui/macro";
+import { Trans } from "@lingui/macro";
 import {
   Box,
   Button,
   Divider,
-  IconButton,
+  Grid,
   MenuItem,
   Select,
   SelectProps,
@@ -12,25 +12,23 @@ import {
 } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import get from "lodash/get";
-import dynamic from "next/dynamic";
-import { forwardRef, MouseEventHandler, useCallback, useState } from "react";
+import { MouseEventHandler, useCallback, useState } from "react";
 
 import { EncodingFieldType } from "@/charts/chart-config-ui-options";
 import { hasDimensionColors } from "@/charts/shared/colors";
-import { LegendItem, LegendSymbol } from "@/charts/shared/legend-color";
+import { LegendSymbol } from "@/charts/shared/legend-color";
 import Flex from "@/components/flex";
-import { Input, Label } from "@/components/form";
+import { Label } from "@/components/form";
 import { getChartConfig } from "@/config-utils";
 import {
   ConfiguratorStateConfiguringChart,
+  CustomPaletteType,
   isColorInConfig,
   isConfiguring,
   useConfiguratorState,
 } from "@/configurator";
 import { mapValueIrisToColor } from "@/configurator/components/ui-helpers";
 import { Component, isNumericalMeasure } from "@/domain/data";
-import { Icon } from "@/icons";
-import SvgIcClose from "@/icons/components/IcClose";
 import { useUser } from "@/login/utils";
 import {
   categoricalPalettes,
@@ -39,54 +37,27 @@ import {
   getDefaultCategoricalPalette,
   getPalette,
 } from "@/palettes";
-import { theme } from "@/themes/federal";
-import { createCustomColorPalette } from "@/utils/chart-config/api";
+import { getCustomColorPalettes } from "@/utils/chart-config/api";
 import useEvent from "@/utils/use-event";
-import { useMutate } from "@/utils/use-fetch-data";
+import { useFetchData } from "@/utils/use-fetch-data";
 
-import { ConfiguratorDrawer, DRAWER_WIDTH } from "../drawer";
+import { ConfiguratorDrawer } from "../drawer";
 
-//have to import dynamically to avoid @uiw/react-color dependency issues with the server
-const ColorPickerMenu = dynamic(
-  () =>
-    import("../chart-controls/color-picker").then((mod) => mod.ColorPickerMenu),
-  { ssr: false }
-);
+import { DrawerContent } from "./create-color-palette";
 
 const useStyles = makeStyles({
   root: {
     width: "100%",
+    padding: "8px 0px",
   },
   select: {
     "&.MuiSelect-select": {
       padding: "0 0.75rem",
+      height: "auto",
       display: "flex",
       alignItems: "center",
       justifyContent: "flex-start",
     },
-  },
-  autocompleteMenuContent: {
-    "--mx": "1rem",
-    "--colorBoxSize": "1.25rem",
-    "--columnGap": "0.75rem",
-    width: DRAWER_WIDTH,
-  },
-  autocompleteHeader: {
-    margin: "1rem var(--mx)",
-  },
-  autocompleteApplyButtonContainer: {
-    padding: "1rem var(--mx)",
-    background: "rgba(255,255,255,0.75)",
-  },
-  textInput: {
-    margin: `${theme.spacing(4)} 0px`,
-    padding: "30px",
-    width: "100%",
-    height: 40,
-    minHeight: 40,
-  },
-  autocompleteApplyButton: {
-    justifyContent: "center",
   },
 });
 
@@ -109,6 +80,14 @@ export const ColorPalette = ({
   const chartConfig = getChartConfig(state);
   const classes = useStyles();
   const user = useUser();
+
+  const { data: customColorPalettes, invalidate } = useFetchData({
+    queryKey: ["colorPalettes", user?.id],
+    queryFn: getCustomColorPalettes,
+    options: {
+      enable: !!user?.id,
+    },
+  });
 
   const hasColors = hasDimensionColors(component);
   const defaultPalette =
@@ -135,11 +114,65 @@ export const ColorPalette = ({
 
   const currentPalette = palettes.find((p) => p.value === currentPaletteName);
 
-  const handleChangePalette: SelectProps["onChange"] = useEvent((ev) => {
-    const palette = palettes.find((p) => p.value === ev.target.value);
-    if (!component || !palette) {
+  const handleChangePalette: SelectProps["onChange"] = useEvent((ev) =>
+    handleChartConfigUpdate(ev.target.value as string)
+  );
+
+  const handleChartConfigUpdate = (
+    value?: string,
+    selectedPalette?: CustomPaletteType
+  ) => {
+    const regularPalette = palettes.find((p) => p.value === value);
+    const customPalette =
+      customColorPalettes?.find((p) => p.paletteId === value) ||
+      selectedPalette;
+
+    if (!component) {
       return;
     }
+
+    if (customPalette && values) {
+      const colorMapping = Object.fromEntries(
+        values.map((value, index) => [
+          value.id,
+          customPalette.colors[index % customPalette.colors.length],
+        ])
+      );
+
+      if (isColorInConfig(chartConfig)) {
+        dispatch({
+          type: "COLOR_FIELD_SET",
+          value:
+            chartConfig.fields.color.type === "single"
+              ? {
+                  type: chartConfig.fields.color.type,
+                  paletteId: customPalette.paletteId,
+                  color: customPalette.colors[0],
+                }
+              : {
+                  type: chartConfig.fields.color.type,
+                  paletteId: customPalette.paletteId,
+                  colorMapping,
+                },
+        });
+      } else {
+        dispatch({
+          type: "CHART_PALETTE_CHANGED",
+          value: {
+            field,
+            colorConfigPath,
+            paletteId: customPalette.paletteId,
+            colorMapping,
+          },
+        });
+      }
+      return;
+    }
+
+    if (!regularPalette) {
+      return;
+    }
+
     if (isColorInConfig(chartConfig)) {
       dispatch({
         type: "COLOR_FIELD_SET",
@@ -147,14 +180,14 @@ export const ColorPalette = ({
           chartConfig.fields.color.type === "single"
             ? {
                 type: chartConfig.fields.color.type,
-                paletteId: palette.value,
-                color: palette.colors[0],
+                paletteId: regularPalette.value,
+                color: regularPalette.colors[0],
               }
             : {
                 type: chartConfig.fields.color.type,
-                paletteId: palette.value,
+                paletteId: regularPalette.value,
                 colorMapping: mapValueIrisToColor({
-                  paletteId: palette.value,
+                  paletteId: regularPalette.value,
                   dimensionValues: component.values,
                 }),
               },
@@ -165,29 +198,34 @@ export const ColorPalette = ({
         value: {
           field,
           colorConfigPath,
-          paletteId: palette.value,
+          paletteId: regularPalette.value,
           colorMapping: mapValueIrisToColor({
-            paletteId: palette.value,
+            paletteId: regularPalette.value,
             dimensionValues: component.values,
           }),
         },
       });
     }
-  });
+  };
 
   const withColorField = isColorInConfig(chartConfig);
 
   const [anchorEl, setAnchorEl] = useState<HTMLElement>();
-  const handleOpenAutocomplete: MouseEventHandler<HTMLButtonElement> = useEvent(
-    (ev) => {
+  const handleOpenCreateColorPalette: MouseEventHandler<HTMLButtonElement> =
+    useEvent((ev) => {
       setAnchorEl(ev.currentTarget);
+    });
+
+  const handleCloseCreateColorPalette = useEvent(
+    (palette?: CustomPaletteType) => {
+      invalidate();
+      setAnchorEl(undefined);
+
+      handleChartConfigUpdate(palette?.paletteId, palette);
+
+      anchorEl?.focus();
     }
   );
-  const handleCloseAutocomplete = useEvent(() => {
-    setAnchorEl(undefined);
-
-    anchorEl?.focus();
-  });
 
   return (
     <Box mt={2} sx={{ pointerEvents: disabled ? "none" : "auto" }}>
@@ -199,34 +237,50 @@ export const ColorPalette = ({
         classes={classes}
         renderValue={() => {
           return (
-            <Flex>
+            <Grid
+              container
+              spacing={0.5}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                "& .MuiGrid-item": {
+                  display: "flex",
+                  alignItems: "center",
+                },
+              }}
+            >
               {currentPalette
                 ? currentPalette.colors.map((color: string) => (
-                    <ColorSquare
-                      key={color}
-                      color={color}
-                      disabled={disabled}
-                    />
+                    <Grid item key={color}>
+                      <ColorSquare color={color} disabled={disabled} />
+                    </Grid>
                   ))
                 : withColorField &&
                   (chartConfig.fields.color.type === "single" ? (
-                    <ColorSquare
-                      key={chartConfig.fields.color.color}
-                      color={chartConfig.fields.color.color as string}
-                      disabled={disabled}
-                    />
+                    <Grid item>
+                      <ColorSquare
+                        key={chartConfig.fields.color.color}
+                        color={chartConfig.fields.color.color as string}
+                        disabled={disabled}
+                      />
+                    </Grid>
                   ) : (
-                    Object.entries(chartConfig.fields.color.colorMapping).map(
-                      ([id, color]) => (
-                        <ColorSquare
-                          key={id}
-                          color={color as string}
-                          disabled={disabled}
-                        />
+                    customColorPalettes
+                      ?.find(
+                        (palette) =>
+                          palette.paletteId ===
+                          chartConfig.fields.color.paletteId
                       )
-                    )
+                      ?.colors.map((color, i) => (
+                        <Grid item key={`color-palette-tile-${i}`}>
+                          <ColorSquare
+                            color={color as string}
+                            disabled={disabled}
+                          />
+                        </Grid>
+                      ))
                   ))}
-            </Flex>
+            </Grid>
           );
         }}
         value={
@@ -238,7 +292,7 @@ export const ColorPalette = ({
       >
         {user && (
           <Button
-            onClick={handleOpenAutocomplete}
+            onClick={handleOpenCreateColorPalette}
             variant="text"
             sx={{
               width: "100%",
@@ -246,9 +300,56 @@ export const ColorPalette = ({
               paddingX: 4,
             }}
           >
-            Add color palette
+            <Trans id="login.profile.my-color-palettes.add">
+              Add color palette
+            </Trans>
           </Button>
         )}
+        {customColorPalettes && customColorPalettes?.length > 0 && (
+          <Box
+            sx={{
+              paddingTop: 3,
+              paddingX: 4,
+              display: "flex",
+              gap: 2,
+              flexDirection: "column",
+            }}
+          >
+            <Typography variant="caption" fontWeight={700} align="left">
+              <Trans id="controls.custom-color-palettes">
+                Custom color palettes
+              </Trans>
+            </Typography>
+            <Divider sx={{ width: "100%", paddingY: 1 }} />
+          </Box>
+        )}
+        {customColorPalettes &&
+          customColorPalettes?.length > 0 &&
+          customColorPalettes
+            .filter((palette) => palette.type === "categorical")
+            .map((palette, index) => (
+              <MenuItem
+                sx={{ paddingY: 2 }}
+                key={`${palette.paletteId}${index}`}
+                value={palette.paletteId}
+              >
+                <Flex sx={{ flexDirection: "column", gap: "4px" }}>
+                  <Typography component="div" variant="caption">
+                    {palette.name}
+                  </Typography>
+                  <Flex>
+                    {palette.colors.map((color) => (
+                      <ColorSquare
+                        key={`option-${color}`}
+                        color={color}
+                        disabled={false}
+                      />
+                    ))}
+                  </Flex>
+                </Flex>
+              </MenuItem>
+            ))}
+
         <Box
           sx={{
             paddingTop: 3,
@@ -259,7 +360,9 @@ export const ColorPalette = ({
           }}
         >
           <Typography variant="caption" fontWeight={700} align="left">
-            Visualize color palettes
+            <Trans id="controls.visualize-color-palette">
+              Visualize color palettes
+            </Trans>
           </Typography>
           <Divider sx={{ width: "100%", paddingY: 1 }} />
         </Box>
@@ -296,7 +399,11 @@ export const ColorPalette = ({
       )}
       {values && (
         <ConfiguratorDrawer open={!!anchorEl} hideBackdrop>
-          <DrawerContent onClose={handleCloseAutocomplete} values={values} />
+          <DrawerContent
+            onClose={(palette) => handleCloseCreateColorPalette(palette)}
+            type="categorical"
+            customColorPalettes={customColorPalettes}
+          />
         </ConfiguratorDrawer>
       )}
     </Box>
@@ -324,7 +431,7 @@ const useColorSquareStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
-const ColorSquare = ({
+export const ColorSquare = ({
   disabled,
   color,
 }: {
@@ -436,177 +543,3 @@ const ColorPaletteControls = ({
     return <Box mt={2} />;
   }
 };
-
-const DrawerContent = forwardRef<
-  HTMLDivElement,
-  {
-    onClose: () => void;
-    values: { id: string; symbol: LegendSymbol }[];
-  }
->((props, ref) => {
-  const { onClose, values } = props;
-  const classes = useStyles();
-
-  const [colorValues, setColorValues] = useState<
-    { color: string; id: string }[]
-  >([]);
-  const [titleInput, setTitleInput] = useState<string>("");
-
-  const [state, dispatch] = useConfiguratorState(isConfiguring);
-  const chartConfig = getChartConfig(state);
-
-  const createColorPalette = useMutate(createCustomColorPalette);
-
-  const updateColor = useCallback((color, id) => {
-    setColorValues((prevColors) => {
-      return prevColors.map((oldColor) =>
-        oldColor.id === id ? { ...oldColor, color } : oldColor
-      );
-    });
-  }, []);
-
-  const saveColorPalette = useEvent(async () => {
-    const colorMapping = Object.fromEntries(
-      values.map((value, index) => [
-        value.id,
-        colorValues[index % colorValues.length].color,
-      ])
-    );
-
-    if (isColorInConfig(chartConfig)) {
-      dispatch({
-        type: "COLOR_FIELD_SET",
-        value:
-          chartConfig.fields.color.type === "single"
-            ? {
-                type: chartConfig.fields.color.type,
-                paletteId: titleInput,
-                color: colorValues[0].color,
-              }
-            : {
-                type: chartConfig.fields.color.type,
-                paletteId: titleInput,
-                colorMapping: colorMapping,
-              },
-      });
-    }
-
-    await createColorPalette.mutate({
-      name: titleInput,
-      colors: colorValues.map((c) => c.color),
-      type: "categorical",
-    });
-
-    onClose();
-  });
-
-  return (
-    <div
-      className={classes.autocompleteMenuContent}
-      ref={ref}
-      data-testid="colors-filters-drawer"
-    >
-      <Box className={classes.autocompleteHeader}>
-        <Flex alignItems="center" justifyContent="space-between">
-          <Typography
-            variant="h4"
-            sx={{
-              py: 4,
-            }}
-          >
-            <Trans id="controls.custom-color-palettes">
-              Custom color palettes
-            </Trans>
-          </Typography>
-          <IconButton sx={{ mt: "-0.5rem" }} size="small" onClick={onClose}>
-            <SvgIcClose fontSize="inherit" />
-          </IconButton>
-        </Flex>
-        <Typography variant="body2" color="textSecondary">
-          <Trans id="controls.custom-color-palettes.caption">
-            Use distinct, high-contrast colors. Avoid using too many colors,
-            maximum 5–7. Apply sequential palettes for ordered data and
-            diverging palettes for extremes.
-          </Trans>
-        </Typography>
-      </Box>
-      <Flex
-        sx={{
-          mt: 4,
-          paddingX: "1rem",
-        }}
-        flexDirection={"column"}
-      >
-        <Input
-          label={t({ id: "controls.custom-color-palettes.title" })}
-          name="custom-color-palette-title"
-          value={titleInput}
-          onChange={(e) => setTitleInput(e.target.value)}
-        />
-        {
-          <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 5 }}>
-            {colorValues.map((item) => {
-              return (
-                <Box
-                  key={item.id}
-                  sx={{
-                    paddingY: 1,
-                  }}
-                >
-                  <Flex
-                    sx={{
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      width: "100%",
-                    }}
-                  >
-                    <LegendItem
-                      item={item.color}
-                      color={item.color}
-                      symbol={"square"}
-                      usage="colorPicker"
-                    />
-                    <ColorPickerMenu
-                      key={`color-picker-${item.id}`}
-                      colors={colorValues.map((c) => c.color)}
-                      selectedColor={item.color}
-                      onChange={(newColor) => {
-                        updateColor(newColor, item.id);
-                      }}
-                    />
-                  </Flex>
-                </Box>
-              );
-            })}
-          </Box>
-        }
-        <Button
-          variant="text"
-          fullWidth
-          disabled={values.length <= colorValues.length}
-          onClick={() =>
-            setColorValues((ps) => [
-              ...ps,
-              { color: "#000000", id: values[colorValues.length].id },
-            ])
-          }
-          startIcon={<Icon name="add" />}
-          sx={{ minWidth: "fit-content", mt: 2, paddingLeft: 0 }}
-        >
-          <Trans id="controls.custom-color-palettes.add-color">Add color</Trans>
-        </Button>
-      </Flex>
-      <Box className={classes.autocompleteApplyButtonContainer}>
-        <Button
-          disabled={colorValues.length === 0 || titleInput === ""}
-          className={classes.autocompleteApplyButton}
-          onClick={saveColorPalette}
-        >
-          <Trans id="controls.custom-color-palettes.set-values-apply">
-            Save color palette
-          </Trans>
-        </Button>
-      </Box>
-    </div>
-  );
-});
