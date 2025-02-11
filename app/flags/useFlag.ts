b55241e59d25f1a344e9842ff21ae8f99cdd56d0
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2025-02-11 11:28:15 +0100

    feat: extended flags to toggle debug w/ keyboard commands

--- app/flags/useFlag.ts
+++ app/flags/useFlag.ts
@@ -1,6 +1,9 @@
+import qs from "qs";
 import { useEffect, useState } from "react";
 
-import { flag } from "./flag";
+import { useKeyboardShortcut } from "@/utils/use-keyboard-shortcuts";
+
+import { flag, FLAG_PREFIX } from "./flag";
 import { FlagName } from "./types";
 
 export default function useFlagValue(name: FlagName) {
@@ -40,3 +43,37 @@ export function useFlags() {
 
   return flags;
 }
+
+export const useDebugShortcut = (enable?: boolean) => {
+  useKeyboardShortcut(
+    {
+      keys: ["t", "d"],
+      ctrlKey: true,
+    },
+    (event) => {
+      event.preventDefault();
+      enable && addDebugFlag();
+    }
+  );
+};
+
+const addDebugFlag = () => {
+  const currentUrl = new URL(window.location.href);
+  const currentParams = qs.parse(currentUrl.search.substring(1));
+
+  const debugFlagKey = `${FLAG_PREFIX}debug`;
+  const hasDebugFlag = currentParams[debugFlagKey] === "true";
+
+  if (hasDebugFlag) {
+    delete currentParams[debugFlagKey];
+  } else {
+    currentParams[debugFlagKey] = "true";
+  }
+
+  const newSearch = qs.stringify(currentParams);
+  const newUrl = `${window.location.pathname}${newSearch ? `?${newSearch}` : ""}`;
+  window.history.pushState({}, "", newUrl);
+
+  const event = new Event("popstate");
+  window.dispatchEvent(event);
+};
