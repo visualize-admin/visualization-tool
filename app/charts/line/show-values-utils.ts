d0a8283e6b6d4f9b662de253daebc2ba0b3f1305
Bartosz Prusinowski <bartosz@interactivethings.com>
2025-03-04 13:19:52 +0100

    fix: Offset

--- app/charts/line/show-values-utils.ts
+++ app/charts/line/show-values-utils.ts
@@ -48,7 +48,7 @@ export const useShowLineValueLabelsVariables = (
   );
 
   return {
-    yOffset: fontSize,
+    yOffset: showValues ? fontSize : 0,
     showValues,
     valueLabelFormatter,
   };
