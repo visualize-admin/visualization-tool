8fb8c2e3c525c221c61fc55def426ea72896831a
Bartosz Prusinowski <bartosz@interactivethings.com>
2025-03-27 16:59:09 +0100

    feat: Adjust Actions to new CI / CD

--- app/homepage/actions.tsx
+++ app/homepage/actions.tsx
@@ -1,4 +1,7 @@
 import { ContentWrapper } from "@interactivethings/swiss-federal-ci/dist/components";
+import { Box, Button, Divider, Typography } from "@mui/material";
+
+import { useIsMobile } from "@/utils/use-is-mobile";
 
 type ActionElementProps = {
   headline: string;
@@ -18,10 +21,72 @@ export const Actions = ({
   bugReport: ActionElementProps;
   featureRequest: ActionElementProps;
 }) => {
+  const isMobile = useIsMobile();
   return (
     <>
-      <ContentWrapper sx={{ py: 15 }}></ContentWrapper>
-      <ContentWrapper sx={{ py: 15 }}></ContentWrapper>
+      <ContentWrapper
+        sx={{
+          alignItems: "unset !important",
+          flexDirection: isMobile ? "column" : "row",
+          gap: 12,
+          py: 15,
+        }}
+      >
+        <Action {...contribute} />
+        <Divider flexItem orientation={isMobile ? "horizontal" : "vertical"} />
+        <Action {...newsletter} />
+      </ContentWrapper>
+      <Box sx={{ backgroundColor: "background.paper" }}>
+        <ContentWrapper
+          sx={{
+            alignItems: "unset !important",
+            flexDirection: isMobile ? "column" : "row",
+            gap: 12,
+            py: 15,
+          }}
+        >
+          <Action {...bugReport} />
+          <Divider
+            flexItem
+            orientation={isMobile ? "horizontal" : "vertical"}
+          />
+          <Action {...featureRequest} />
+        </ContentWrapper>
+      </Box>
     </>
+  );
+};
+
+const Action = ({
+  headline,
+  description,
+  buttonLabel,
+  buttonUrl,
+}: ActionElementProps) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "flex-start",
+        width: "100%",
+      }}
+    >
+      <Box sx={{ mb: 8 }}>
+        <Typography variant="h2">{headline}</Typography>
+        <Typography variant="body2" sx={{ mt: 4 }}>
+          {description}
+        </Typography>
+      </Box>
+      <Button
+        href={buttonUrl}
+        size="sm"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {buttonLabel}
+      </Button>
+    </Box>
   );
 };
