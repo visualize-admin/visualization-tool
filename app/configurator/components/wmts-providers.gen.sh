6dcc660a13666f69959aaf1393172012d45a1862
Patrick Browne <pt.browne@gmail.com>
2025-04-15 15:06:14 +0200

    feat: Add script to download providers from geo admin github

---
+++ app/configurator/components/wmts-providers.gen.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/geoadmin/web-mapviewer/refs/heads/develop/packages/mapviewer/src/modules/menu/components/advancedTools/ImportCatalogue/external-providers.json -O wmts-providers.json
