67f1878c01b1966415171bc30898a6522d7035b0
Patrick Browne <pt.browne@gmail.com>
2025-04-24 09:15:31 +0200

    feat: Start a file with extra information

--- app/charts/map/wmts-providers-autocomplete.tsx
+++ app/charts/map/wmts-providers-autocomplete.tsx
@@ -1,8 +1,15 @@
 import { t } from "@lingui/macro";
-import { Autocomplete, TextField, Theme } from "@mui/material";
+import { Alert, Autocomplete, TextField, Theme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import { useMemo } from "react";
 
+import wmtsProvidersExtra_ from "@/charts/map/wmts-providers-extra.json";
 import wmtsProviders from "@/charts/map/wmts-providers.json";
+
+const wmtsProvidersExtra = wmtsProvidersExtra_ as Record<
+  string,
+  { hidden?: boolean; note?: string }
+>;
 
 type ProviderUrl = string;
 
@@ -36,61 +43,78 @@ const ProviderAutocomplete = ({
   onChange: (newValue: ProviderUrl | null) => void;
 }) => {
   const classes = useStyles();
-  const options = wmtsProviders.map((provider) => {
-    const url = new URL(provider);
-    return {
-      url: url,
-      host: url.host,
-      value: provider,
-    };
-  });
+  const extraInfo = value ? wmtsProvidersExtra[value] : undefined;
+  const options = useMemo(() => {
+    return wmtsProviders
+      .filter((p) => {
+        return !wmtsProvidersExtra[p] || !wmtsProvidersExtra[p].hidden;
+      })
+      .map((provider) => {
+        const url = new URL(provider);
+        return {
+          url: url,
+          host: url.host,
+          value: provider,
+        };
+      });
+  }, []);
   return (
-    <Autocomplete
-      options={options}
-      value={value}
-      freeSolo
-      groupBy={(option) => option.host}
-      getOptionLabel={(option) =>
-        typeof option === "string" ? option : option.value
-      }
-      onChange={(_ev, newValue) =>
-        onChange(
-          typeof newValue === "string" ? newValue : (newValue?.value ?? null)
-        )
-      }
-      size="small"
-      ListboxProps={{
-        className: classes.listbox,
-      }}
-      slotProps={{
-        paper: {
-          elevation: 6,
-          className: classes.paper,
-        },
-      }}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          placeholder={t({
-            id: "wmts-providers-autocomplete.placeholder",
-            message: "Enter a WMTS provider URL",
-          })}
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          size="small"
-          InputProps={{
-            ...params.InputProps,
-            slotProps: {
-              input: {
-                className: classes.input,
+    <>
+      <Autocomplete
+        options={options}
+        value={value}
+        freeSolo
+        groupBy={(option) => option.host}
+        getOptionLabel={(option) =>
+          typeof option === "string" ? option : option.value
+        }
+        onChange={(_ev, newValue) =>
+          onChange(
+            typeof newValue === "string" ? newValue : (newValue?.value ?? null)
+          )
+        }
+        size="small"
+        ListboxProps={{
+          className: classes.listbox,
+        }}
+        slotProps={{
+          paper: {
+            elevation: 6,
+            className: classes.paper,
+          },
+        }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            placeholder={t({
+              id: "wmts-providers-autocomplete.placeholder",
+              message: "Enter a WMTS provider URL",
+            })}
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            size="small"
+            InputProps={{
+              ...params.InputProps,
+              slotProps: {
+                input: {
+                  className: classes.input,
+                },
               },
-            },
-          }}
-        />
+            }}
+          />
+        )}
+      />
+      {extraInfo && extraInfo.note && (
+        <Alert
+          severity="orange"
+          sx={{ mb: 2, boxShadow: "none", p: 1 }}
+          elevation={0}
+        >
+          {extraInfo.note}
+        </Alert>
       )}
-      sx={{ mb: 2 }}
-    />
+    </>
   );
 };
 
