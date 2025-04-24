64669974407ee5a1aa5f1a835df557d06fbe6d4e
Patrick Browne <pt.browne@gmail.com>
2025-04-24 10:18:27 +0200

    wip

--- app/configurator/components/wmts-providers.gen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl https://raw.githubusercontent.com/geoadmin/web-mapviewer/refs/heads/develop/packages/mapviewer/src/modules/menu/components/advancedTools/ImportCatalogue/external-providers.json -O wmts-providers.json
