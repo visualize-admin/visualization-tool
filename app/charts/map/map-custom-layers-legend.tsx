4cd06ed73d9eb0617645f8638655070951c4e330
Patrick Browne <pt.browne@gmail.com>
2025-04-29 14:44:00 +0200

    fix: Support html description for external layers

--- app/charts/map/map-custom-layers-legend.tsx
+++ app/charts/map/map-custom-layers-legend.tsx
@@ -67,7 +67,11 @@ export const MapCustomLayersLegend = ({
                 </Typography>
                 {layer.description ? (
                   <InfoIconTooltip
-                    title={layer.description}
+                    title={
+                      <div
+                        dangerouslySetInnerHTML={{ __html: layer.description }}
+                      />
+                    }
                     sx={{ width: "fit-content" }}
                   />
                 ) : null}
