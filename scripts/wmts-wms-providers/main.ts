e269e137d83ccd9a2ddab700010cb08fc56380b3
Patrick Browne <pt.browne@gmail.com>
2025-05-14 11:53:13 +0200

    fix: Removed servers that would not work
    Use Tomas Bettler list along with some known good URLs

---
+++ scripts/wmts-wms-providers/main.ts
@@ -0,0 +1,93 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const normalizeUrl = (url: string): string => {
+  // remove trailing ?
+  // remove request parameters
+  return url.split("?")[0].split("#")[0].trim();
+};
+
+const knownGoodEndpoints = [
+  "https://bio.discomap.eea.europa.eu/arcgis/services/Ecosystem/Ecosystems/MapServer/WMSServer",
+  "https://geoserver.swissdatacube.org/geoserver/gwc/service/wmts?REQUEST=GetCapabilities",
+];
+
+async function fetchAllowList() {
+  /** fetches https://rdataflow.github.io/WMS-monitor/result.txt & https://rdataflow.github.io/WTMS-monitor/result.txt */
+  const urls = [
+    "https://rdataflow.github.io/WMS-monitor/result.txt",
+    "https://rdataflow.github.io/WTMS-monitor/result.txt",
+  ];
+  const results = await Promise.all(
+    urls.map((url) =>
+      fetch(url)
+        .then((x) => x.text())
+        .then((text) => {
+          return text.split("\n");
+        })
+    )
+  );
+  return new Set(
+    results
+      .flat()
+      .filter((x) => x.trim() !== "")
+      .concat(knownGoodEndpoints)
+      .map((x) => normalizeUrl(x.trim()))
+  );
+}
+
+// Function to read and filter the repository providers JSON file
+function filterProviders(providers: string[], allowList: Set<string>): any[] {
+  try {
+    const filteredProviders = providers.filter(
+      (provider: any) => provider && allowList.has(normalizeUrl(provider))
+    );
+
+    return filteredProviders;
+  } catch (error) {
+    console.error(`Error processing JSON file: ${error}`);
+    process.exit(1);
+  }
+}
+
+// Main function
+async function main() {
+  const initialProvidersPath = path.join(
+    __dirname,
+    "../../",
+    "./app/charts/map/initial-wms-wmts-providers.json"
+  );
+
+  const providersPath = path.join(
+    __dirname,
+    "../../",
+    "./app/charts/map/wms-wmts-providers.json"
+  ); // Path to your JSON file
+
+  // Read the allow list and create a Set
+  const allowList = await fetchAllowList();
+  console.log(`Loaded ${allowList.size} allowed URLs`);
+
+  // Filter the providers
+  const jsonContent = fs.readFileSync(initialProvidersPath, "utf-8");
+  const providers = JSON.parse(jsonContent);
+  console.log(`Loaded ${providers.length} providers from JSON file`);
+  const filteredProviders = filterProviders(providers, allowList);
+  console.log(
+    `Filtered ${filteredProviders.length} providers from the JSON file`
+  );
+
+  // Write the result to a new file
+  fs.writeFileSync(providersPath, JSON.stringify(filteredProviders, null, 2));
+  console.log(`Filtered providers saved to ${providersPath}`);
+}
+
+// Execute the main function
+main()
+  .then((x) => {
+    console.log("Done");
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+    process.exit(1);
+  });
