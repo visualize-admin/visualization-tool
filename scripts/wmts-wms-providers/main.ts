2a9b10449ea82cfea2ccde4dae835ccd7853c19f
Patrick Browne <pt.browne@gmail.com>
2025-05-15 13:59:55 +0200

    chore: cleanup

--- scripts/wmts-wms-providers/main.ts
+++ scripts/wmts-wms-providers/main.ts
@@ -2,13 +2,10 @@ import * as fs from "fs";
 import * as path from "path";
 
 const normalizeUrl = (url: string): string => {
-  // remove trailing ?
-  // remove request parameters
   return url.split("?")[0].split("#")[0].trim();
 };
 
 async function fetchAllowList() {
-  /** fetches https://rdataflow.github.io/WMS-monitor/result.txt & https://rdataflow.github.io/WTMS-monitor/result.txt */
   const urls = ["https://rdataflow.github.io/WMTS-WMS-monitor/result.json"];
   const results = await Promise.all(
     urls.map((url) => fetch(url).then((x) => x.json()))
@@ -21,7 +18,6 @@ async function fetchAllowList() {
   );
 }
 
-// Function to read and filter the repository providers JSON file
 function filterProviders(providers: string[], allowList: Set<string>): any[] {
   try {
     const filteredProviders = providers.filter(
@@ -35,7 +31,6 @@ function filterProviders(providers: string[], allowList: Set<string>): any[] {
   }
 }
 
-// Main function
 async function main() {
   const initialProvidersPath = path.join(
     __dirname,
@@ -47,13 +42,11 @@ async function main() {
     __dirname,
     "../../",
     "./app/charts/map/wms-wmts-providers.json"
-  ); // Path to your JSON file
+  );
 
-  // Read the allow list and create a Set
   const allowList = await fetchAllowList();
   console.log(`Loaded ${allowList.size} allowed URLs`);
 
-  // Filter the providers
   const jsonContent = fs.readFileSync(initialProvidersPath, "utf-8");
   const providers = JSON.parse(jsonContent);
   console.log(`Loaded ${providers.length} providers from JSON file`);
@@ -62,12 +55,10 @@ async function main() {
     `Filtered ${filteredProviders.length} providers from the JSON file`
   );
 
-  // Write the result to a new file
   fs.writeFileSync(providersPath, JSON.stringify(filteredProviders, null, 2));
   console.log(`Filtered providers saved to ${providersPath}`);
 }
 
-// Execute the main function
 main()
   .then((x) => {
     console.log("Done");
