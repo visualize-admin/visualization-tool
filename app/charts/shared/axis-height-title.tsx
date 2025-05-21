b093ac07db9c8539a57f0e2bb082c3429ae16884
Bartosz Prusinowski <bartosz@interactivethings.com>
2025-05-21 12:43:52 +0200

    feat: Add Y axis title to pie charts

--- app/charts/shared/axis-height-title.tsx
+++ app/charts/shared/axis-height-title.tsx
@@ -1,15 +1,19 @@
+import { PieState } from "@/charts/pie/pie-state";
 import { AxisHeightLinearChartState } from "@/charts/shared/axis-height-linear";
 import { useChartState } from "@/charts/shared/chart-state";
 import { useChartTheme } from "@/charts/shared/use-chart-theme";
 import { OpenMetadataPanelWrapper } from "@/components/metadata-panel";
 
+// Axis title can be used in both "linear" and pie charts.
+type AxisHeightTitleChartState = AxisHeightLinearChartState | PieState;
+
 export const AxisHeightTitle = () => {
   const { axisLabelFontSize } = useChartTheme();
   const { chartType, yAxisLabel, leftAxisLabelSize, leftAxisLabelOffsetTop } =
-    useChartState() as AxisHeightLinearChartState;
+    useChartState() as AxisHeightTitleChartState;
   // Axis title can also be used in combo line single charts.
   const { yMeasure } = useChartState() as Exclude<
-    AxisHeightLinearChartState,
+    AxisHeightTitleChartState,
     { chartType: "comboLineSingle" }
   >;
 
