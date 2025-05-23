92fa8960d960ecd7e6791384a180606edfd39ef9
Bartosz Prusinowski <bartosz@interactivethings.com>
2025-05-23 12:56:06 +0200

    refactor: Extract green

--- app/themes/palette.ts
+++ app/themes/palette.ts
@@ -30,6 +30,20 @@ const orange: PaletteOptions["orange"] = {
   900: "#7C2D12",
 };
 
+const green: PaletteOptions["green"] = {
+  main: "#047857",
+  50: "#ECFDF5",
+  100: "#D1FAE5",
+  200: "#A7F3D0",
+  300: "#6EE7B7",
+  400: "#34D399",
+  500: "#10B981",
+  600: "#059669",
+  700: "#047857",
+  800: "#065F46",
+  900: "#064E3B",
+};
+
 export const palette = {
   primary: {
     main: blue[700],
@@ -79,18 +93,6 @@ export const palette = {
     800: "#92400E",
     900: "#78350F",
   },
-  green: {
-    main: "#047857",
-    50: "#ECFDF5",
-    100: "#D1FAE5",
-    200: "#A7F3D0",
-    300: "#6EE7B7",
-    400: "#34D399",
-    500: "#10B981",
-    600: "#059669",
-    700: "#047857",
-    800: "#065F46",
-    900: "#064E3B",
-  },
+  green,
   blue,
 } satisfies ThemeOptions["palette"];
