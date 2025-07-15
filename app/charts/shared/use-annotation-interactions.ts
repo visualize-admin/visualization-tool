04f0d0345e89461dacfa1777dade9025b1a5b686
Bartosz Prusinowski <bartosz@interactivethings.com>
2025-07-15 10:08:26 +0200

    refactor: useOverlayInteractions -> useAnnotationInteractions

--- app/charts/shared/use-annotation-interactions.ts
+++ app/charts/shared/use-annotation-interactions.ts
@@ -11,11 +11,7 @@ import { useConfiguratorState } from "@/configurator/configurator-state";
 import { Observation } from "@/domain/data";
 import { useEvent } from "@/utils/use-event";
 
-export const useOverlayInteractions = ({
-  getSegment,
-}: {
-  getSegment?: (d: Observation) => string;
-}) => {
+export const useAnnotationInteractions = () => {
   const [, dispatchInteraction] = useInteraction();
   const [state, dispatch] = useConfiguratorState();
   const chartConfig = getChartConfig(state);
@@ -24,60 +20,55 @@ export const useOverlayInteractions = ({
   const activeAnnotation = annotations.find((a) => a.key === activeField);
 
   // TODO: Differentiate between edit and view modes (to open annotation).
-  const onClick = useEvent((observation: Observation) => {
-    if (!isEditingAnnotation || !activeAnnotation || !activeField) {
-      return;
-    }
+  const onClick = useEvent(
+    (observation: Observation, { segment }: { segment?: string }) => {
+      if (!isEditingAnnotation || !activeAnnotation || !activeField) {
+        return;
+      }
 
-    const currentTarget = activeAnnotation.target;
-    const newTarget = getAnnotationTargetFromObservation(observation, {
-      chartConfig,
-      getSegment,
-    });
-
-    if (isEqual(currentTarget, newTarget)) {
-      return;
-    }
-
-    const otherAnnotationWithSameTarget = annotations.find(
-      (a) =>
-        a.key !== activeField && matchesAnnotationTarget(observation, a.target)
-    );
-
-    if (otherAnnotationWithSameTarget) {
-      return;
-    }
-
-    dispatch({
-      type: "CHART_ANNOTATION_TARGET_CHANGE",
-      value: {
-        key: activeField,
-        target: newTarget,
-      },
-    });
-  });
-
-  const onHover = useEvent((observation: Observation) => {
-    if (isEditingAnnotation) {
-      dispatchInteraction({
-        type: "INTERACTION_UPDATE",
-        value: {
-          type: "focus",
-          visible: true,
-          observation,
-        },
+      const currentTarget = activeAnnotation.target;
+      const newTarget = getAnnotationTargetFromObservation(observation, {
+        chartConfig,
+        segment,
       });
-    } else {
-      dispatchInteraction({
-        type: "INTERACTION_UPDATE",
+
+      if (isEqual(currentTarget, newTarget)) {
+        return;
+      }
+
+      const otherAnnotationWithSameTarget = annotations.find(
+        (a) =>
+          a.key !== activeField &&
+          matchesAnnotationTarget(observation, a.target)
+      );
+
+      if (otherAnnotationWithSameTarget) {
+        return;
+      }
+
+      dispatch({
+        type: "CHART_ANNOTATION_TARGET_CHANGE",
         value: {
-          type: "tooltip",
-          visible: true,
-          observation,
+          key: activeField,
+          target: newTarget,
         },
       });
     }
-  });
+  );
+
+  const onHover = useEvent(
+    (observation: Observation, { segment }: { segment?: string }) => {
+      dispatchInteraction({
+        type: "INTERACTION_UPDATE",
+        value: {
+          type: isEditingAnnotation ? "focus" : "tooltip",
+          visible: true,
+          observation,
+          segment,
+        },
+      });
+    }
+  );
 
   const onHoverOut = useEvent(() => {
     dispatchInteraction({
