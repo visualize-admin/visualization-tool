2acbe023de6c8dfdee9821742bfbd54a2645c6f9
Bartosz Prusinowski <bartosz@interactivethings.com>
2025-08-28 12:10:01 +0200

    feat: Add support for interactive time range filters in tables

--- app/charts/table/chart-table.tsx
+++ app/charts/table/chart-table.tsx
@@ -1,10 +1,12 @@
 import { memo } from "react";
 
 import { ChartDataWrapper } from "@/charts/chart-data-wrapper";
+import { BrushTime, shouldShowBrush } from "@/charts/shared/brush";
 import { ChartContainer } from "@/charts/shared/containers";
-import { Table } from "@/charts/table/table";
+import { Table, TABLE_TIME_RANGE_HEIGHT } from "@/charts/table/table";
 import { TableChart } from "@/charts/table/table-state";
 import { TableConfig } from "@/configurator";
+import { hasChartConfigs, useConfiguratorState } from "@/configurator";
 
 import { ChartProps, VisualizationProps } from "../shared/chart-props";
 
@@ -23,9 +25,28 @@ export const ChartTableVisualization = (
 };
 
 const ChartTable = memo(function ChartTable(props: ChartProps<TableConfig>) {
+  const { chartConfig } = props;
+  const { interactiveFiltersConfig } = chartConfig;
+  const [{ dashboardFilters }] = useConfiguratorState(hasChartConfigs);
+  const showTimeBrush = shouldShowBrush(
+    interactiveFiltersConfig,
+    dashboardFilters?.timeRange
+  );
+
   return (
     <TableChart {...props}>
       <ChartContainer>
+        {showTimeBrush && (
+          <svg
+            style={{
+              width: "100%",
+              height: TABLE_TIME_RANGE_HEIGHT,
+              minHeight: TABLE_TIME_RANGE_HEIGHT,
+            }}
+          >
+            <BrushTime yOffset={10} />
+          </svg>
+        )}
         <Table />
       </ChartContainer>
     </TableChart>
