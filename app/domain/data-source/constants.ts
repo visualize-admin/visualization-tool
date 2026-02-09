90bdcf0f4a248bbdd644bd3239bb2e14e7f17aea
Ludovic Muller <ludovicmuller1@gmail.com>
2026-02-09 16:10:49 +0100

    lindas-next: update int cached endpoint

--- app/domain/data-source/constants.ts
+++ app/domain/data-source/constants.ts
@@ -67,9 +67,9 @@ export const SOURCE_OPTIONS = [
     supportsCachingPerCubeIri: true,
   },
   {
-    value: "sparql+https://lindas-cached.int.cz-aws.net/query",
+    value: "sparql+https://int.cached.lindas.admin.ch/query",
     label: "LINDASnext-Int",
-    url: "https://lindas-cached.int.cz-aws.net/query",
+    url: "https://int.cached.lindas.admin.ch/query",
     isTrusted: false,
     supportsCachingPerCubeIri: true,
   },
