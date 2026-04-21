65475f582e680ddc46fde58c6b85e5091750606a
Ludovic Muller <ludovicmuller1@gmail.com>
2026-04-21 13:07:22 +0200

    feat: enable Sentry back

--- app/domain/env.ts
+++ app/domain/env.ts
@@ -51,6 +51,10 @@ export const ADFS_PROFILE_URL =
 export const MAPTILER_API_KEY =
   clientEnv?.MAPTILER_API_KEY ?? process.env.MAPTILER_API_KEY ?? "";
 
+export const SENTRY_CSP = process.env.NEXT_PUBLIC_SENTRY_CSP ?? "";
+export const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
+export const SENTRY_ENV = process.env.NEXT_PUBLIC_SENTRY_ENV;
+
 /**
  * Server-side-only **RUNTIME** variables (not exposed through window)
  */
