aff265780b9bb0051471d89658544e4ed0d0a3f3
Mathis Hofer <hofer@puzzle.ch>
2026-05-18 16:58:20 +0200

    chore: Read Node.js version from .nvmrc & update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,17 @@
 let
   pkgs = import <nixpkgs> { };
-  nodejs = pkgs.nodejs-22_x;
 
-in pkgs.mkShell {
-  buildInputs = [
-    pkgs.darwin.apple_sdk.frameworks.CoreServices
-    nodejs
-  ];
+  # Read the Node.js major version from .nvmrc (e.g. "24" -> pkgs.nodejs_24).
+  nvmrc = pkgs.lib.fileContents ./.nvmrc;
+  major = pkgs.lib.head (pkgs.lib.splitString "." (pkgs.lib.removePrefix "v" nvmrc));
+  nodejs = pkgs."nodejs_${major}";
+
+  yarn = pkgs.yarn;
+
+in
+pkgs.mkShell {
+  buildInputs = [ nodejs yarn ]
+    ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+      pkgs.darwin.apple_sdk.frameworks.CoreServices
+    ];
 }
